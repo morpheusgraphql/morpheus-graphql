dbe59474711a1d4f8f429911745d9247f0722774
nalchevanidze <d.nalchevanidze@gmail.com>
2019-02-13 16:04:23 +0100

    replace validation

--- src/Data/GraphqlHS/PreProcess.hs
+++ src/Data/GraphqlHS/PreProcess.hs
@@ -68,7 +68,7 @@ existsType typeName typeLib = case (lookup typeName typeLib) of
 validateSpread :: FragmentLib -> Text -> Eval [(Text, QuerySelection)]
 validateSpread frags key = case lookup key frags of
     Nothing -> handleError $ pack $ "Fragment not found: " ++ (show key)
-    Just (Fragment _ _ (SelectionSet gqlObj)) -> pure (toList gqlObj)
+    Just (Fragment _ _ (SelectionSet arguments gqlObj)) -> pure (toList gqlObj)
 
 
 -- TODO: replace all var types with Variable values
@@ -90,44 +90,21 @@ validateArg root requestArgs inpValue =
         Just x -> replaceVariable root x >>= \x -> pure (key, x)
             where key = inputValueName inpValue
 
-
-
 -- TODO: throw Error when gql request has more arguments al then inputType
 validateHead :: GQLQueryRoot -> GQL__Type -> Text -> Head -> Eval Head
 validateHead root currentType key (Arguments args) =
     case (fieldArgsByKey key currentType) of
         Nothing -> Left $ cannotQueryField key (name currentType)
         Just field ->
-            mapM (validateArg root args) field >>= pure . Head . fromList
+            mapM (validateArg root args) field >>= pure . Arguments . fromList
 
-validateFieldBody
-    :: GQLTypeLib
-    -> GQLQueryRoot
-    -> GQL__Type
-    -> (Text, QuerySelection)
-    -> Eval (Text, QuerySelection)
-validateFieldBody typeLib root currentType (fieldName, field) =
-    case (fields currentType) of
-        Some gqlVal -> case (getFieldTypeByKey fieldName currentType) of
-            Nothing -> Left $ cannotQueryField fieldName (name currentType)
-            Just fieldType -> do
-                value <- validateBySchema typeLib (name fieldType) root field
-                pure (fieldName, value)
-        _ ->
-            handleError $ pack $ "has not fields" ++ (show $ fields currentType)
 
-handleField
-    :: GQLTypeLib
-    -> GQLQueryRoot
-    -> GQL__Type
-    -> (Text, QuerySelection)
-    -> Eval (Text, QuerySelection)
-handleField typeLib root currentType (fieldName, field) = case field of
-    SelectionSet head0 body0 -> do
-        body <- validateFieldBody typeLib root currentType (fieldName, body0)
-        pure $ (fieldName, SelectionSet head (snd body))
-    _ -> validateFieldBody typeLib root currentType (fieldName, field)
-
+fieldTypeOf :: GQL__Type -> Text -> Eval GQL__Type
+fieldTypeOf _type fieldName = case (fields _type) of
+    Some gqlVal -> case (getFieldTypeByKey fieldName _type) of
+        Nothing    -> Left $ cannotQueryField fieldName (name _type)
+        Just ftype -> pure ftype
+    _ -> handleError $ pack $ "has not fields" ++ (show $ fields _type)
 
 propagateSpread
     :: GQLQueryRoot -> (Text, QuerySelection) -> Eval [(Text, QuerySelection)]
@@ -137,16 +114,16 @@ propagateSpread frags (text, value     ) = pure [(text, value)]
 validateBySchema
     :: GQLTypeLib
     -> GQLQueryRoot
-    -> Text
+    -> GQL__Type
     -> (Text, QuerySelection)
     -> Eval (Text, QuerySelection)
-validateBySchema typeLib root typeName (fieldName, (SelectionSet head gqlObj))
+validateBySchema typeLib root _parentType (fieldName, (SelectionSet head selectors0))
     = do
-        extended <- concat <$> (mapM (propagateSpread root) (toList gqlObj))
-        existsType typeName typeLib >>= \_type -> do
-            head' <- validateHead root _type fieldName head
-            body' <-
-                fromList
-                    <$> (mapM (validateBySchema typeLib root typeName) extended)
-            pure (fieldName, SelectionSet head body')
+        _type <- fieldTypeOf _parentType fieldName
+        existsType (name _type) typeLib >>= \_type -> do
+            head'     <- validateHead root _type fieldName head
+            selectors <- concat
+                <$> mapM (propagateSpread root) (toList selectors0)
+            selectors' <- mapM (validateBySchema typeLib root _type) selectors
+            pure (fieldName, SelectionSet head (fromList selectors'))
 validateBySchema _ _ _ x = pure x
