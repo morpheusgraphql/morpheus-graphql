bf419555c284a2327f55ad3ae757546f05759be6
nalchevanidze <d.nalchevanidze@gmail.com>
2019-02-20 23:09:30 +0100

    add inputType Transformer

---
+++ src/Data/Morpheus/Generics/GenericInputType.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE TypeOperators , FlexibleInstances #-}
+
+module Data.Morpheus.Generics.GenericInputType (GToInput(..)) where
+
+import           GHC.Generics
+import           Data.Morpheus.Types.Types     ( Arguments
+                                                , Eval(..)
+                                                , (::->)(Some, None)
+                                                , MetaInfo(..)
+                                                , Argument(..)
+                                                , JSType(..)
+                                                )
+import           Data.Text                     ( Text, pack )
+
+fixProxy :: (a -> f a) -> f a
+fixProxy f = f undefined
+
+class GToInput f where
+    gToInput :: MetaInfo -> Arguments -> Eval (f a)
+
+instance GToInput U1  where
+    gToInput _ _ = pure U1
+
+instance (Selector c, GToInput f ) => GToInput (M1 S c f) where
+    gToInput meta gql = fixProxy (\x -> M1 <$> gToInput (meta{ key = pack $ selName x}) gql)
+
+instance (Datatype c, GToInput f)  => GToInput (M1 D c f)  where
+    gToInput meta gql  = fixProxy(\x -> M1 <$> gToInput (meta {className = pack $ datatypeName x}) gql)
+
+instance GToInput f  => GToInput (M1 C c f)  where
+    gToInput meta gql  = M1 <$> gToInput meta gql
+
+instance (GToInput f , GToInput g ) => GToInput (f :*: g)  where
+    gToInput meta gql = (:*:) <$> gToInput meta gql <*> gToInput meta gql
