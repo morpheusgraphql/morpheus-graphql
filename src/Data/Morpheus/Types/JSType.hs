65f09a8a409bbbb349d5fe647fce083156afae95
nalchevanidze <d.nalchevanidze@gmail.com>
2019-03-01 17:57:35 +0100

    organize imports in JSType

--- src/Data/Morpheus/Types/JSType.hs
+++ src/Data/Morpheus/Types/JSType.hs
@@ -2,14 +2,21 @@
 
 module Data.Morpheus.Types.JSType where
 
-import           Data.Aeson
-import           Data.Text                      (Text , pack )
+import           Data.Aeson                     ( ToJSON(..)
+                                                , (.=)
+                                                , FromJSON(..)
+                                                , Value(..)
+                                                , pairs
+                                                )
+import           Data.Text                      ( Text
+                                                , pack
+                                                )
 import           GHC.Generics                   ( Generic )
-import           Data.HashMap.Strict            (toList)
+import           Data.HashMap.Strict            ( toList )
 
 replaceType :: Text -> Text
 replaceType "_type" = "type"
-replaceType x = x
+replaceType x       = x
 
 data JSType =  JSObject [(Text, JSType)] | JSList [JSType] |  JSEnum Text | JSInt Int | JSBool Bool | JSString Text | JSNull  deriving (Show, Generic)
 
@@ -22,13 +29,13 @@ instance ToJSON JSType where
     toEncoding (JSObject x) = pairs $ foldl1 (<>) $ map encodeField x
        where encodeField (key, value ) = key .= value
 
-replace (key, val ) = (key, replaceValue val )
+replace (key, val) = (key, replaceValue val)
 
 replaceValue :: Value -> JSType
-replaceValue (Bool v) = JSBool v
+replaceValue (Bool   v) = JSBool v
 replaceValue (Number v) = JSInt 0 -- TODO: fix number from 0 to actual value
 replaceValue (String v) = JSString v
-replaceValue (Object v) = JSObject  $ map replace (toList v)
+replaceValue (Object v) = JSObject $ map replace (toList v)
 
 instance FromJSON JSType where
     parseJSON = pure . replaceValue
