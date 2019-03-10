44ac3f1630f6c84da744d71bc4a1eab7b005f98d
nalchevanidze <d.nalchevanidze@gmail.com>
2019-03-10 14:03:25 +0100

    internal error

--- morpheus/src/Data/Morpheus/Types/Error.hs
+++ morpheus/src/Data/Morpheus/Types/Error.hs
@@ -14,17 +14,23 @@ import           Data.Data                      ( Data )
 import           Data.Text                      ( Text )
 import           GHC.Generics                   ( Generic )
 
+
+data InternalError = InternalError {
+    desc :: Text ,
+    posIndex :: Int
+} deriving (Show)
+
 type GQLErrors = [GQLError ]
 
 data ErrorLocation = ErrorLocation {
     line :: Int
     ,column :: Int
-} deriving (Show , Generic, ToJSON,Data,FromJSON)
+} deriving (Show , Generic, ToJSON)
 
 
 data GQLError = GQLError {
     message::Text,
     locations:: [ErrorLocation]
-} deriving (Show , Generic, Data, ToJSON,FromJSON)
+} deriving (Show , Generic , ToJSON)
 
 
