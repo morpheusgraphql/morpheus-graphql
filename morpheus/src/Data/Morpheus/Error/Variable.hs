4df5e488b3cf5478ad3dc8b344c9434384594472
nalchevanidze <d.nalchevanidze@gmail.com>
2019-03-14 08:38:40 +0100

    TODO error messages for GQL

---
+++ morpheus/src/Data/Morpheus/Error/Variable.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.Morpheus.Error.Variable where
+
+import           Data.Morpheus.Error.Utils    (errorMessage)
+import           Data.Morpheus.Types.Error    (GQLErrors)
+import           Data.Morpheus.Types.MetaInfo (MetaInfo (..))
+import qualified Data.Text                    as T (concat)
+
+{-|
+VARIABLES:
+
+Variable -> Error (position Query Head)
+  data E = EN | DE
+  query M ( $v : E ){...}
+
+case type does not exists
+  query Q ($a: D) ->  "Unknown type \"D\"."
+
+
+case String
+  - { "v" : "EN" }  ->  no error converts as enum
+
+case type mismatch
+  - { "v": { "a": "v1" ... } } -> "Variable \"$v\" got invalid value { "a": "v1" ... } ; Expected type LANGUAGE."
+  - { "v" : "v1" }  -> "Variable \"$v\" got invalid value \"v1\"; Expected type LANGUAGE."
+  - { "v": 1  }        "Variable \"$v\" got invalid value 1; Expected type LANGUAGE."
+
+case unused variable
+  - query M ( $v : String ) { a } -> "Variable \"$bla\" is never used in operation \"MyMutation\".",
+
+case variable does not match to argument type
+  - query M ( $v : String ) { a(p:$v) } -> "Variable \"$v\" of type \"String\" used in position expecting type \"LANGUAGE\"."
+
+|-}
+variableIsNotDefined :: MetaInfo -> GQLErrors
+variableIsNotDefined meta = errorMessage (position meta) text
+  where
+    text = T.concat ["Variable \"", key meta, "\" is not defined by operation \"", typeName meta, "\"."]
--- morpheus/src/Data/Morpheus/ErrorMessage.hs
+++ morpheus/src/Data/Morpheus/ErrorMessage.hs
@@ -7,7 +7,6 @@ module Data.Morpheus.ErrorMessage
   , handleError
   , requiredArgument
   , errorMessage
-  , variableIsNotDefined
   , unsupportedArgumentType
   , invalidEnumOption
   , unknownArguments
@@ -20,35 +19,6 @@ import           Data.Morpheus.Types.JSType   (JSType (..))
 import           Data.Morpheus.Types.MetaInfo (MetaInfo (..), Position)
 import           Data.Text                    (Text, pack)
 import qualified Data.Text                    as T (concat)
-
-
-{-|
-VARIABLES:
-
-Variable -> Error (position Query Head)
-  data E = EN | DE
-  query M ( $v : E ){...}
-
-case type does not exists
-  query Q ($a: D) ->  "Unknown type \"D\"."
-
-
-case String
-  - { "v" : "EN" }  ->  no error converts as enum
-
-case type mismatch
-  - { "v": { "a": "v1" ... } } -> "Variable \"$v\" got invalid value { "a": "v1" ... } ; Expected type LANGUAGE."
-  - { "v" : "v1" }  -> "Variable \"$v\" got invalid value \"v1\"; Expected type LANGUAGE."
-  - { "v": 1  }        "Variable \"$v\" got invalid value 1; Expected type LANGUAGE."
-
-case unused variable
-  - query M ( $v : String ) { a } -> "Variable \"$bla\" is never used in operation \"MyMutation\".",
-
-case variable does not match to argument type
-  - query M ( $v : String ) { a(p:$v) } -> "Variable \"$v\" of type \"String\" used in position expecting type \"LANGUAGE\"."
-
-|-}
-
 
 {-
   ARGUMENTS:
@@ -95,10 +65,6 @@ unsupportedArgumentType meta = errorMessage (position meta) text
   where
     text = T.concat ["Argument \"", key meta, "\" has unsuported type \"", typeName meta, "\"."]
 
-variableIsNotDefined :: MetaInfo -> GQLErrors
-variableIsNotDefined meta = errorMessage (position meta) text
-  where
-    text = T.concat ["Variable \"", key meta, "\" is not defined by operation \"", typeName meta, "\"."]
 
 unknownArguments :: Text -> [Text] -> GQLErrors
 unknownArguments fieldName = map keyToError
