5f3333804bf3f41c74842efd2b535457643118ce
nalchevanidze <d.nalchevanidze@gmail.com>
2019-03-18 23:43:05 +0100

    warnings to Error

--- morpheus/src/Data/Morpheus/Error/Internal.hs
+++ morpheus/src/Data/Morpheus/Error/Internal.hs
@@ -14,19 +14,17 @@ import           Data.Morpheus.Types.JSType (JSType (..))
 import qualified Data.Text                  as T (Text, concat, pack)
 
 -- GQL:: if no mutation defined -> "Schema is not configured for mutations."
--- TODO: remove it
 internalError :: T.Text -> Either GQLErrors b
-internalError x = Left $ errorMessage 0 $ T.concat ["Field Error: ", x]
+internalError x = Left $ errorMessage 0 $ T.concat ["INTERNAL ERROR: ", x]
 
 internalArgumentError :: T.Text -> Either GQLErrors b
-internalArgumentError x = Left $ errorMessage 0 $ T.concat ["Field Argument Error: ", x]
+internalArgumentError x = internalError $ T.concat ["Field Argument Error: ", x]
 
 internalTypeMismatch :: T.Text -> JSType -> Either GQLErrors b
-internalTypeMismatch text jsType =
-  Left $ errorMessage 0 $ T.concat ["Field Error: Type mismatch", text, T.pack $ show jsType]
+internalTypeMismatch text jsType = internalError $ T.concat ["Type mismatch", text, T.pack $ show jsType]
 
 internalUnresolvedField :: T.Text -> Either GQLErrors b
-internalUnresolvedField text = Left $ errorMessage 0 $ T.concat ["Field Error: Unresolverd field", text]
+internalUnresolvedField text = internalError $ T.concat ["Unresolverd field", text]
 
 internalUndefinedResolver :: T.Text -> Either GQLErrors b
-internalUndefinedResolver text = Left $ errorMessage 0 $ T.concat ["Field resolver not implemented Error: ", text]
+internalUndefinedResolver text = internalError $ T.concat ["Resolver not implemented Error: ", text]
