b0c6e1868f9e15a8973879ad069d64219904ab3c
nalchevanidze <d.nalchevanidze@gmail.com>
2019-03-23 00:19:22 +0100

    right error message on duplicate key selection

--- morpheus/src/Data/Morpheus/PreProcess/PreProcess.hs
+++ morpheus/src/Data/Morpheus/PreProcess/PreProcess.hs
@@ -7,29 +7,31 @@ module Data.Morpheus.PreProcess.PreProcess
   ( preProcessQuery
   ) where
 
-import           Data.List                              ((\\))
-import           Data.Morpheus.Error.Selection          (cannotQueryField, selectionError)
+import           Data.Morpheus.Error.Selection          (duplicateQuerySelections, selectionError)
 import           Data.Morpheus.Error.Utils              (toGQLError)
 import           Data.Morpheus.PreProcess.Arguments     (validateArguments)
 import           Data.Morpheus.PreProcess.Fragment      (validateFragments)
 import           Data.Morpheus.PreProcess.Spread        (prepareRawSelection)
-import           Data.Morpheus.PreProcess.Utils         (existsType, fieldOf, fieldType)
+import           Data.Morpheus.PreProcess.Utils         (differKeys, existsType, fieldOf, fieldType)
 import           Data.Morpheus.PreProcess.Variable      (validateVariables)
+import qualified Data.Morpheus.Schema.Type              as T (name)
 import           Data.Morpheus.Schema.Utils.Utils       (Type, TypeLib)
+
+import           Data.Morpheus.Types.Core               (EnhancedKey (..))
 import           Data.Morpheus.Types.Error              (MetaValidation, Validation)
-import           Data.Morpheus.Types.MetaInfo           (MetaInfo (..), Position)
+import           Data.Morpheus.Types.MetaInfo           (Position)
 import           Data.Morpheus.Types.Query.Operator     (Operator (..), RawOperator, ValidOperator)
 import           Data.Morpheus.Types.Query.RawSelection (RawArguments, RawSelectionSet)
 import           Data.Morpheus.Types.Query.Selection    (Selection (..), SelectionSet)
 import           Data.Morpheus.Types.Types              (GQLQueryRoot (..))
 import qualified Data.Set                               as S
-import           Data.Text                              (Text, pack)
+import           Data.Text                              (Text)
 
 asSelectionValidation :: MetaValidation a -> Validation a
 asSelectionValidation = toGQLError selectionError
 
 mapSelectors :: TypeLib -> Type -> SelectionSet -> Validation SelectionSet
-mapSelectors typeLib _type selectors = checkDuplicates selectors >>= mapM (validateBySchema typeLib _type)
+mapSelectors typeLib type' selectors = checkDuplicatesOn type' selectors >>= mapM (validateBySchema typeLib type')
 
 validateBySchema :: TypeLib -> Type -> (Text, Selection) -> Validation (Text, Selection)
 validateBySchema typeLib _parentType (sName, SelectionSet args selectors pos) = do
@@ -44,15 +46,18 @@ validateBySchema typeLib _parentType (sName, Field args field pos) = do
   headQS <- validateArguments typeLib fieldSD pos args
   pure (sName, Field headQS field pos)
 
-checkDuplicates :: [(Text, a)] -> Validation [(Text, a)]
-checkDuplicates x =
-  case keys \\ noDuplicates keys of
-    []         -> pure x
-    duplicates -> Left $ cannotQueryField $ meta duplicates
+selToKey :: (Text, Selection) -> EnhancedKey
+selToKey (sName, Field _ _ pos)        = EnhancedKey sName pos
+selToKey (sName, SelectionSet _ _ pos) = EnhancedKey sName pos
+
+checkDuplicatesOn :: Type -> SelectionSet -> Validation SelectionSet
+checkDuplicatesOn type' keys =
+  case differKeys enhancedKeys noDuplicates of
+    []         -> pure keys
+    duplicates -> Left $ duplicateQuerySelections (T.name type') duplicates
   where
-    keys = map fst x
-    noDuplicates = S.toList . S.fromList
-    meta duplicates = MetaInfo {typeName = "-- TODO: Error handling", key = pack $ show duplicates, position = 0}
+    enhancedKeys = map selToKey keys
+    noDuplicates = S.toList $ S.fromList (map fst keys)
 
 getOperationInfo :: RawOperator -> (Text, RawArguments, RawSelectionSet, Position)
 getOperationInfo (Query _ args sel pos)    = ("Query", args, sel, pos)
