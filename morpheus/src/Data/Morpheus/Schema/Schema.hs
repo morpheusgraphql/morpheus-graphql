b37c32b6c8661ffbfa3dace56cf59dfd74a403e4
nalchevanidze <d.nalchevanidze@gmail.com>
2019-04-01 17:42:19 +0200

    add Query name

--- morpheus/src/Data/Morpheus/Schema/Schema.hs
+++ morpheus/src/Data/Morpheus/Schema/Schema.hs
@@ -6,9 +6,10 @@ module Data.Morpheus.Schema.Schema where
 
 import           Data.Data                           (Data)
 import           Data.Morpheus.Schema.Directive      (Directive)
-import           Data.Morpheus.Schema.Internal.Types (TypeLib (..))
+import           Data.Morpheus.Schema.Internal.Types (OutputObject, TypeLib (..))
 import           Data.Morpheus.Schema.Utils.Utils    (Type, createObjectType, typeFromInputObject, typeFromLeaf,
                                                       typeFromObject)
+import           Data.Text                           (Text)
 import           GHC.Generics                        (Generic)
 
 data Schema = Schema
@@ -21,14 +22,19 @@ data Schema = Schema
 
 convertTypes :: TypeLib -> [Type]
 convertTypes lib' =
-  map typeFromObject (object lib') ++
-  [typeFromObject $ query lib'] ++ map typeFromInputObject (inputObject lib') ++ map typeFromLeaf (leaf lib')
+  [typeFromObject $ query lib'] ++
+  typeFromMutation (mutation lib') ++
+  map typeFromObject (object lib') ++ map typeFromInputObject (inputObject lib') ++ map typeFromLeaf (leaf lib')
+
+typeFromMutation :: Maybe (Text, OutputObject) -> [Type]
+typeFromMutation (Just x) = [typeFromObject x]
+typeFromMutation Nothing  = []
 
 initSchema :: TypeLib -> Schema
 initSchema types' =
   Schema
     { types = convertTypes types'
-    , queryType = createObjectType "Query" "Query Description" []
+    , queryType = createObjectType (fst $ query types') "Query Description" []
     , mutationType = Nothing
     , subscriptionType = Nothing
     , directives = []
