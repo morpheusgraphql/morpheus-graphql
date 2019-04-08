0558afd84d3ec2ff563cdf38b14d3f36481d0e1e
nalchevanidze <d.nalchevanidze@gmail.com>
2019-04-08 20:09:19 +0200

    remove meta errors from argument validation

--- morpheus/src/Data/Morpheus/Error/Arguments.hs
+++ morpheus/src/Data/Morpheus/Error/Arguments.hs
@@ -3,13 +3,12 @@
 module Data.Morpheus.Error.Arguments
   ( requiredArgument
   , unknownArguments
-  , argumentError
   , argumentGotInvalidValue
   ) where
 
 import           Data.Morpheus.Error.Utils    (errorMessage)
 import           Data.Morpheus.Types.Core     (EnhancedKey (..))
-import           Data.Morpheus.Types.Error    (GQLError (..), GQLErrors, MetaError (..))
+import           Data.Morpheus.Types.Error    (GQLError (..), GQLErrors)
 import           Data.Morpheus.Types.MetaInfo (MetaInfo (..))
 import           Data.Text                    (Text)
 import qualified Data.Text                    as T (concat)
@@ -28,11 +27,6 @@ import qualified Data.Text                    as T (concat)
   - experience( a1 : 1 ) -> "Unknown argument \"a1\" on field \"experience\" of type \"Experience\".",
   - date(name: "name") -> "Unknown argument \"name\" on field \"date\" of type \"Experience\"."
 -}
-argumentError :: MetaError -> GQLErrors
-argumentError (UnknownType meta)    = requiredArgument meta
-argumentError (UnknownField meta)   = requiredArgument meta
-argumentError (TypeMismatch meta _) = requiredArgument meta
-
 argumentGotInvalidValue :: Text -> Text -> Int -> GQLErrors
 argumentGotInvalidValue key' inputMessage' position' = errorMessage position' text
   where
