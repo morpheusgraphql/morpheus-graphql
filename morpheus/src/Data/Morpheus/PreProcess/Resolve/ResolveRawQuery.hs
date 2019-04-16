d55f0e86bfa216db2bda610bdb85e9f60f0c92fe
nalchevanidze <d.nalchevanidze@gmail.com>
2019-04-16 20:17:14 +0200

    resolve Arguments

--- morpheus/src/Data/Morpheus/PreProcess/Resolve/ResolveRawQuery.hs
+++ morpheus/src/Data/Morpheus/PreProcess/Resolve/ResolveRawQuery.hs
@@ -2,27 +2,26 @@ module Data.Morpheus.PreProcess.Resolve.ResolveRawQuery
   ( resolveRawQuery
   ) where
 
-import           Data.Morpheus.PreProcess.Resolve.Spread     (resolveSpread)
-import           Data.Morpheus.PreProcess.Selection          (lookupSelectionObjectFieldType)
-import           Data.Morpheus.PreProcess.Validate.Arguments (onlyResolveArguments)
-import           Data.Morpheus.Schema.Internal.Types         (GObject (..), ObjectField (..), OutputObject,
-                                                              TypeLib (..))
-import           Data.Morpheus.Types.Error                   (Validation)
-import           Data.Morpheus.Types.Query.Fragment          (FragmentLib)
-import           Data.Morpheus.Types.Query.RawSelection      (RawSelection (..), RawSelectionSet)
-import           Data.Morpheus.Types.Query.Selection         (Selection (..), SelectionSet)
-import           Data.Morpheus.Types.Types                   (Variables)
-import           Data.Text                                   (Text)
+import           Data.Morpheus.PreProcess.Resolve.Arguments (resolveArguments)
+import           Data.Morpheus.PreProcess.Resolve.Spread    (resolveSpread)
+import           Data.Morpheus.PreProcess.Selection         (lookupSelectionObjectFieldType)
+import           Data.Morpheus.Schema.Internal.Types        (GObject (..), ObjectField (..), OutputObject, TypeLib (..))
+import           Data.Morpheus.Types.Error                  (Validation)
+import           Data.Morpheus.Types.Query.Fragment         (FragmentLib)
+import           Data.Morpheus.Types.Query.RawSelection     (RawSelection (..), RawSelectionSet)
+import           Data.Morpheus.Types.Query.Selection        (Selection (..), SelectionSet)
+import           Data.Morpheus.Types.Types                  (Variables)
+import           Data.Text                                  (Text)
 
 resolveVariableAndSpread ::
      TypeLib -> FragmentLib -> Variables -> GObject ObjectField -> (Text, RawSelection) -> Validation SelectionSet
 resolveVariableAndSpread lib' fragments' variables' parent' (key', RawSelectionSet rawArgs rawSelectors position') = do
   fieldType' <- lookupSelectionObjectFieldType position' key' lib' parent'
-  args' <- onlyResolveArguments variables' position' rawArgs
+  args' <- resolveArguments variables' position' rawArgs
   sel <- concat <$> mapM (resolveVariableAndSpread lib' fragments' variables' fieldType') rawSelectors
   pure [(key', SelectionSet args' sel position')]
 resolveVariableAndSpread _ _ variables' _ (sKey, RawField rawArgs field sPos) = do
-  args' <- onlyResolveArguments variables' sPos rawArgs
+  args' <- resolveArguments variables' sPos rawArgs
   pure [(sKey, Field args' field sPos)]
 resolveVariableAndSpread lib' fragments' variables' parent' (spreadID, Spread _ sPos) =
   concat <$> (resolveSpread fragments' parent' sPos spreadID >>= recursiveResolve)
--- morpheus/src/Data/Morpheus/PreProcess/Validate/Arguments.hs
+++ morpheus/src/Data/Morpheus/PreProcess/Validate/Arguments.hs
@@ -1,7 +1,5 @@
 module Data.Morpheus.PreProcess.Validate.Arguments
   ( validateArguments
-  , resolveArguments
-  , onlyResolveArguments
   ) where
 
 import           Data.Morpheus.Error.Arguments          (argumentGotInvalidValue, argumentNameCollision,
@@ -10,15 +8,11 @@ import           Data.Morpheus.Error.Input              (InputValidation, inputE
 import           Data.Morpheus.Error.Internal           (internalUnknownTypeMessage)
 import           Data.Morpheus.PreProcess.Input.Object  (validateInput)
 import           Data.Morpheus.PreProcess.Utils         (checkForUnknownKeys, checkNameCollision, getInputType)
-import           Data.Morpheus.PreProcess.Variable      (resolveArgumentValue)
-import           Data.Morpheus.Schema.Internal.Types    (Field (..), InputField (..), ObjectField (..), TypeLib)
-import           Data.Morpheus.Types.Core               (EnhancedKey (..))
+import Data.Morpheus.Schema.Internal.Types (Field(..), InputField(..), ObjectField(..), TypeLib)import           Data.Morpheus.Types.Core               (EnhancedKey (..))
 import           Data.Morpheus.Types.Error              (Validation)
 import           Data.Morpheus.Types.JSType             (JSType (JSNull))
 import           Data.Morpheus.Types.MetaInfo           (Position)
-import qualified Data.Morpheus.Types.Query.RawSelection as Raw (RawArguments)
 import           Data.Morpheus.Types.Query.Selection    (Argument (..), Arguments)
-import           Data.Morpheus.Types.Types              (Variables)
 import           Data.Text                              (Text)
 
 handleInputError :: Text -> Int -> InputValidation a -> Validation ()
@@ -40,9 +34,6 @@ validateArgument types position' requestArgs (key', InputField arg) =
         else pure (key', Argument JSNull position')
     Just (Argument value pos) -> validateArgumentValue types (fieldType arg) (key', Argument value pos)
 
-onlyResolveArguments :: Variables -> Position -> Raw.RawArguments -> Validation Arguments
-onlyResolveArguments variables' _ = mapM (resolveArgumentValue variables')
-
 checkForUnknownArguments :: (Text, ObjectField) -> Arguments -> Validation [(Text, InputField)]
 checkForUnknownArguments (fieldKey', ObjectField fieldArgs _) args' =
   checkForUnknownKeys enhancedKeys' fieldKeys error' >> checkNameCollision enhancedKeys' fieldKeys argumentNameCollision >>
@@ -52,10 +43,6 @@ checkForUnknownArguments (fieldKey', ObjectField fieldArgs _) args' =
     enhancedKeys' = map argToKey args'
     argToKey (key', Argument _ pos) = EnhancedKey key' pos
     fieldKeys = map fst fieldArgs
-
-resolveArguments :: TypeLib -> Variables -> (Text, ObjectField) -> Position -> Raw.RawArguments -> Validation Arguments
-resolveArguments typeLib root objectField pos args' =
-  onlyResolveArguments root pos args' >>= validateArguments typeLib objectField pos
 
 validateArguments :: TypeLib -> (Text, ObjectField) -> Position -> Arguments -> Validation Arguments
 validateArguments typeLib inputs pos args' =
