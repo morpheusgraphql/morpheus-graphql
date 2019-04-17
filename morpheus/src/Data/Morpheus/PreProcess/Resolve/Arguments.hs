fe887e3c8e2aa6746181072e1e36fe6850966414
Daviti Nalchevanidze <daviti.nalchevanidze@sinnerschrader.com>
2019-04-17 14:10:29 +0200

    validate type compatibility of argument and variable

--- morpheus/src/Data/Morpheus/PreProcess/Resolve/Arguments.hs
+++ morpheus/src/Data/Morpheus/PreProcess/Resolve/Arguments.hs
@@ -4,10 +4,10 @@ module Data.Morpheus.PreProcess.Resolve.Arguments
 
 import           Data.Morpheus.PreProcess.Variable      (resolveArgumentValue)
 import           Data.Morpheus.Types.Error              (Validation)
-import           Data.Morpheus.Types.MetaInfo           (Position)
 import qualified Data.Morpheus.Types.Query.RawSelection as Raw (RawArguments)
 import           Data.Morpheus.Types.Query.Selection    (Arguments)
 import           Data.Morpheus.Types.Types              (Variables)
+import           Data.Morpheus.Schema.Internal.Types    (GObject (..), ObjectField (..))
 
-resolveArguments :: Variables -> Position -> Raw.RawArguments -> Validation Arguments
+resolveArguments :: Variables -> GObject ObjectField -> Raw.RawArguments -> Validation Arguments
 resolveArguments variables' _ = mapM (resolveArgumentValue variables')
