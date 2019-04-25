d5c86ed39c33ffc282f08eb5bb9190c6d6b5d181
nalchevanidze <d.nalchevanidze@gmail.com>
2019-04-25 09:20:00 +0200

    move types

--- example/src/Example/Schema.hs
+++ example/src/Example/Schema.hs
@@ -11,10 +11,10 @@ module Example.Schema
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import           Data.Maybe                 (fromMaybe)
-import           Data.Morpheus              ((::->) (..), interpreter)
+import           Data.Morpheus              (interpreter)
 import           Data.Morpheus.Kind         (GQLArgs, GQLEnum, GQLInput, GQLKind (..), GQLMutation, GQLObject, GQLQuery,
                                              GQLScalar (..))
-import           Data.Morpheus.Types        (EnumOf (unpackEnum), GQLRoot (..), ScalarOf (..))
+import           Data.Morpheus.Types        ((::->) (..), EnumOf (unpackEnum), GQLRoot (..), ScalarOf (..))
 import           Data.Morpheus.Types.JSType (ScalarValue (..))
 import           Data.Text                  (Text, pack)
 import qualified Data.Text                  as T (concat)
