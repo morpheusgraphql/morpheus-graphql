881b19193515e6ebcbe598ac28a13c41180af25c
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-02 22:20:57 +0200

    feature/add-types-export-module

--- example/src/Example/Schema.hs
+++ example/src/Example/Schema.hs
@@ -14,7 +14,7 @@ import           Data.Maybe                 (fromMaybe)
 import           Data.Morpheus              (interpreter)
 import           Data.Morpheus.Kind         (GQLArgs, GQLEnum, GQLInput, GQLKind (..), GQLMutation, GQLObject, GQLQuery,
                                              GQLScalar (..))
-import           Data.Morpheus.Types.JSType (ScalarValue (..))
+import           Data.Morpheus.Types        (ScalarValue (..))
 import           Data.Morpheus.Wrapper      ((::->) (..), EnumOf, GQLRoot (..), ScalarOf, unwrap)
 import           Data.Text                  (Text, pack)
 import qualified Data.Text                  as T (concat)
---
+++ morpheus/src/Data/Morpheus/Types.hs
@@ -0,0 +1,5 @@
+module Data.Morpheus.Types
+  ( ScalarValue(..)
+  ) where
+
+import           Data.Morpheus.Types.JSType (ScalarValue (..))
