f4845c2c214ac88dfd10767aee7d9319878d878f
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-02 21:02:37 +0200

    interpreter can parse Text and Lazy Bytestring

--- morpheus/src/Data/Morpheus.hs
+++ morpheus/src/Data/Morpheus.hs
@@ -7,7 +7,7 @@ module Data.Morpheus
 
 import           Control.Monad.Trans.Except          (ExceptT (..), runExceptT)
 import           Data.Aeson                          (decode, encode)
-import qualified Data.ByteString.Lazy.Char8          as B
+import           Data.ByteString.Lazy.Char8          (ByteString)
 import           Data.Morpheus.Error.Utils           (errorMessage, renderErrors)
 import           Data.Morpheus.Kind.GQLMutation      (GQLMutation (..))
 import           Data.Morpheus.Kind.GQLQuery         (GQLQuery (..))
@@ -20,7 +20,9 @@ import           Data.Morpheus.Types.Query.Operator  (Operator (..))
 import           Data.Morpheus.Types.Request         (GQLRequest)
 import           Data.Morpheus.Types.Response        (GQLResponse (..))
 import           Data.Morpheus.Types.Types           (GQLRoot (..))
-import           Data.Text                           (pack)
+import           Data.Text                           (Text, pack)
+import           Data.Text.Lazy                      (fromStrict, toStrict)
+import           Data.Text.Lazy.Encoding             (decodeUtf8, encodeUtf8)
 
 schema :: (GQLQuery a, GQLMutation b) => a -> b -> TypeLib
 schema queryRes mutationRes = mutationSchema mutationRes $ querySchema queryRes
@@ -36,24 +38,30 @@ resolve rootResolver body = do
     queryRes = query rootResolver
     mutationRes = mutation rootResolver
 
-lineBreaks :: B.ByteString -> [Int]
+lineBreaks :: ByteString -> [Int]
 lineBreaks req =
   case decode req of
     Just x  -> parseLineBreaks x
     Nothing -> []
 
-interpreterRaw :: (GQLQuery a, GQLMutation b) => GQLRoot a b -> B.ByteString -> IO GQLResponse
+interpreterRaw :: (GQLQuery a, GQLMutation b) => GQLRoot a b -> ByteString -> IO GQLResponse
 interpreterRaw rootResolver request = do
   value <- runExceptT $ parseRequest request >>= resolve rootResolver
   case value of
     Left x  -> pure $ Errors $ renderErrors (lineBreaks request) x
     Right x -> pure $ Data x
 
-interpreter :: (GQLQuery a, GQLMutation b) => GQLRoot a b -> B.ByteString -> IO B.ByteString
-interpreter rootResolver request = encode <$> interpreterRaw rootResolver request
-
-parseRequest :: B.ByteString -> ResolveIO GQLRequest
+parseRequest :: ByteString -> ResolveIO GQLRequest
 parseRequest text =
   case decode text of
     Just x  -> pure x
     Nothing -> failResolveIO $ errorMessage 0 (pack $ show text)
+
+class Interpreter a where
+  interpreter :: (GQLQuery q, GQLMutation m) => GQLRoot q m -> a -> IO a
+
+instance Interpreter ByteString where
+  interpreter root request = encode <$> interpreterRaw root request
+
+instance Interpreter Text where
+  interpreter root request = toStrict . decodeUtf8 . encode <$> interpreterRaw root (encodeUtf8 $ fromStrict request)
