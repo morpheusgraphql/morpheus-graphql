99ea44913764ba02e198028f2ff868926c86f915
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-04 15:01:29 +0200

    udpate Introspection

--- morpheus/src/Data/Morpheus/Kind/Internal.hs
+++ morpheus/src/Data/Morpheus/Kind/Internal.hs
@@ -58,11 +58,40 @@ data INPUT_OBJECT
 
 type family GQLConstraint a b :: Constraint
 
+-- IntrospectionRouter class
+type Intro_ a = Proxy a -> TypeLib -> TypeLib
+
+type Decode_ a = JSType -> Validation a
+
+type Encode_ a = (Text, Selection) -> a -> ResolveIO JSType
+
+type Field_ a = Proxy a -> Text -> InputField
+
 class IntrospectionRouter a b where
-  __introspect :: Proxy b -> Proxy a -> TypeLib -> TypeLib
-  __decode :: Proxy b -> JSType -> Validation a
-  __encode :: Proxy b -> (Text, Selection) -> a -> ResolveIO JSType
-  __field :: Proxy b -> Proxy a -> Text -> InputField
+  __introspect :: Proxy b -> Intro_ a
+  __decode :: Proxy b -> Decode_ a
+  __encode :: Proxy b -> Encode_ a
+  __field :: Proxy b -> Field_ a
+
+_field ::
+     forall a. IntrospectionRouter a (GQL a)
+  => Field_ a
+_field = __field (Proxy @(GQL a))
+
+_encode ::
+     forall a. IntrospectionRouter a (GQL a)
+  => Encode_ a
+_encode = __encode (Proxy @(GQL a))
+
+_decode ::
+     forall a. IntrospectionRouter a (GQL a)
+  => Decode_ a
+_decode = __decode (Proxy @(GQL a))
+
+_introspect ::
+     forall a. IntrospectionRouter a (GQL a)
+  => Intro_ a
+_introspect = __introspect (Proxy @(GQL a))
 
 --instance IntrospectionRouter a b => IntrospectionRouter (Maybe a) b where
 --  __introspect proxyB _ = __introspect proxyB (Proxy @a)
@@ -70,66 +99,3 @@ class IntrospectionRouter a b where
 --  __encode proxyB sel (Just x) = __encode proxyB sel x
 --  __encode _ _ Nothing = pure JSNull
 --  __field proxyB _ = __field proxyB (Proxy @a)
--- TODO: Remove It after finishing
---instance IntrospectionRouter a SCALAR => IntrospectionRouter (ScalarOf a) SCALAR where
---  __introspect proxyB _ = __introspect proxyB (Proxy @a)
---  __decode proxy value = ScalarOf <$> __decode proxy value
---  __encode proxyB sel (ScalarOf x) = __encode proxyB sel x
---  __field proxyB _ = __field proxyB (Proxy @a)
-_field ::
-     forall a. IntrospectionRouter a (GQL a)
-  => Proxy a
-  -> Text
-  -> InputField
-_field = __field (Proxy @(GQL a))
-
-_encode ::
-     forall a. IntrospectionRouter a (GQL a)
-  => (Text, Selection)
-  -> a
-  -> ResolveIO JSType
-_encode = __encode (Proxy @(GQL a))
-
-_decode ::
-     forall a. IntrospectionRouter a (GQL a)
-  => JSType
-  -> Validation a
-_decode = __decode (Proxy @(GQL a))
-
-_introspect ::
-     forall a. IntrospectionRouter a (GQL a)
-  => Proxy a
-  -> TypeLib
-  -> TypeLib
-_introspect = __introspect (Proxy @(GQL a))
---introspection ::
---     forall a. IntrospectionRouter a (GQL a)
---  => a
---  -> Text
---introspection = routeIntrospection (Proxy @(GQL a))
--- Define GQL Kind Classes
--- Define and bind GQLObject
---class GQLObject a where
---  introspectObject :: a -> Text
---  default introspectObject :: (Show a, Eq a) =>
---    a -> Text
---  introspectObject x = concat ["Resolved Object :: ", pack (show x)]
---default introInput :: (GQLKind a, Selectors (Rep a) (Text, InputField)) =>
---    Proxy a -> TypeLib -> TypeLib
---  introInput = updateLib (inputObjectOf fields) stack
---    where
---      fieldTypes = getFields (Proxy @(Rep a))
---      stack = map snd fieldTypes
---      fields = map fst fieldTypes
--- type instance GQLConstraint a OBJECT = GQLObject a
---instance GQLObject a => IntrospectionRouter a OBJECT where
---  routeIntrospection _ = introspectObject
--- Define and bind GQLScalar
---class GQLScalar a where
---  introspectScalar :: a -> Text
---  default introspectScalar :: (Show a, Read a) =>
---    a -> Text
---  introspectScalar x = concat ["Resolved Scalar:: ", pack (show x)]
---type instance GQLConstraint a SCALAR = GQLScalar a
---instance GQLScalar a => IntrospectionRouter a SCALAR where
---  routeIntrospection _ = introspectScalar
