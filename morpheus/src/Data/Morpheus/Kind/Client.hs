a156ab9ca73fc2a5963972fd8981d5d5e3b6807b
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-04 02:35:10 +0200

    refactor with type families

--- morpheus/src/Data/Morpheus/Kind/Client.hs
+++ morpheus/src/Data/Morpheus/Kind/Client.hs
@@ -4,10 +4,10 @@
 
 module Data.Morpheus.Kind.Client where
 
-import           Data.Morpheus.Kind.Internal (GQL, GQLObject, OBJECT, SCALAR, introspection)
-import           Data.Text                   (Text)
-import qualified Data.Text                   as T (concat)
+--import           Data.Morpheus.Kind.Internal (GQL, GQLObject, OBJECT, SCALAR, introspection)
+import           Data.Text (Text)
 
+-- import qualified Data.Text                   as T (concat)
 -- Client Side
 -- define GQL Schema
 --newtype User =
@@ -17,11 +17,8 @@ import qualified Data.Text                   as T (concat)
 -- newtype Odd =
 --  Odd Int
 --  deriving (Show, Read, GQLScalar)
-
 -- type instance GQL Odd = SCALAR
-
 -- type instance GQL User = OBJECT
-
 -- Resolvers
 resolveTypes :: Text
 resolveTypes = ""
--- morpheus/src/Data/Morpheus/Kind/Internal.hs
+++ morpheus/src/Data/Morpheus/Kind/Internal.hs
@@ -1,18 +1,16 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Data.Morpheus.Kind.Internal
-  ( GQLObject
+  --GQLObject
  -- , GQLScalar
-  , introspection
-  , PRIMITIVE
+  --, introspection
+  ( PRIMITIVE
   , SCALAR
   , OBJECT
   , ENUM
@@ -20,17 +18,19 @@ module Data.Morpheus.Kind.Internal
   , GQL
   , _introspect
   , _decode
+  , _field
+  , _encode
   , GQLConstraint
   , IntrospectionRouter(..)
   ) where
 
 import Data.Morpheus.Schema.Internal.Types (TypeLib)
-import Data.Morpheus.Types.Error (Validation(..))
+import Data.Morpheus.Types.Error (ResolveIO, Validation)
 import Data.Morpheus.Types.JSType (JSType(..))
+import Data.Morpheus.Types.Query.Selection (Selection)
 import Data.Proxy (Proxy(..))
-import Data.Text (Text, concat, pack)
+import Data.Text (Text)
 import GHC.Exts (Constraint)
-import Prelude (Eq, Read, Show(..))
 
 -- Hidden GQL type System API
 type family GQL a :: *
@@ -45,13 +45,27 @@ data ENUM
 
 data INPUT_OBJECT
 
--- UNION : TODO: implement UNION
 type family GQLConstraint a b :: Constraint
 
 class IntrospectionRouter a b where
-  routeIntrospection :: Proxy b -> a -> Text
   __introspect :: Proxy b -> Proxy a -> TypeLib -> TypeLib
   __decode :: Proxy b -> JSType -> Validation a
+  __encode :: Proxy b -> (Text, Selection) -> a -> ResolveIO JSType
+  __field :: Proxy b -> Proxy a -> Text -> o
+
+_field ::
+     forall a. IntrospectionRouter a (GQL a)
+  => Proxy a
+  -> Text
+  -> o
+_field = __field (Proxy @(GQL a))
+
+_encode ::
+     forall a. IntrospectionRouter a (GQL a)
+  => (Text, Selection)
+  -> a
+  -> ResolveIO JSType
+_encode = __encode (Proxy @(GQL a))
 
 _decode ::
      forall a. IntrospectionRouter a (GQL a)
@@ -65,21 +79,18 @@ _introspect ::
   -> TypeLib
   -> TypeLib
 _introspect = __introspect (Proxy @(GQL a))
-
-introspection ::
-     forall a. IntrospectionRouter a (GQL a)
-  => a
-  -> Text
-introspection = routeIntrospection (Proxy @(GQL a))
-
+--introspection ::
+--     forall a. IntrospectionRouter a (GQL a)
+--  => a
+--  -> Text
+--introspection = routeIntrospection (Proxy @(GQL a))
 -- Define GQL Kind Classes
 -- Define and bind GQLObject
-class GQLObject a where
-  introspectObject :: a -> Text
-  default introspectObject :: (Show a, Eq a) =>
-    a -> Text
-  introspectObject x = concat ["Resolved Object :: ", pack (show x)]
-
+--class GQLObject a where
+--  introspectObject :: a -> Text
+--  default introspectObject :: (Show a, Eq a) =>
+--    a -> Text
+--  introspectObject x = concat ["Resolved Object :: ", pack (show x)]
 --default introInput :: (GQLKind a, Selectors (Rep a) (Text, InputField)) =>
 --    Proxy a -> TypeLib -> TypeLib
 --  introInput = updateLib (inputObjectOf fields) stack
@@ -87,19 +98,15 @@ class GQLObject a where
 --      fieldTypes = getFields (Proxy @(Rep a))
 --      stack = map snd fieldTypes
 --      fields = map fst fieldTypes
-type instance GQLConstraint a OBJECT = GQLObject a
-
-instance GQLObject a => IntrospectionRouter a OBJECT where
-  routeIntrospection _ = introspectObject
-
+-- type instance GQLConstraint a OBJECT = GQLObject a
+--instance GQLObject a => IntrospectionRouter a OBJECT where
+--  routeIntrospection _ = introspectObject
 -- Define and bind GQLScalar
 --class GQLScalar a where
 --  introspectScalar :: a -> Text
 --  default introspectScalar :: (Show a, Read a) =>
 --    a -> Text
 --  introspectScalar x = concat ["Resolved Scalar:: ", pack (show x)]
-
 --type instance GQLConstraint a SCALAR = GQLScalar a
-
 --instance GQLScalar a => IntrospectionRouter a SCALAR where
 --  routeIntrospection _ = introspectScalar
