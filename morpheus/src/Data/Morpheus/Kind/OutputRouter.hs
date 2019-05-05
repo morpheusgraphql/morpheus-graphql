cade270f971ae19b552712bbff96a9cf83d17d11
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-05 14:03:55 +0200

    define OutputRouter

--- morpheus/src/Data/Morpheus/Kind/OutputRouter.hs
+++ morpheus/src/Data/Morpheus/Kind/OutputRouter.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeApplications      #-}
 
 module Data.Morpheus.Kind.OutputRouter where
 
-import           Data.Morpheus.Kind.Internal (Encode_, GQL, Intro_, OField_)
-import           Data.Proxy                  (Proxy (..))
+import qualified Data.Morpheus.Kind.GQLEnum          as E (GQLEnum (..))
+import           Data.Morpheus.Kind.GQLKind          (GQLKind)
+import qualified Data.Morpheus.Kind.GQLPrimitive     as P (GQLPrimitive (..))
+import qualified Data.Morpheus.Kind.GQLScalar        as S (GQLScalar (..))
+import           Data.Morpheus.Kind.Internal         (ENUM, Encode_, GQL, Intro_, OField_, PRIMITIVE, SCALAR)
+import           Data.Morpheus.Schema.Internal.Types (ObjectField (..))
+import           Data.Morpheus.Types.JSType          (JSType (..), ScalarValue (..))
+import           Data.Proxy                          (Proxy (..))
+import           Data.Text                           (pack)
 
 class OutputTypeRouter a b where
   __introspect :: Proxy b -> Intro_ a
@@ -27,3 +35,18 @@ _encode ::
      forall a. OutputTypeRouter a (GQL a)
   => Encode_ a
 _encode = __encode (Proxy @(GQL a))
+
+instance (S.GQLScalar a, GQLKind a) => OutputTypeRouter a SCALAR where
+  __introspect _ _ = S.introspect (Proxy @a)
+  __encode _ _ = pure . S.encode
+  __objectField _ _ = ObjectField [] . S.asField (Proxy @a)
+
+instance (E.GQLEnum a, Show a, GQLKind a) => OutputTypeRouter a ENUM where
+  __introspect _ _ = E.introspect (Proxy @a)
+  __encode _ _ = pure . Scalar . String . pack . show
+  __objectField _ _ = ObjectField [] . E.asField (Proxy @a)
+
+instance (P.GQLPrimitive a, GQLKind a) => OutputTypeRouter a PRIMITIVE where
+  __introspect _ = P.introspect'
+  __encode _ = P.encode'
+  __objectField _ = P.objectField'
