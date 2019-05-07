3f20e9c38f72e41477644a0dc7b852cf64e16c73
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-07 17:48:46 +0200

    add tests

--- morpheus/test/Lib.hs
+++ morpheus/test/Lib.hs
@@ -7,13 +7,16 @@ import           Data.Text    (Text, unpack)
 import qualified Data.Text.IO as TIO (readFile)
 
 path :: String -> String
-path name = "test/" ++ name ++ ".gql"
+path name = "test/lib/" ++ name ++ ".gql"
 
 gqlLib :: String -> String
 gqlLib x = path x ++ ".gql"
+
+resLib :: String -> String
+resLib x = path x ++ ".res.json"
 
 getGQLBody :: Text -> IO Text
 getGQLBody p = TIO.readFile (gqlLib $ unpack p)
 
 getResponseBody :: Text -> IO Text
-getResponseBody p = TIO.readFile (gqlLib $ unpack p)
+getResponseBody p = TIO.readFile (resLib $ unpack p)
--- morpheus/test/Spec.hs
+++ morpheus/test/Spec.hs
@@ -7,38 +7,26 @@ module Main
 import           Data.Text        (Text, pack, unpack)
 import           Lib              (getGQLBody, getResponseBody)
 import           Test.Tasty       (TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit (Assertion, testCase, (@=?))
+import           Test.Tasty.HUnit (testCase, (@=?))
 import           TestAPI          (api)
 
 packGQLRequest :: Text -> Text
 packGQLRequest x = pack $ "{\"query\":" ++ show x ++ "}"
 
-test1 :: [Assertion]
-test1 = ["ac" @=? "a" ++ "c"]
+unknownArguments :: IO TestTree
+unknownArguments = testByFiles "looping Fragment should throw error" "unknownArguments"
 
-test2 :: [Assertion]
-test2 = ["ab" @=? "a" ++ "b"]
-
-helloWorld :: IO [TestTree]
-helloWorld = do
-  response' <- api $ packGQLRequest "{ user { name  } }"
-  return [testCase "anonymus Query" ("{\"data\":{\"user\":{\"name\":\"\"}}}" @=? response')]
-
-testFragmentLoop :: IO [TestTree]
+testFragmentLoop :: IO TestTree
 testFragmentLoop = testByFiles "looping Fragment should throw error" "loopingFragment"
 
-testByFiles :: Text -> Text -> IO [TestTree]
+testByFiles :: Text -> Text -> IO TestTree
 testByFiles description' fileName' = do
   query' <- getGQLBody fileName'
   response' <- getResponseBody fileName'
   result' <- api $ packGQLRequest query'
-  return [testCase (unpack description') $ result' @=? response']
-
-tests :: TestTree
-tests = testGroup "Tests" [testGroup "just tests " $ testCase "-" <$> test1 ++ test2]
+  return $ testCase (unpack description') $ result' @=? response'
 
 main :: IO ()
 main = do
-  iot <- helloWorld
-  fragment <- testFragmentLoop
-  defaultMain (testGroup "tests" $ tests : iot ++ fragment)
+  ioTests <- sequence [unknownArguments, testFragmentLoop]
+  defaultMain (testGroup "Integration Tests" ioTests)
