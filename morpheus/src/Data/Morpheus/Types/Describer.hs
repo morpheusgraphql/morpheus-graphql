b601d0c6c733b539825cc93a2764839b15f874ac
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-07 02:54:43 +0200

    add Monad Resolvers

--- morpheus/src/Data/Morpheus/Types/Describer.hs
+++ morpheus/src/Data/Morpheus/Types/Describer.hs
@@ -10,3 +10,29 @@ import           GHC.Generics (Generic)
 newtype a ::-> b =
   Resolver (a -> IO (Either String b))
   deriving (Generic)
+
+instance Functor ((::->) p) where
+  fmap func (Resolver resolver) =
+    Resolver $ \args -> do
+      value <- resolver args
+      return (func <$> value)
+
+instance Applicative ((::->) p) where
+  pure = Resolver . const . return . pure
+  Resolver func <*> Resolver resolver =
+    Resolver $ \args -> do
+      func1 <- func args
+      value1 <- resolver args
+      return (func1 <*> value1)
+
+instance Monad ((::->) p) where
+  return = pure
+  (Resolver func1) >>= func2 =
+    Resolver $ \args -> do
+      value1 <- func1 args
+      case value1 of
+        Left x -> return $ Left x
+        Right y {--}
+         -> do
+          let (Resolver x) = func2 y
+          x args
