6006dc14f0019bc4713bcca9b6a8b261c0aec829
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-07 16:59:29 +0200

    add hello world test

--- morpheus/test/Spec.hs
+++ morpheus/test/Spec.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main
   ( main
   ) where
 
+import           Data.Text
 import           Test.Tasty       (TestTree, defaultMain, testGroup)
 import           Test.Tasty.HUnit (Assertion, testCase, (@=?))
+import           TestAPI          (api)
+
+packGQLRequest :: Text -> Text
+packGQLRequest _ = "{\"query\":\"{ user { name  } }\"}"
 
 test1 :: [Assertion]
 test1 = ["ac" @=? "a" ++ "c"]
@@ -12,13 +19,14 @@ test2 :: [Assertion]
 test2 = ["ab" @=? "a" ++ "b"]
 
 ioTests :: IO [TestTree]
-ioTests = return [testCase "" ("ab" @=? "a" ++ "b")]
+ioTests = do
+  response' <- api $ packGQLRequest "hello world Test"
+  return [testCase "" ("{\"data\":{\"user\":{\"name\":\"\"}}}" @=? response')]
 
 tests :: TestTree
-tests = testGroup "unit" [testGroup "Nullary constructors" $ fmap (testCase "-") (test1 ++ test2)]
+tests = testGroup "Tests" [testGroup "just tests " $ testCase "-" <$> test1 ++ test2]
 
 main :: IO ()
 main = do
   iot <- ioTests
-  let allTests = tests : iot
-  defaultMain (testGroup "tests" allTests)
+  defaultMain (testGroup "tests" $ tests : iot)
