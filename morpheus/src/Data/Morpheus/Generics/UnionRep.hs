7495e4d80be0200409afb4c1ba55c6c54678a618
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-11 13:56:45 +0200

    add CurrentType

--- morpheus/src/Data/Morpheus/Generics/UnionRep.hs
+++ morpheus/src/Data/Morpheus/Generics/UnionRep.hs
@@ -13,20 +13,23 @@ module Data.Morpheus.Generics.UnionRep
   ) where
 
 import           Data.Morpheus.Schema.Internal.AST (Field, TypeLib)
-import           Data.Proxy                          (Proxy (..))
+import           Data.Proxy                        (Proxy (..))
+import           Data.Text                         (Text)
 import           GHC.Generics
 
 class UnionRep f where
   possibleTypes :: Proxy f -> [(Field, TypeLib -> TypeLib)]
+  currentType :: f a -> (Text, ())
 
 instance UnionRep f => UnionRep (M1 D x f) where
   possibleTypes _ = possibleTypes (Proxy @f)
+  currentType (M1 x) = currentType x
 
 instance UnionRep f => UnionRep (M1 C x f) where
   possibleTypes _ = possibleTypes (Proxy @f)
-
-instance UnionRep U1 where
-  possibleTypes _ = []
+  currentType (M1 x) = currentType x
 
 instance (UnionRep a, UnionRep b) => UnionRep (a :+: b) where
   possibleTypes _ = possibleTypes (Proxy @a) ++ possibleTypes (Proxy @b)
+  currentType (L1 x) = currentType x
+  currentType (R1 x) = currentType x
