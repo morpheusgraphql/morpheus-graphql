7323b5642496b3637d6776c719787a9241206397
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-11 23:16:59 +0200

    __typename field returns current type on union selection

--- morpheus/src/Data/Morpheus/Kind/GQLObject.hs
+++ morpheus/src/Data/Morpheus/Kind/GQLObject.hs
@@ -17,7 +17,7 @@ module Data.Morpheus.Kind.GQLObject
   ) where
 
 import           Data.Morpheus.Error.Selection          (subfieldsNotSelected)
-import           Data.Morpheus.Generics.DeriveResolvers (DeriveResolvers (..), resolveBySelection)
+import           Data.Morpheus.Generics.DeriveResolvers (DeriveResolvers, resolveBySelection, resolversBy)
 import           Data.Morpheus.Generics.ObjectRep       (ObjectRep (..))
 import           Data.Morpheus.Kind.GQLType             (GQLType (..), asObjectType)
 import           Data.Morpheus.Kind.Internal            (KIND, OBJECT)
@@ -27,7 +27,7 @@ import           Data.Morpheus.Schema.Internal.AST      (ObjectField (..), TypeL
 import           Data.Morpheus.Schema.Schema            (Schema)
 import           Data.Morpheus.Schema.Utils.Utils       (Field, InputValue, Type)
 import           Data.Morpheus.Types.Error              (ResolveIO, failResolveIO)
-import           Data.Morpheus.Types.JSType             (JSType (..))
+import           Data.Morpheus.Types.JSType             (JSType (..), ScalarValue (..))
 import           Data.Morpheus.Types.Query.Selection    (Selection (..))
 import           Data.Proxy
 import           Data.Text                              (Text)
@@ -35,20 +35,24 @@ import           GHC.Generics
 
 type ObjectConstraint a = (Generic a, DeriveResolvers (Rep a), ObjectRep (Rep a) (Text, ObjectField), GQLType a)
 
-encode :: (Generic a, DeriveResolvers (Rep a)) => (Text, Selection) -> a -> ResolveIO JSType
-encode (_, SelectionSet _ selection _pos) = resolveBySelection selection . deriveResolvers "" . from
-encode (_, Field _ key pos)               = const $ failResolveIO $ subfieldsNotSelected key "" pos -- TODO: must be internal Error
+encode ::
+     forall a. (GQLType a, Generic a, DeriveResolvers (Rep a))
+  => (Text, Selection)
+  -> a
+  -> ResolveIO JSType
+encode (_, SelectionSet _ selection _pos) value = resolveBySelection selection (resolversBy value ++ [__typename])
+  where
+    __typename = ("__typename", \_ -> return $ Scalar $ String (typeID (Proxy @a)))
+encode (_, Field _ key pos) _ = failResolveIO $ subfieldsNotSelected key "" pos -- TODO: must be internal Error
 
 introspect ::
      forall a. (ObjectRep (Rep a) (Text, ObjectField), GQLType a)
   => Proxy a
   -> TypeLib
   -> TypeLib
-introspect = updateLib (asObjectType fields) stack
+introspect = updateLib (asObjectType fields') stack'
   where
-    fieldTypes = getFields (Proxy @(Rep a))
-    fields = map fst fieldTypes
-    stack = map snd fieldTypes
+    (fields', stack') = unzip $ getFields (Proxy @(Rep a))
 
 type instance KIND EnumValue = OBJECT
 
