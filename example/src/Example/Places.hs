1110a86424aca1d34a10abc27932c05ea96a3866
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-13 09:38:10 +0200

    Add Places

---
+++ example/src/Example/Places.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE TypeFamilies   #-}
+
+module Example.Places
+  ( Places
+  ) where
+
+import           Data.Morpheus.Kind (ENUM, GQLType, KIND)
+import           GHC.Generics       (Generic)
+
+type instance KIND Places = ENUM
+
+data Places
+  = Olympus
+  | Athens
+  | Colchis
+  | Underworld
+  deriving (Generic, GQLType)
