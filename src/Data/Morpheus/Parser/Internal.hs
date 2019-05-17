5923c3fbd1fc16699c62e5ca9a49cfa3849f0e5e
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-17 22:44:43 +0200

    better syntax validation for ending

--- src/Data/Morpheus/Parser/Internal.hs
+++ src/Data/Morpheus/Parser/Internal.hs
@@ -1,10 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.Morpheus.Parser.Internal
   ( GQLSyntax(..)
+  , endParsing
   ) where
 
-import           Data.Text (Text)
+import           Control.Applicative
+import           Data.Attoparsec.Text           (Parser, endOfInput, skipSpace)
+import           Data.Morpheus.Parser.Primitive (getPosition)
+import           Data.Text                      (Text)
 
 data GQLSyntax a
-  = Invalid Int
-            Text
+  = Invalid Text
+            Int
   | Valid a
+
+endParsing :: a -> Parser (GQLSyntax a)
+endParsing x = do
+  skipSpace
+  position' <- getPosition
+  (endOfInput >> return (Valid x)) <|> return (Invalid "Could not read after the Position" position')
--- src/Data/Morpheus/Parser/Parser.hs
+++ src/Data/Morpheus/Parser/Parser.hs
@@ -9,6 +9,7 @@ import           Data.Map                          (fromList, toList)
 import           Data.Maybe                        (maybe)
 import           Data.Morpheus.Error.Syntax        (syntaxError)
 import           Data.Morpheus.Parser.Fragment     (fragment)
+import           Data.Morpheus.Parser.Internal     (GQLSyntax (..), endParsing)
 import qualified Data.Morpheus.Parser.Mutation     as M
 import           Data.Morpheus.Parser.Primitive    (getLines)
 import qualified Data.Morpheus.Parser.Query        as Q
@@ -23,15 +24,13 @@ request :: Parser GQLQueryRoot
 request = do
   queryValue <- Q.query <|> M.mutation <|> S.subscription
   fragmentLib <- fromList <$> many fragment
-  skipSpace
-  endOfInput
   pure GQLQueryRoot {queryBody = queryValue, fragments = fragmentLib, inputVariables = []}
 
 getVariables :: GQLRequest -> [(Text, JSType)]
 getVariables request' = maybe [] toList (variables request')
 
-parseReq :: GQLRequest -> Either String GQLQueryRoot
-parseReq requestBody = parseOnly request $ query requestBody
+parseReq :: GQLRequest -> Either String (GQLSyntax GQLQueryRoot)
+parseReq requestBody = parseOnly (request >>= endParsing) $ query requestBody
 
 parseLineBreaks :: GQLRequest -> [Int]
 parseLineBreaks requestBody =
@@ -42,5 +41,6 @@ parseLineBreaks requestBody =
 parseGQL :: GQLRequest -> Validation GQLQueryRoot
 parseGQL requestBody =
   case parseReq requestBody of
-    Right root      -> Right $ root {inputVariables = getVariables requestBody}
-    Left parseError -> Left $ syntaxError (pack $ show parseError) 0
+    Right (Valid root)         -> Right $ root {inputVariables = getVariables requestBody}
+    Right (Invalid text index) -> Left $ syntaxError text index
+    Left parseError            -> Left $ syntaxError (pack $ show parseError) 0
