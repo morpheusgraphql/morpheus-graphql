305faebe48ad8ed0aa7447df5aaba0e3483684cd
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-17 22:56:54 +0200

    add gqlSyntax

--- src/Data/Morpheus/Parser/Internal.hs
+++ src/Data/Morpheus/Parser/Internal.hs
@@ -3,17 +3,24 @@
 module Data.Morpheus.Parser.Internal
   ( GQLSyntax(..)
   , endParsing
+  , gqlSyntax
   ) where
 
 import           Control.Applicative
 import           Data.Attoparsec.Text           (Parser, endOfInput, skipSpace)
 import           Data.Morpheus.Parser.Primitive (getPosition)
 import           Data.Text                      (Text)
+import qualified Data.Text                      as T (concat)
 
 data GQLSyntax a
   = Invalid Text
             Int
   | Valid a
+
+gqlSyntax :: Parser a -> Text -> Parser (GQLSyntax a)
+gqlSyntax parser message' = do
+  position' <- getPosition
+  Valid <$> parser <|> return (Invalid (T.concat ["GQL Syntax:", message']) position')
 
 endParsing :: a -> Parser (GQLSyntax a)
 endParsing x = do
