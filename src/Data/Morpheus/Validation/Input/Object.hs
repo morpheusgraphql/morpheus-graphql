89076153f2a018f3dcbfdbaf5da7e8bdc4a7d4f4
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-19 21:55:51 +0200

    input validation

--- src/Data/Morpheus/Validation/Input/Object.hs
+++ src/Data/Morpheus/Validation/Input/Object.hs
@@ -5,107 +5,65 @@ module Data.Morpheus.Validation.Input.Object
   ) where
 
 import           Data.Morpheus.Error.Input            (InputError (..), InputValidation, Prop (..))
-import           Data.Morpheus.Schema.Internal.AST    (Core (..), Field (..), GObject (..), InputField (..),
-                                                       InputObject, InputType, Leaf (..), TypeLib (..), showFullAstType,
-                                                       showWrappedType)
+import           Data.Morpheus.Schema.Internal.AST    (Core (..), Field (..), GObject (..), InputField (..), InputType,
+                                                       TypeLib (..), showFullAstType)
 import qualified Data.Morpheus.Schema.Internal.AST    as T (InternalType (..))
 import           Data.Morpheus.Types.JSType           (JSType (..), ScalarValue (..))
 import           Data.Morpheus.Types.Query.Operator   (TypeWrapper (..))
 import           Data.Morpheus.Validation.Input.Enum  (validateEnum)
-import           Data.Morpheus.Validation.Utils.Utils (lookupField, lookupType)
+import           Data.Morpheus.Validation.Utils.Utils (getInputType, lookupField)
 import           Data.Text                            (Text)
 
-generateError :: JSType -> Text -> [Prop] -> InputError
-generateError jsType expected' path' = UnexpectedType path' expected' jsType
-
-existsInputObjectType :: InputError -> TypeLib -> Text -> InputValidation InputObject
-existsInputObjectType error' lib' = lookupType error' (inputObject lib')
-
-existsLeafType :: InputError -> TypeLib -> Text -> InputValidation Leaf
-existsLeafType error' lib' = lookupType error' (leaf lib')
+typeMismatch :: JSType -> Text -> [Prop] -> InputError
+typeMismatch jsType expected' path' = UnexpectedType path' expected' jsType
 
 validateScalarTypes :: Text -> ScalarValue -> [Prop] -> InputValidation ScalarValue
 validateScalarTypes "String" (String x)   = pure . const (String x)
-validateScalarTypes "String" scalar       = Left . generateError (Scalar scalar) "String"
+validateScalarTypes "String" scalar       = Left . typeMismatch (Scalar scalar) "String"
 validateScalarTypes "Int" (Int x)         = pure . const (Int x)
-validateScalarTypes "Int" scalar          = Left . generateError (Scalar scalar) "Int"
+validateScalarTypes "Int" scalar          = Left . typeMismatch (Scalar scalar) "Int"
 validateScalarTypes "Boolean" (Boolean x) = pure . const (Boolean x)
-validateScalarTypes "Boolean" scalar      = Left . generateError (Scalar scalar) "Boolean"
+validateScalarTypes "Boolean" scalar      = Left . typeMismatch (Scalar scalar) "Boolean"
 validateScalarTypes _ scalar              = pure . const scalar
-
-validateLeaf :: Leaf -> JSType -> [Prop] -> InputValidation JSType
-validateLeaf (LEnum tags core) jsType props      = validateEnum (UnexpectedType props (name core) jsType) tags jsType
-validateLeaf (LScalar core) (Scalar found) props = Scalar <$> validateScalarTypes (name core) found props
-validateLeaf (LScalar core) jsType props         = Left $ generateError jsType (name core) props
-
-isNullableType :: [TypeWrapper] -> Bool
-isNullableType (NonNullType:_) = False
-isNullableType _               = True
-
-unwrapped :: [TypeWrapper] -> Bool
-unwrapped []            = True
-unwrapped [NonNullType] = True
-unwrapped _             = False
-
-validateI :: [Prop] -> TypeLib -> GObject InputField -> (Text, JSType) -> InputValidation (Text, JSType)
-validateI prop' lib' parent'@(GObject fields' _) (_name, value') = do
-  wrappers' <- fieldTypeWrappers . unpackInputField <$> lookupField _name fields' (UnknownField prop' _name)
-  validateInputObject prop' lib' wrappers' parent' (_name, value')
-
-validateInputObject ::
-     [Prop] -> TypeLib -> [TypeWrapper] -> GObject InputField -> (Text, JSType) -> InputValidation (Text, JSType)
-validateInputObject prop' lib' (ListType:wrappers') (GObject parentFields pos) (_name, JSList list') =
-  mapM_ recValidate list' >> pure (_name, JSList list')
-  where
-    recValidate x = validateInputObject prop' lib' wrappers' (GObject parentFields pos) (_name, x)
-validateInputObject prop' lib' wrappers' (GObject parentFields _) (_name, value') = do
-  field' <- getField
-  case value' of
-    JSNull
-      | isNullableType wrappers' -> return (_name, value')
-    JSObject fields
-      | unwrapped wrappers' -> mapM_ recVal fields >> return (_name, value')
-      where recVal v' = do
-              (fieldTypeName', currentProp, error') <- validationData (JSObject fields)
-              inputObject' <- existsInputObjectType error' lib' fieldTypeName'
-              validateI currentProp lib' inputObject' v'
-    leafValue'
-      | unwrapped wrappers' -> do
-        (fieldTypeName', currentProp, error') <- validationData leafValue'
-        leafType' <- existsLeafType error' lib' fieldTypeName'
-        validateLeaf leafType' leafValue' currentProp >> pure (_name, leafValue')
-    invalidValue' -> Left $ UnexpectedType prop' (showWrappedType wrappers' $ fieldType field') invalidValue'
-  where
-    validationData x = do
-      fieldTypeName' <- fieldType <$> getField
-      let currentProp = prop' ++ [Prop _name fieldTypeName']
-      let inputError = generateError x fieldTypeName' currentProp
-      return (fieldTypeName', currentProp, inputError)
-    getField = unpackInputField <$> lookupField _name parentFields (UnknownField prop' _name)
 
 -- Validate Variable Argument or all Possible input Values
 validateInputValue :: TypeLib -> [Prop] -> [TypeWrapper] -> InputType -> (Text, JSType) -> InputValidation JSType
-validateInputValue lib' prop' = validateInput
-    -- handle nullable fields
+validateInputValue lib' prop' = validate
   where
     throwError wrappers' type' value' = Left $ UnexpectedType prop' (showFullAstType wrappers' type') value'
-    {--
-      actual validation
-    --}
-    validateInput :: [TypeWrapper] -> InputType -> (Text, JSType) -> InputValidation JSType
-    validateInput (NonNullType:wrappers') type' (_, JSNull) = throwError wrappers' type' JSNull
-    validateInput _ _ (_, JSNull) = return JSNull
+    {-- VALIDATION --}
+    {-- 1. VALIDATE WRAPPERS -}
+    validate :: [TypeWrapper] -> InputType -> (Text, JSType) -> InputValidation JSType
+    -- throw error on not nullable type if value = null
+    validate (NonNullType:wrappers') type' (_, JSNull) = throwError wrappers' type' JSNull
+    -- resolves nullable value as null
+    validate _ _ (_, JSNull) = return JSNull
     -- ignores NonNUllTypes if value /= null
-    validateInput (NonNullType:wrappers') type' value' = validateInputValue lib' prop' wrappers' type' value'
-    -- recursively validates array
-    validateInput (ListType:wrappers') type' (key', JSList list') = JSList <$> mapM listCheck list'
+    validate (NonNullType:wrappers') type' value' = validateInputValue lib' prop' wrappers' type' value'
+    {-- VALIDATE LIST -}
+    validate (ListType:wrappers') type' (key', JSList list') = JSList <$> mapM listCheck list'
       where
         listCheck element' = validateInputValue lib' prop' wrappers' type' (key', element')
-    {--
-      validates concrete types
-     when all wrapper validation are done --}
-    validateInput [] (T.Object type') (_, JSObject fields) = JSObject <$> mapM (validateI prop' lib' type') fields
-    validateInput [] (T.Scalar core) (_, jsValue) = validateLeaf (LScalar core) jsValue prop'
-    validateInput [] (T.Enum tags core) (_, jsValue) = validateLeaf (LEnum tags core) jsValue prop'
-    -- throw error on invalid values
-    validateInput wrappers' type' (_, value') = throwError wrappers' type' value'
+    {-- 2. VALIDATE TYPES, all wrappers are already Processed --}
+    {-- VALIDATE OBJECT--}
+    validate [] (T.Object (GObject parentFields' _)) (_, JSObject fields) = JSObject <$> mapM validateObject fields
+      where
+        validateObject (_name, value') = do
+          (fieldTypeName', currentProp', error') <- validationData value'
+          type' <- getInputType fieldTypeName' lib' error'
+          wrappers' <- fieldTypeWrappers <$> getField
+          value'' <- validateInputValue lib' currentProp' wrappers' type' (_name, value')
+          return (_name, value'')
+          where
+            validationData x = do
+              fieldTypeName' <- fieldType <$> getField
+              let currentProp = prop' ++ [Prop _name fieldTypeName']
+              let inputError = typeMismatch x fieldTypeName' currentProp
+              return (fieldTypeName', currentProp, inputError)
+            getField = unpackInputField <$> lookupField _name parentFields' (UnknownField prop' _name)
+    {-- VALIDATE SCALAR --}
+    validate [] (T.Enum tags core) (_, value') = validateEnum (UnexpectedType prop' (name core) value') tags value'
+    {-- VALIDATE ENUM --}
+    validate [] (T.Scalar core) (_, Scalar value') = Scalar <$> validateScalarTypes (name core) value' prop'
+    {-- 3. THROW ERROR: on invalid values --}
+    validate wrappers' type' (_, value') = throwError wrappers' type' value'
