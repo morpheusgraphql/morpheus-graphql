c024f4be8692b94c4d5d83c18788d1fafea8c2f7
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-20 09:39:24 +0200

    add Scalar Type ID

--- src/Data/Morpheus/Types/ID.hs
+++ src/Data/Morpheus/Types/ID.hs
@@ -1,18 +1,28 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
-{-# LANGUAGE TypeFamilies   #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
 
 module Data.Morpheus.Types.ID
   ( ID(..)
   ) where
 
-import           Data.Morpheus.Kind.GQLType  (GQLType)
-import           Data.Morpheus.Kind.Internal (KIND, SCALAR)
-import           Data.Text                   (Text)
-import           GHC.Generics                (Generic)
+import           Data.Morpheus.Error.Internal (internalTypeMismatch)
+import           Data.Morpheus.Kind.GQLScalar (GQLScalar (..))
+import           Data.Morpheus.Kind.GQLType   (GQLType)
+import           Data.Morpheus.Kind.Internal  (KIND, SCALAR)
+import           Data.Morpheus.Types.JSType   (JSType (..), ScalarValue (..))
+import           Data.Text                    (Text, pack)
+import           GHC.Generics                 (Generic)
 
 type instance KIND ID = SCALAR
 
 newtype ID = ID
   { unpackID :: Text
   } deriving (Generic, GQLType)
+
+instance GQLScalar ID where
+  parseValue (Int x)    = return (ID $ pack $ show x)
+  parseValue (String x) = return (ID x)
+  parseValue value'     = internalTypeMismatch "ID" (Scalar value')
+  serialize (ID x) = String x
