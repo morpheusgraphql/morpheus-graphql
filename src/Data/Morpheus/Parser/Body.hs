080961e667d281a1fe5305dba3b793e94d9a28c2
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-24 14:34:30 +0200

    basic support of alias parsing

--- src/Data/Morpheus/Parser/Body.hs
+++ src/Data/Morpheus/Parser/Body.hs
@@ -8,7 +8,7 @@ module Data.Morpheus.Parser.Body
 import           Control.Applicative                           ((<|>))
 import           Data.Attoparsec.Text                          (Parser, char, sepBy, skipSpace, try)
 import           Data.Morpheus.Parser.Arguments                (arguments)
-import           Data.Morpheus.Parser.Primitive                (getPosition, separator, token)
+import           Data.Morpheus.Parser.Primitive                (getPosition, qualifier, separator, token)
 import           Data.Morpheus.Parser.Terms                    (onType, spreadLiteral)
 import           Data.Morpheus.Types.Internal.AST.RawSelection (Fragment (..), RawArguments, RawSelection (..),
                                                                 RawSelection' (..), RawSelectionSet, Reference (..))
@@ -44,6 +44,19 @@ entry = do
        RawSelection' {rawSelectionArguments = args, rawSelectionRec = (), rawSelectionPosition = index})
   return (key, value)
 
+{--
+  {
+    bigImage: image(320)
+    smallImage:  image(640)
+  }
+--}
+alias :: Parser (Text, RawSelection)
+alias = do
+  (name_, position_) <- qualifier
+  _ <- char ':'
+  value' <- entry
+  return value'
+
 separated :: Parser a -> Parser [a]
 separated x = x `sepBy` separator
 
@@ -51,7 +64,7 @@ entries :: Parser RawSelectionSet
 entries = do
   _ <- char '{'
   skipSpace
-  entries' <- separated (entry <|> inlineFragment <|> spread)
+  entries' <- separated (alias <|> inlineFragment <|> spread <|> entry)
   skipSpace
   _ <- char '}'
   return entries'
