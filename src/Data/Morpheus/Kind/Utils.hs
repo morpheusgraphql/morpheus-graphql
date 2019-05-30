d55479a19ed42a4e66205e7ae0c58b543b3c3c77
nalchevanidze <d.nalchevanidze@gmail.com>
2019-05-30 03:28:50 +0200

    fix utils

--- src/Data/Morpheus/Kind/Utils.hs
+++ src/Data/Morpheus/Kind/Utils.hs
@@ -9,6 +9,7 @@ import           Data.Morpheus.Kind.GQLKinds                (Encode_)
 import           Data.Morpheus.Types.Internal.AST.Selection (Selection (..), SelectionRec (..))
 import           Data.Morpheus.Types.Internal.Data          (DataField (..), DataTypeWrapper (..))
 import           Data.Morpheus.Types.Internal.Value         (Value (..))
+import           Data.Morpheus.Types.Resolver               (Result (..))
 
 maybeField :: DataField a -> DataField a
 maybeField field@DataField {fieldTypeWrappers = NonNullType:xs} = field {fieldTypeWrappers = xs}
@@ -17,12 +18,12 @@ maybeField field                                                = field
 listField :: DataField a -> DataField a
 listField x = x {fieldTypeWrappers = [NonNullType, ListType] ++ fieldTypeWrappers x}
 
-encodeList :: Encode_ a c -> Encode_ [a] c
-encodeList _ (_, Selection {selectionRec = SelectionField {}}) _ = pure (List [], [])
+encodeList :: Encode_ a -> Encode_ [a]
+encodeList _ (_, Selection {selectionRec = SelectionField {}}) _ = pure $ pure (List [])
 encodeList f query list = do
   value' <- mapM (f query) list
-  return (List (map fst value'), concatMap snd value')
+  return $ Result (List (map resultValue value')) (concatMap resultEffects value')
 
-encodeMaybe :: Encode_ a c -> Encode_ (Maybe a) c
-encodeMaybe _ _ Nothing          = pure (Null, [])
+encodeMaybe :: Encode_ a -> Encode_ (Maybe a)
+encodeMaybe _ _ Nothing          = pure $ pure Null
 encodeMaybe f query (Just value) = f query value
