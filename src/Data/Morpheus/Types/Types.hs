91060fe1b8b1a09fec584bb63b1ec66b024b719f
nalchevanidze <d.nalchevanidze@gmail.com>
2019-06-27 01:15:32 +0200

    cleanup Types

--- src/Data/Morpheus/Types/Types.hs
+++ src/Data/Morpheus/Types/Types.hs
@@ -2,24 +2,15 @@ module Data.Morpheus.Types.Types
   ( GQLQueryRoot(..)
   , Variables
   , GQLRootResolver(..)
-  , SubscriptionResolver(..)
   ) where
 
 import           Data.Map                                      (Map)
 import           Data.Morpheus.Types.Internal.AST.Operator     (RawOperator)
 import           Data.Morpheus.Types.Internal.AST.RawSelection (FragmentLib)
 import           Data.Morpheus.Types.Internal.Base             (Key)
-import           Data.Morpheus.Types.Internal.Validation       (ResolveIO)
 import           Data.Morpheus.Types.Internal.Value            (Value)
 
 type Variables = Map Key Value
-
-newtype SubscriptionResolver = SubscriptionResolver
-  { unpackSubscriptionResolver :: () -> ResolveIO Value
-  }
-
-instance Show SubscriptionResolver where
-  show = const "SubscriptionResolver"
 
 data GQLQueryRoot = GQLQueryRoot
   { fragments      :: FragmentLib
