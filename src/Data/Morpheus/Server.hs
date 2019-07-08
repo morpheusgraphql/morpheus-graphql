74673f704a41fc17f054e3e3a3ce43e425f43b98
nalchevanidze <d.nalchevanidze@gmail.com>
2019-07-08 13:13:53 +0200

    ws accepts both apollo protocol

--- src/Data/Morpheus/Server.hs
+++ src/Data/Morpheus/Server.hs
@@ -16,15 +16,15 @@ import           Control.Monad                          (forever)
 import           Data.Aeson                             (encode)
 import           Data.ByteString.Lazy.Char8             (ByteString)
 import           Data.Morpheus.Resolve.Resolve          (RootResCon, streamResolver)
-import           Data.Morpheus.Server.Apollo            (ApolloAction (..), apolloProtocol, oldApolloFormat)
+import           Data.Morpheus.Server.Apollo            (ApolloAction (..), acceptApolloSubProtocol, oldApolloFormat)
 import           Data.Morpheus.Server.ClientRegister    (GQLState, addClientSubscription, connectClient,
                                                          disconnectClient, initGQLState, publishUpdates,
                                                          removeClientSubscription)
 import           Data.Morpheus.Types.Internal.Stream    (ResponseEvent (..), ResponseStream, closeStream)
 import           Data.Morpheus.Types.Internal.WebSocket (GQLClient (..))
 import           Data.Morpheus.Types.Resolver           (GQLRootResolver (..))
-import           Network.WebSockets                     (ServerApp, acceptRequestWith, forkPingThread, receiveData,
-                                                         sendTextData)
+import           Network.WebSockets                     (ServerApp, acceptRequestWith, forkPingThread, pendingRequest,
+                                                         receiveData, sendTextData)
 
 handleGQLResponse :: Eq s => GQLClient IO s -> GQLState IO s -> Int -> ResponseStream IO s ByteString -> IO ()
 handleGQLResponse GQLClient {clientConnection, clientID} state sessionId stream = do
@@ -38,7 +38,8 @@ handleGQLResponse GQLClient {clientConnection, clientID} state sessionId stream 
 -- | Wai WebSocket Server App for GraphQL subscriptions
 gqlSocketApp :: RootResCon IO s a b c => GQLRootResolver IO s a b c -> GQLState IO s -> ServerApp
 gqlSocketApp gqlRoot state pending = do
-  connection' <- acceptRequestWith pending apolloProtocol
+  let acceptHead = acceptApolloSubProtocol (pendingRequest pending)
+  connection' <- acceptRequestWith pending acceptHead
   forkPingThread connection' 30
   client' <- connectClient connection' state
   finally (queryHandler client') (disconnectClient client' state)
