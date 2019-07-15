f89031e4305df5363947979de6eb3aa3820fe358
nalchevanidze <d.nalchevanidze@gmail.com>
2019-07-15 03:44:21 +0200

    fix test

--- test/Feature/WrappedTypeName/API.hs
+++ test/Feature/WrappedTypeName/API.hs
@@ -11,7 +11,7 @@ module Feature.WrappedTypeName.API
 import           Data.ByteString.Lazy.Char8 (ByteString)
 import           Data.Morpheus              (interpreter)
 import           Data.Morpheus.Kind         (KIND, OBJECT)
-import           Data.Morpheus.Types        (GQLRootResolver (..), GQLType (..), ResM, StreamM)
+import           Data.Morpheus.Types        (EventContent, GQLRootResolver (..), GQLType (..), ResM, StreamM)
 import           Data.Text                  (Text)
 import           GHC.Generics               (Generic)
 
@@ -36,23 +36,35 @@ data Query = Query
   } deriving (Generic)
 
 data Mutation = Mutation
-  { mut1 :: Maybe (WA (StreamM ()))
+  { mut1 :: Maybe (WA (StreamM EVENT))
   , mut2 :: Maybe (Wrapped Int Int)
   , mut3 :: Maybe (Wrapped (Wrapped Text Int) Text)
   } deriving (Generic)
 
+data EVENT =
+  EVENT
+  deriving (Show, Eq)
+
+data instance  EventContent EVENT = Content
+
 data Subscription = Subscription
-  { sub1 :: Maybe (WA ResM)
-  , sub2 :: Maybe (Wrapped Int Int)
-  , sub3 :: Maybe (Wrapped (Wrapped Text Int) Text)
+  { sub1 :: () -> ([EVENT], EventContent EVENT -> ResM (Maybe (WA ResM)))
+  , sub2 :: () -> ([EVENT], EventContent EVENT -> ResM (Maybe (Wrapped Int Int)))
+  , sub3 :: () -> ([EVENT], EventContent EVENT -> ResM (Maybe (Wrapped (Wrapped Text Int) Text)))
   } deriving (Generic)
 
-rootResolver :: GQLRootResolver IO () Query Mutation Subscription
+rootResolver :: GQLRootResolver IO EVENT Query Mutation Subscription
 rootResolver =
   GQLRootResolver
     { queryResolver = return Query {a1 = WA {aText = const $ pure "test1", aInt = 0}, a2 = Nothing, a3 = Nothing}
     , mutationResolver = return Mutation {mut1 = Nothing, mut2 = Nothing, mut3 = Nothing}
-    , subscriptionResolver = return Subscription {sub1 = Nothing, sub2 = Nothing, sub3 = Nothing}
+    , subscriptionResolver =
+        return
+          Subscription
+            { sub1 = const ([EVENT], const $ return Nothing)
+            , sub2 = const ([EVENT], const $ return Nothing)
+            , sub3 = const ([EVENT], const $ return Nothing)
+            }
     }
 
 api :: ByteString -> IO ByteString
