5d3ccacf9c1153890ea82dd5a0d98d80ec7eb140
nalchevanidze <d.nalchevanidze@gmail.com>
2019-07-25 01:49:54 +0200

    optimize show

--- src/Data/Morpheus/Document.hs
+++ src/Data/Morpheus/Document.hs
@@ -31,11 +31,14 @@ isVisible (InputObject DataType {typeVisibility})       = typeVisibility
 isVisible (InputUnion DataType {typeVisibility})        = typeVisibility
 isVisible (OutputObject DataType {typeVisibility})      = typeVisibility
 
+renderIndent :: Text
+renderIndent = "  "
+
 renderType :: (Text, DataFullType) -> Text
 renderType (name, Leaf (LeafScalar _)) = "scalar " <> name
 renderType (name, Leaf (LeafEnum DataType {typeData})) = "enum " <> name <> renderObject id typeData
 renderType (name, Union DataType {typeData}) =
-  "union " <> name <> " =\n    " <> intercalate "\n  | " (map fieldType typeData)
+  "union " <> name <> " =\n    " <> intercalate ("\n" <> renderIndent <> "| ") (map fieldType typeData)
 renderType (name, InputObject DataType {typeData}) = "input " <> name <> renderDataObject renderInputField typeData
 renderType (name, InputUnion DataType {typeData}) =
   "input " <> name <> renderDataObject renderInputField (mapKeys typeData)
@@ -45,7 +48,7 @@ mapKeys :: [DataField a] -> [(Text, DataField a)]
 mapKeys = map (\x -> (fieldName x, x))
 
 renderObject :: (a -> Text) -> [a] -> Text
-renderObject f list = " { \n  " <> intercalate "\n  " (map f list) <> "\n}"
+renderObject f list = " { \n  " <> intercalate ("\n" <> renderIndent) (map f list) <> "\n}"
 
 renderDataObject :: ((Text, DataField a) -> Text) -> [(Text, DataField a)] -> Text
 renderDataObject f list = renderObject f (ignoreHidden list)
