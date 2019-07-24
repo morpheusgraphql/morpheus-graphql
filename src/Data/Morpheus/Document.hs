df37527b491fb0c1a723cc4cb46334e995829d79
nalchevanidze <d.nalchevanidze@gmail.com>
2019-07-25 01:18:56 +0200

    reorder Render Documents

--- src/Data/Morpheus/Document.hs
+++ src/Data/Morpheus/Document.hs
@@ -33,17 +33,21 @@ isVisible (OutputObject DataType {typeVisibility})      = typeVisibility
 
 renderType :: (Text, DataFullType) -> Text
 renderType (name, Leaf (LeafScalar _)) = "scalar " <> name
-renderType (name, Leaf (LeafEnum DataType {typeData})) = "enum " <> name <> " = " <> intercalate "\n | " typeData
+renderType (name, Leaf (LeafEnum DataType {typeData})) = "enum " <> name <> " = " <> renderObject id typeData
 renderType (name, Union DataType {typeData}) = "union " <> name <> " = " <> intercalate "\n | " (map fieldType typeData)
-renderType (name, InputObject DataType {typeData}) = "input " <> name <> renderObject renderInputField typeData
-renderType (name, InputUnion DataType {typeData}) = "input " <> name <> renderObject renderInputField (mapKeys typeData)
-renderType (name, OutputObject DataType {typeData}) = "type " <> name <> renderObject renderField typeData
+renderType (name, InputObject DataType {typeData}) = "input " <> name <> renderDataObject renderInputField typeData
+renderType (name, InputUnion DataType {typeData}) =
+  "input " <> name <> renderDataObject renderInputField (mapKeys typeData)
+renderType (name, OutputObject DataType {typeData}) = "type " <> name <> renderDataObject renderField typeData
 
 mapKeys :: [DataField a] -> [(Text, DataField a)]
 mapKeys = map (\x -> (fieldName x, x))
 
-renderObject :: ((Text, DataField a) -> Text) -> [(Text, DataField a)] -> Text
-renderObject f list = " { \n  " <> intercalate "\n  " (map f (ignoreHidden list)) <> "  \n}"
+renderObject :: (a -> Text) -> [a] -> Text
+renderObject f list = " { \n  " <> intercalate "\n  " (map f list) <> "  \n}"
+
+renderDataObject :: ((Text, DataField a) -> Text) -> [(Text, DataField a)] -> Text
+renderDataObject f list = renderSet f (ignoreHidden list)
   where
     ignoreHidden :: [(Text, DataField a)] -> [(Text, DataField a)]
     ignoreHidden = filter (not . fieldHidden . snd)
@@ -59,4 +63,3 @@ renderField (key, DataField {fieldTypeWrappers, fieldType, fieldArgs}) =
     renderArguments :: [(Text, DataArgument)] -> Text
     renderArguments []   = ""
     renderArguments list = "( " <> intercalate ", " (map renderInputField list) <> " )"
-
