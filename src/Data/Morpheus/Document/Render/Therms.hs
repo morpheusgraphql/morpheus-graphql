5f6a6b155a2a71b7a444c33e08a6bff0f8e686d3
nalchevanidze <d.nalchevanidze@gmail.com>
2019-07-31 20:45:55 +0200

    terms

---
+++ src/Data/Morpheus/Document/Render/Therms.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.Morpheus.Document.Render.Therms
+  ( indent
+  , renderData
+  , renderCon
+  , renderMaybe
+  , renderList
+  , renderTuple
+  , renderAssignment
+  ) where
+
+import           Data.Text (Text)
+
+indent :: Text
+indent = "  "
+
+renderData :: Text -> Text
+renderData name = "data " <> name <> " = "
+
+renderCon :: Text -> Text
+renderCon name = name <> " "
+
+renderMaybe :: Text -> Text
+renderMaybe typeName = "Maybe " <> typeName
+
+renderList :: Text -> Text
+renderList typeName = "[" <> typeName <> "]"
+
+renderTuple :: Text -> Text
+renderTuple typeName = "(" <> typeName <> ")"
+
+renderAssignment :: Text -> Text -> Text
+renderAssignment key value = key <> " :: " <> value
