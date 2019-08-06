a235cb8cfe2627ce2e8ca855158bc440511ecae6
nalchevanidze <d.nalchevanidze@gmail.com>
2019-08-06 19:12:29 +0200

    regexp Generators

--- src/Data/Morpheus/Client.hs
+++ src/Data/Morpheus/Client.hs
@@ -36,21 +36,27 @@ graphql =
 compile :: String -> Q Exp
 compile inputText =
   case runParser regexParser "<input>" (pack inputText) of
-    Left err     -> fail (show err)
-    Right regexp -> [|regexp|]
+    Left err         -> fail (show err)
+    Right gqlASTData -> [|gqlASTData|]
 
+
+
+------ LIFT --------------------------------------------
 instance Lift Text where
   lift set = appE (varE 'pack) (lift (unpack set))
 
-instance Lift GQLData
-  -- lift :: RegExp -> Q Exp
-                             where
+instance Lift GQLData where
   lift (MyList cs) = apply 'MyList [lift cs]
   lift Empty       = apply 'Empty []
 
 apply :: Name -> [Q Exp] -> Q Exp
 apply n = foldl appE (conE n)
 
+
+
+
+
+----------- Parsers --------------------------------------
 parseTuple :: Parsec Void Text a -> Parsec Void Text [a]
 parseTuple parser =
   label "Tuple" $
