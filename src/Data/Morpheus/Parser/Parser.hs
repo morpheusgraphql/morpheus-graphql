3929b0d6d79d64d885d525d4b5765e888cf70cba
nalchevanidze <d.nalchevanidze@gmail.com>
2019-08-06 19:20:04 +0200

    Optimize Parser

--- src/Data/Morpheus/Parser/Parser.hs
+++ src/Data/Morpheus/Parser/Parser.hs
@@ -3,6 +3,7 @@
 
 module Data.Morpheus.Parser.Parser
   ( parseGQL
+  , parseGQLSyntax
   ) where
 
 import qualified Data.List.NonEmpty                      as NonEmpty
@@ -12,7 +13,6 @@ import           Data.Morpheus.Parser.Fragment           (fragment)
 import           Data.Morpheus.Parser.Internal           (Parser)
 import           Data.Morpheus.Parser.Operator           (parseAnonymousQuery, parseOperator)
 import           Data.Morpheus.Types.Internal.Validation (GQLError (GQLError), GQLErrors, Validation, desc, positions)
-import           Data.Morpheus.Types.Internal.Value      (Value (..))
 import           Data.Morpheus.Types.IO                  (GQLRequest (..))
 import           Data.Morpheus.Types.Types               (GQLQueryRoot (..))
 import           Data.Text                               (Text, pack)
@@ -23,14 +23,6 @@ import           Text.Megaparsec                         (ParseError, ParseError
                                                           (<|>))
 import           Text.Megaparsec.Char                    (space)
 
-request :: Parser GQLQueryRoot
-request =
-  label "GQLQueryRoot" $ do
-    space
-    operator <- parseAnonymousQuery <|> parseOperator
-    fragments <- manyTill fragment eof
-    pure GQLQueryRoot {operator, fragments, inputVariables = []}
-
 processErrorBundle :: ParseErrorBundle Text Void -> GQLErrors
 processErrorBundle = fmap parseErrorToGQLError . bundleToErrors
   where
@@ -40,14 +32,19 @@ processErrorBundle = fmap parseErrorToGQLError . bundleToErrors
     bundleToErrors ParseErrorBundle {bundleErrors, bundlePosState} =
       NonEmpty.toList $ fst $ attachSourcePos errorOffset bundleErrors bundlePosState
 
-getVariables :: GQLRequest -> [(Text, Value)]
-getVariables request' = maybe [] toList (variables request')
-
-parseReq :: GQLRequest -> Either (ParseErrorBundle Text Void) GQLQueryRoot
-parseReq requestBody = runParser request "<input>" $ query requestBody
+parseGQLSyntax :: Text -> Either (ParseErrorBundle Text Void) GQLQueryRoot
+parseGQLSyntax = runParser request "<input>"
+  where
+    request :: Parser GQLQueryRoot
+    request =
+      label "GQLQueryRoot" $ do
+        space
+        operator <- parseAnonymousQuery <|> parseOperator
+        fragments <- manyTill fragment eof
+        pure GQLQueryRoot {operator, fragments, inputVariables = []}
 
 parseGQL :: GQLRequest -> Validation GQLQueryRoot
-parseGQL requestBody =
-  case parseReq requestBody of
-    Right root      -> Right $ root {inputVariables = getVariables requestBody}
+parseGQL GQLRequest {query, variables} =
+  case parseGQLSyntax query of
+    Right root      -> Right $ root {inputVariables = maybe [] toList variables}
     Left parseError -> Left $ processErrorBundle parseError
