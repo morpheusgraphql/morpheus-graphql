d9c467d24d67a663330ab4957bd90032c0a5efa4
horus <horus.li@gmail.com>
2019-08-15 00:00:57 +0800

    Feature/Handle Reserved Names #207
    Extend convertTo* to handle all Haskell 2010 reservedid

--- src/Data/Morpheus/Types/Internal/Value.hs
+++ src/Data/Morpheus/Types/Internal/Value.hs
@@ -18,12 +18,56 @@ import qualified Data.Vector         as V (toList)
 import           GHC.Generics        (Generic)
 
 convertToJSONName :: Text -> Text
-convertToJSONName "type'" = "type"
-convertToJSONName x       = x
+convertToJSONName "case'"     = "case"
+convertToJSONName "class'"    = "class"
+convertToJSONName "data'"     = "data"
+convertToJSONName "default'"  = "default"
+convertToJSONName "deriving'" = "deriving"
+convertToJSONName "do'"       = "do"
+convertToJSONName "else'"     = "else"
+convertToJSONName "foreign'"  = "foreign"
+convertToJSONName "if'"       = "if"
+convertToJSONName "import'"   = "import"
+convertToJSONName "in'"       = "in"
+convertToJSONName "infix'"    = "infix"
+convertToJSONName "infixl'"   = "infixl"
+convertToJSONName "infixr'"   = "infixr"
+convertToJSONName "instance'" = "instance"
+convertToJSONName "let'"      = "let"
+convertToJSONName "module'"   = "module"
+convertToJSONName "newtype'"  = "newtype"
+convertToJSONName "of'"       = "of"
+convertToJSONName "then'"     = "then"
+convertToJSONName "type'"     = "type"
+convertToJSONName "where'"    = "where"
+convertToJSONName "_'"        = "_"
+convertToJSONName x           = x
 
 convertToHaskellName :: Text -> Text
-convertToHaskellName "type" = "type'"
-convertToHaskellName x      = x
+convertToHaskellName "case"     = "case'"
+convertToHaskellName "class"    = "class'"
+convertToHaskellName "data"     = "data'"
+convertToHaskellName "default"  = "default'"
+convertToHaskellName "deriving" = "deriving'"
+convertToHaskellName "do"       = "do'"
+convertToHaskellName "else"     = "else'"
+convertToHaskellName "foreign"  = "foreign'"
+convertToHaskellName "if"       = "if'"
+convertToHaskellName "import"   = "import'"
+convertToHaskellName "in"       = "in'"
+convertToHaskellName "infix"    = "infix'"
+convertToHaskellName "infixl"   = "infixl'"
+convertToHaskellName "infixr"   = "infixr'"
+convertToHaskellName "instance" = "instance'"
+convertToHaskellName "let"      = "let'"
+convertToHaskellName "module"   = "module'"
+convertToHaskellName "newtype"  = "newtype'"
+convertToHaskellName "of"       = "of'"
+convertToHaskellName "then"     = "then'"
+convertToHaskellName "type"     = "type'"
+convertToHaskellName "where"    = "where'"
+convertToHaskellName "_"        = "_'"
+convertToHaskellName x          = x
 
 -- | Primitive Values for GQLScalar: 'Int', 'Float', 'String', 'Boolean'.
 -- for performance reason type 'Text' represents GraphQl 'String' value
