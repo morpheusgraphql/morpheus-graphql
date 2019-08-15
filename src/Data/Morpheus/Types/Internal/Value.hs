7608653dfb24d4e64b8490754e62df4ac95d61ea
horus <horus.li@gmail.com>
2019-08-16 00:00:04 +0800

    Reimplement convertTo* with isReserved

--- src/Data/Morpheus/Types/Internal/Value.hs
+++ src/Data/Morpheus/Types/Internal/Value.hs
@@ -14,60 +14,47 @@ import qualified Data.HashMap.Strict as M (toList)
 import           Data.Scientific     (Scientific, floatingOrInteger)
 import           Data.Semigroup      ((<>))
 import           Data.Text           (Text)
+import qualified Data.Text           as T
 import qualified Data.Vector         as V (toList)
 import           GHC.Generics        (Generic)
 
+isReserved :: Text -> Bool
+isReserved "case"     = True
+isReserved "class"    = True
+isReserved "data"     = True
+isReserved "default"  = True
+isReserved "deriving" = True
+isReserved "do"       = True
+isReserved "else"     = True
+isReserved "foreign"  = True
+isReserved "if"       = True
+isReserved "import"   = True
+isReserved "in"       = True
+isReserved "infix"    = True
+isReserved "infixl"   = True
+isReserved "infixr"   = True
+isReserved "instance" = True
+isReserved "let"      = True
+isReserved "module"   = True
+isReserved "newtype"  = True
+isReserved "of"       = True
+isReserved "then"     = True
+isReserved "type"     = True
+isReserved "where"    = True
+isReserved "_"        = True
+isReserved _          = False
+{-# INLINE isReserved #-}
+
 convertToJSONName :: Text -> Text
-convertToJSONName "case'"     = "case"
-convertToJSONName "class'"    = "class"
-convertToJSONName "data'"     = "data"
-convertToJSONName "default'"  = "default"
-convertToJSONName "deriving'" = "deriving"
-convertToJSONName "do'"       = "do"
-convertToJSONName "else'"     = "else"
-convertToJSONName "foreign'"  = "foreign"
-convertToJSONName "if'"       = "if"
-convertToJSONName "import'"   = "import"
-convertToJSONName "in'"       = "in"
-convertToJSONName "infix'"    = "infix"
-convertToJSONName "infixl'"   = "infixl"
-convertToJSONName "infixr'"   = "infixr"
-convertToJSONName "instance'" = "instance"
-convertToJSONName "let'"      = "let"
-convertToJSONName "module'"   = "module"
-convertToJSONName "newtype'"  = "newtype"
-convertToJSONName "of'"       = "of"
-convertToJSONName "then'"     = "then"
-convertToJSONName "type'"     = "type"
-convertToJSONName "where'"    = "where"
-convertToJSONName "_'"        = "_"
-convertToJSONName x           = x
+convertToJSONName hsName
+  | not (T.null hsName) && isReserved name && (T.last hsName == '\'') = name
+  | otherwise = hsName
+  where name = T.init hsName
 
 convertToHaskellName :: Text -> Text
-convertToHaskellName "case"     = "case'"
-convertToHaskellName "class"    = "class'"
-convertToHaskellName "data"     = "data'"
-convertToHaskellName "default"  = "default'"
-convertToHaskellName "deriving" = "deriving'"
-convertToHaskellName "do"       = "do'"
-convertToHaskellName "else"     = "else'"
-convertToHaskellName "foreign"  = "foreign'"
-convertToHaskellName "if"       = "if'"
-convertToHaskellName "import"   = "import'"
-convertToHaskellName "in"       = "in'"
-convertToHaskellName "infix"    = "infix'"
-convertToHaskellName "infixl"   = "infixl'"
-convertToHaskellName "infixr"   = "infixr'"
-convertToHaskellName "instance" = "instance'"
-convertToHaskellName "let"      = "let'"
-convertToHaskellName "module"   = "module'"
-convertToHaskellName "newtype"  = "newtype'"
-convertToHaskellName "of"       = "of'"
-convertToHaskellName "then"     = "then'"
-convertToHaskellName "type"     = "type'"
-convertToHaskellName "where"    = "where'"
-convertToHaskellName "_"        = "_'"
-convertToHaskellName x          = x
+convertToHaskellName name
+  | isReserved name = name <> "'"
+  | otherwise = name
 
 -- | Primitive Values for GQLScalar: 'Int', 'Float', 'String', 'Boolean'.
 -- for performance reason type 'Text' represents GraphQl 'String' value
