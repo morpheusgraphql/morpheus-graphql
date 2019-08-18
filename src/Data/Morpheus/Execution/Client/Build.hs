1e2d515d80b33b9de392958008375ed763df690b
nalchevanidze <d.nalchevanidze@gmail.com>
2019-08-18 19:19:17 +0200

    correct OperationName (#212)

--- src/Data/Morpheus/Execution/Client/Build.hs
+++ src/Data/Morpheus/Execution/Client/Build.hs
@@ -27,15 +27,13 @@ queryArgumentType (rootType@TypeD {tName}:xs) = (ConT $ mkName tName, types)
 
 defineType :: [String] -> TypeD -> Dec
 defineType derivings TypeD {tName, tCons} =
-  DataD [] (mkName tName) [] Nothing (map cons tCons) $
-  map derive (["Show", "Generic"] ++ derivings)
+  DataD [] (mkName tName) [] Nothing (map cons tCons) $ map derive (["Show", "Generic"] ++ derivings)
   where
     defBang = Bang NoSourceUnpackedness NoSourceStrictness
     derive className = DerivClause Nothing [ConT (mkName className)]
     cons ConsD {cName, cFields} = RecC (mkName cName) (map genField cFields)
       where
-        genField FieldD {fieldNameD, fieldTypeD} =
-          (mkName fieldNameD, defBang, genFieldT fieldTypeD)
+        genField FieldD {fieldNameD, fieldTypeD} = (mkName fieldNameD, defBang, genFieldT fieldTypeD)
           where
             genFieldT (ListD td)   = AppT (ConT ''[]) (genFieldT td)
             genFieldT (MaybeD td)  = AppT (ConT ''Maybe) (genFieldT td)
@@ -50,14 +48,13 @@ defineJSONType datatype = do
 defineOperationType :: (Type, Q [Dec]) -> String -> TypeD -> Q [Dec]
 defineOperationType (argType, argumentTypes) query datatype = do
   rootType <- defineJSONType datatype
-  typeClassFetch <- deriveFetch argType (mkName $ tName datatype) query
+  typeClassFetch <- deriveFetch argType (tName datatype) query
   args <- argumentTypes
   pure $ rootType <> typeClassFetch <> args
 
 defineQuery :: QueryD -> Q [Dec]
 defineQuery QueryD {queryTypes = rootType:subTypes, queryText, queryArgTypes} = do
-  rootDecs <-
-    defineOperationType (queryArgumentType queryArgTypes) queryText rootType
+  rootDecs <- defineOperationType (queryArgumentType queryArgTypes) queryText rootType
   subTypeDecs <- concat <$> mapM defineJSONType subTypes
   return $ rootDecs ++ subTypeDecs
 defineQuery QueryD {queryTypes = []} = return []
