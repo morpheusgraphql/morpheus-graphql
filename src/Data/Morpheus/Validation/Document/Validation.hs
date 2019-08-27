8f7ebf666c3ef351dbf1659e36d599c0fa389739
nalchevanidze <d.nalchevanidze@gmail.com>
2019-08-27 18:43:16 +0200

    rewrite Validation

--- src/Data/Morpheus/Validation/Document/Validation.hs
+++ src/Data/Morpheus/Validation/Document/Validation.hs
@@ -2,15 +2,14 @@ module Data.Morpheus.Validation.Document.Validation
   ( validatePartialDocument
   ) where
 
+import           Data.Maybe
 import           Data.Morpheus.Types.Internal.Data       (DataFullType (..), Key, RawDataType (..))
 import           Data.Morpheus.Types.Internal.Validation (Validation)
 
 validatePartialDocument :: [(Key, RawDataType)] -> Validation [(Key, DataFullType)]
-validatePartialDocument [] = pure []
-validatePartialDocument ((name, FinalDataType x):xs) = do
-  list <- validatePartialDocument xs
-  pure $ (name, x) : list
-validatePartialDocument ((name, Implements _ dt):xs) = do
-  list <- validatePartialDocument xs
-  pure $ (name, OutputObject dt) : list
-validatePartialDocument (_:xs) = validatePartialDocument xs
+validatePartialDocument = fmap catMaybes . traverse validateType
+  where
+    validateType :: (Key, RawDataType) -> Validation (Maybe (Key, DataFullType))
+    validateType (name, FinalDataType x) = pure $ Just (name, x)
+    validateType (name, Implements _ x)  = pure $ Just (name, OutputObject x)
+    validateType _                       = pure Nothing
