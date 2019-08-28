58018a29d36b3ba4a28ea9df5c29f91de7b608d9
nalchevanidze <d.nalchevanidze@gmail.com>
2019-08-28 07:36:44 +0200

    implements errors

--- src/Data/Morpheus/Error/Document/Interface.hs
+++ src/Data/Morpheus/Error/Document/Interface.hs
@@ -1,15 +1,34 @@
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Data.Morpheus.Error.Document.Interface
   ( unknownInterface
+  , partialImplements
+  , ImplementsError(..)
   ) where
 
 import           Data.Morpheus.Error.Utils               (errorMessage)
 import           Data.Morpheus.Types.Internal.Base       (Key, Position)
-import           Data.Morpheus.Types.Internal.Validation (GQLErrors)
+import           Data.Morpheus.Types.Internal.Validation (GQLError (..), GQLErrors)
 import           Data.Semigroup                          ((<>))
 
 unknownInterface :: Key -> Position -> GQLErrors
 unknownInterface name = (`errorMessage` message)
   where
     message = "Unknown Interface \"" <> name <> "\"."
+
+data ImplementsError
+  = UnexpectedType { expectedType :: Key
+                   , foundType    :: Key }
+  | UndefinedField
+
+partialImplements :: Key -> Key -> Position -> [(Key, ImplementsError)] -> GQLErrors
+partialImplements name interfaceName position = map impError
+  where
+    impError (key, errorType) = GQLError {desc = message, positions = [position]}
+      where
+        message =
+          "type " <> name <> "implements Interface " <> interfaceName <> "Partially," <> detailedMessage errorType
+        detailedMessage UnexpectedType {expectedType, foundType} =
+          " on key \"" <> key <> " expected type " <> expectedType <> " found " <> foundType <> "\"."
+        detailedMessage UndefinedField = " key \"" <> key <> "not found \"."
