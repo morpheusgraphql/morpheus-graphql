949c67ff6e128fca40eb7bdb275240cc8aa6b63f
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-04 22:04:06 +0200

    declare tyVarResolvers

--- src/Data/Morpheus/Execution/Internal/Declare.hs
+++ src/Data/Morpheus/Execution/Internal/Declare.hs
@@ -18,7 +18,7 @@ import           Data.Morpheus.Types.Internal.Data  (DataTypeKind (..))
 
 --
 -- MORPHEUS
-import           Data.Morpheus.Types.Internal.DataD (AppD (..), ConsD (..), FieldD (..), TypeD (..))
+import           Data.Morpheus.Types.Internal.DataD (AppD (..), ConsD (..), FieldD (..), ResolverKind (..), TypeD (..))
 import           GHC.Generics                       (Generic)
 
 type FUNC = (->)
@@ -43,14 +43,17 @@ __declareType isResolver derivingList TypeD {tName, tCons} =
     derive className = DerivClause Nothing [ConT className]
     cons ConsD {cName, cFields} = RecC (mkName cName) (map genField cFields)
       where
-        genField FieldD {fieldNameD, fieldTypeD} = (mkName fieldNameD, defBang, genFieldT fieldTypeD)
+        genField FieldD {fieldNameD, fieldTypeD} = (mkName fieldNameD, defBang, genFieldT False fieldTypeD)
           where
-            genFieldT (ListD td) = AppT (ConT ''[]) (genFieldT td)
-            genFieldT (MaybeD td) = AppT (ConT ''Maybe) (genFieldT td)
-            genFieldT (BaseD name) = ConT (mkName name)
-            genFieldT (ResD arg _ td) = AppT (AppT arrowType argType) resultType
+            monadVar = VarT $ mkName "m"
+            ---------------------------
+            genFieldT resM (ListD td) = AppT (ConT ''[]) (genFieldT resM td)
+            genFieldT resM (MaybeD td) = AppT (ConT ''Maybe) (genFieldT resM td)
+            genFieldT True (BaseD name) = AppT (ConT (mkName name)) monadVar
+            genFieldT False (BaseD name) = ConT (mkName name)
+            genFieldT _ (ResD arg resKind td) = AppT (AppT arrowType argType) (resultType resKind)
               where
                 argType = ConT $ mkName arg
                 arrowType = ConT ''FUNC
-                resultType = AppT monadVar (AppT (genFieldT td) monadVar)
-                monadVar = VarT $ mkName "m"
+                resultType TypeVarResolver = AppT monadVar (genFieldT True td)
+                resultType _               = AppT monadVar (genFieldT False td)
