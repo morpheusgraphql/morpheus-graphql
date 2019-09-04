5ba14a4e831db24109fc47dd674a9800185a205e
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-04 22:11:59 +0200

    GQLType

--- src/Data/Morpheus/Execution/Document/GQLType.hs
+++ src/Data/Morpheus/Execution/Document/GQLType.hs
@@ -16,13 +16,18 @@ import           Data.Morpheus.Kind                 (ENUM, INPUT_OBJECT, INPUT_U
 import           Data.Morpheus.Types.GQLType        (GQLType (..))
 import           Data.Morpheus.Types.Internal.Data  (DataTypeKind (..))
 import           Data.Morpheus.Types.Internal.DataD (GQLTypeD, TypeD (..))
+import           Data.Typeable                      (Typeable)
 
 deriveGQLType :: GQLTypeD -> Q [Dec]
 deriveGQLType (TypeD {tName}, gqlKind, _) =
-  pure <$> instanceD (cxt []) (appT (conT ''GQLType) (genHeadSig gqlKind)) [methods]
+  pure <$> instanceD (cxt $ constrains gqlKind) (appT (conT ''GQLType) (genHeadSig gqlKind)) [methods]
   where
     genHeadSig KindObject = appT (conT $ mkName tName) (varT $ mkName "m")
     genHeadSig _          = conT $ mkName tName
+    ----------
+    constrains KindObject = [appT (conT ''Typeable) (varT $ mkName "m")]
+    constrains _          = []
+    ----
     methods = do
       typeN <- genHeadSig gqlKind
       pure $ TySynInstD ''KIND (TySynEqn [typeN] (ConT $ toKIND gqlKind))
