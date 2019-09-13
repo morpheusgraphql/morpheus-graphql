9bf85ba797120340b5720217c93cecdff6d518ea
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-14 01:29:36 +0200

    deriveDecode

---
+++ src/Data/Morpheus/Execution/Document/Decode.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE MonoLocalBinds    #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+module Data.Morpheus.Execution.Document.Decode
+  ( deriveDecode
+  ) where
+
+import           Data.Text                               (Text)
+import           Language.Haskell.TH
+
+import           Data.Morpheus.Error.Internal            (internalArgumentError, internalTypeMismatch)
+
+--
+-- MORPHEUS
+import           Data.Morpheus.Execution.Server.Decode   (Decode (..))
+import           Data.Morpheus.Types.Internal.DataD      (ConsD (..), FieldD (..), TypeD (..))
+import           Data.Morpheus.Types.Internal.Validation (Validation)
+import           Data.Morpheus.Types.Internal.Value      (Value (..))
+
+objectBody :: ConsD -> ExpQ
+objectBody ConsD {cName, cFields} = handleFields cFields
+  where
+    consName = mkName cName
+    ----------------------------------------------------------------------------------
+    handleFields fNames = uInfixE (conE consName) (varE '(<$>)) (applyFields fNames)
+      where
+        applyFields []     = fail "No Empty fields"
+        applyFields [x]    = defField x
+        applyFields (x:xs) = uInfixE (defField x) (varE '(<*>)) (applyFields xs)
+            ----------
+        defField FieldD {fieldNameD} = [|o .: fieldNameD|]
+
+(.:) :: Decode a => Value -> Text -> Validation a
+(Object object) .: selectorName = selectFromObject
+  where
+    selectFromObject =
+      case lookup selectorName object of
+        Nothing    -> internalArgumentError ("Missing Field: " <> selectorName)
+        Just value -> decode value
+isType .: _ = internalTypeMismatch "InputObject" isType
+
+deriveDecode :: TypeD -> Q [Dec]
+deriveDecode TypeD {tName, tCons = [cons]} = pure <$> instanceD (cxt []) appHead methods
+  where
+    appHead = appT classT typeT
+      where
+        classT = conT ''Decode
+        typeT = conT $ mkName tName
+    methods = [funD 'decode [clause argsE (normalB body) []]]
+      where
+        argsE = [varP (mkName "o")]
+        body = objectBody cons
+deriveDecode _ = pure []
