7c860092d0131aca89d259f41f555636c76a9d29
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-13 22:32:10 +0200

    typeBuilder

--- src/Data/Morpheus/Execution/Document/Introspection.hs
+++ src/Data/Morpheus/Execution/Document/Introspection.hs
@@ -10,11 +10,13 @@ import           Language.Haskell.TH
 
 --
 -- MORPHEUS
-import           Data.Morpheus.Execution.Server.Introspect (Introspect (..), updateLib)
+import           Data.Morpheus.Execution.Server.Introspect (Introspect (..), buildType, updateLib)
 import           Data.Morpheus.Types.GQLType               (GQLType (..))
-import           Data.Morpheus.Types.Internal.Data         (DataField (..), DataFullType (..), DataType (..))
+import           Data.Morpheus.Types.Internal.Data         (DataField (..), DataFullType (..),
+                                                            DataType (..), DataTypeWrapper (..), Key)
 import           Data.Morpheus.Types.Internal.DataD        (ConsD (..), FieldD (..), TypeD (..))
 import           Data.Proxy                                (Proxy (..))
+import           Data.Text                                 (pack)
 
 deriveObjectRep :: TypeD -> Q [Dec]
 deriveObjectRep TypeD {tName, tCons = [ConsD {cFields}]} = pure <$> instanceD (cxt []) appHead methods
@@ -24,40 +26,24 @@ deriveObjectRep TypeD {tName, tCons = [ConsD {cFields}]} = pure <$> instanceD (c
       where
         classT = conT ''Introspect
         typeT = conT $ mkName tName
-     -- objectFieldTypes :: Proxy rep -> [((Text, DataField t), TypeUpdater)]
     methods = [funD 'introspect [clause argsE (normalB body) []]]
       where
         argsE = [varP (mkName "_")]
-        body = [|updateLib $(datatypeE) $(types) $(proxyE)|]
-        types = [|[]|]
-        proxyE = [|(Proxy :: (Proxy $(typeName)))|]
-        datatypeE =
-          [|const $
-            InputObject $
-            DataType
-              { typeName = tName
-              , typeFingerprint = __typeFingerprint $(proxyE)
-              , typeDescription = tName
-              , typeVisibility = True
-              , typeData = []
-              }|]
-          where
-            fieldsE = listE (map genField cFields)
-              where
-                genField FieldD {fieldNameD} =
-                  [|( fieldNameD
-                    , DataField
-                        { fieldArgs = []
-                        , fieldName = fieldNameD
-                        , fieldType = "JOE"
-                        , fieldTypeWrappers = []
-                        , fieldHidden = False
-                        })|]
+        body = [|updateLib (typeBuilder cFields) [] (Proxy :: (Proxy $(typeName)))|]
 deriveObjectRep _ = pure []
---
---
---instance InputObjectConstraint a => Introspect a INPUT_OBJECT InputType where
---  __field _ = buildField (Proxy @a) ()
---  introspect _ = updateLib (InputObject . buildType fields') stack' (Proxy @a)
---  introspect :: Context a kind args -> TypeUpdater -- Generates internal GraphQL Schema
--- type TypeUpdater = DataTypeLib -> SchemaValidation DataTypeLib
+
+typeBuilder :: GQLType a => [FieldD] -> (Proxy a -> DataFullType)
+typeBuilder fields = InputObject . buildType (buildFields fields)
+
+buildFields :: [FieldD] -> [(Key, DataField)]
+buildFields = map buildField
+  where
+    buildField FieldD {fieldNameD, fieldTypeD} =
+      ( pack fieldNameD
+      , DataField
+          { fieldName = pack fieldNameD
+          , fieldArgs = []
+          , fieldTypeWrappers = [NonNullType]
+          , fieldType = pack "Text"
+          , fieldHidden = False
+          })
