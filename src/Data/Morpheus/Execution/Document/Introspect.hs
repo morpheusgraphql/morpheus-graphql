02eb877e2275f309d8e3d75f5a59c11b86502d58
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-13 23:23:47 +0200

    generate FieldTypes

--- src/Data/Morpheus/Execution/Document/Introspect.hs
+++ src/Data/Morpheus/Execution/Document/Introspect.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE QuasiQuotes     #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Data.Morpheus.Execution.Document.Introspection
+module Data.Morpheus.Execution.Document.Introspect
   ( deriveObjectRep
   ) where
 
@@ -14,9 +14,9 @@ import           Data.Morpheus.Execution.Server.Introspect (Introspect (..), bui
 import           Data.Morpheus.Types.GQLType               (GQLType (..))
 import           Data.Morpheus.Types.Internal.Data         (DataField (..), DataFullType (..), DataTypeWrapper (..),
                                                             Key)
-import           Data.Morpheus.Types.Internal.DataD        (ConsD (..), FieldD (..), TypeD (..))
+import           Data.Morpheus.Types.Internal.DataD        (AppD (..), ConsD (..), FieldD (..), TypeD (..))
 import           Data.Proxy                                (Proxy (..))
-import           Data.Text                                 (pack)
+import           Data.Text                                 (Text, pack)
 
 deriveObjectRep :: TypeD -> Q [Dec]
 deriveObjectRep TypeD {tName, tCons = [ConsD {cFields}]} = pure <$> instanceD (cxt []) appHead methods
@@ -40,10 +40,16 @@ buildFields = map buildField
   where
     buildField FieldD {fieldNameD, fieldTypeD} =
       ( pack fieldNameD
-      , DataField
-          { fieldName = pack fieldNameD
-          , fieldArgs = []
-          , fieldTypeWrappers = [NonNullType]
-          , fieldType = pack "String"
-          , fieldHidden = False
-          })
+      , DataField {fieldName = pack fieldNameD, fieldArgs = [], fieldTypeWrappers, fieldType, fieldHidden = False})
+      where
+        (fieldTypeWrappers, fieldType) = appDToField fieldTypeD
+
+appDToField :: AppD String -> ([DataTypeWrapper], Text)
+appDToField = appDToField []
+  where
+    appDToField wrappers (MaybeD (ListD td))   = appDToField (ListType : wrappers) td
+    appDToField wrappers (MaybeD (MaybeD td))  = appDToField wrappers (MaybeD td)
+    appDToField wrappers (MaybeD (BaseD name)) = (wrappers, pack name)
+    ----------------------------------- NONNULL
+    appDToField wrappers (ListD td)            = appDToField ([NonNullType, ListType] <> wrappers) td
+    appDToField wrappers (BaseD name)          = (NonNullType : wrappers, pack name)
