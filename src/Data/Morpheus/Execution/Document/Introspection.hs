b9206b911e844863ba483c1736fbbc16ef8b0207
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-13 17:34:15 +0200

    start Writing Introspection

--- src/Data/Morpheus/Execution/Document/Introspection.hs
+++ src/Data/Morpheus/Execution/Document/Introspection.hs
@@ -10,27 +10,54 @@ import           Language.Haskell.TH
 
 --
 -- MORPHEUS
-import           Data.Morpheus.Execution.Server.Introspect (ObjectRep (..))
+import           Data.Morpheus.Execution.Server.Introspect (Introspect (..))
 import           Data.Morpheus.Types.Internal.Data         (DataField (..))
-import           Data.Morpheus.Types.Internal.DataD        (ConsD (..), FieldD (..), GQLTypeD (..), TypeD (..))
+import           Data.Morpheus.Types.Internal.DataD        (ConsD (..), FieldD (..), TypeD (..))
 import           Data.Text                                 (pack)
 
 deriveObjectRep :: TypeD -> Q [Dec]
-deriveObjectRep TypeD {tName, tCons = [ConsD {cFields}]} =
-  pure <$> instanceD (cxt []) appHead methods
+deriveObjectRep TypeD {tName, tCons = [ConsD {cFields}]} = pure <$> instanceD (cxt []) appHead methods
   where
-    appHead = appT (appT (conT ''ObjectRep) (conT $ mkName tName)) (conT ''())
-     -- objectFieldTypes :: Proxy rep -> [((Text, DataField t), TypeUpdater)]
-    genField FieldD {fieldNameD} = [|((fieldNameD, field), pure)|]
+    appHead = appT (appT (appT classT typeT) aT) bT
       where
-        field =
-          DataField
-            { fieldArgs = ()
-            , fieldName = pack fieldNameD
-            , fieldType = pack "JOE"
-            , fieldTypeWrappers = []
-            , fieldHidden = False
-            }
+        classT = conT ''Introspect
+        typeT = conT $ mkName tName
+        bT = varT (mkName "a")
+        aT = varT (mkName "a")
+     -- objectFieldTypes :: Proxy rep -> [((Text, DataField t), TypeUpdater)]
     fields = map genField cFields
-    methods = [funD 'objectFieldTypes [clause [varP (mkName "_")] (normalB (listE fields)) []]]
+      where
+        genField FieldD {fieldNameD} = [|((fieldNameD, field), pure)|]
+          where
+            field =
+              DataField
+                { fieldArgs = ()
+                , fieldName = pack fieldNameD
+                , fieldType = pack "JOE"
+                , fieldTypeWrappers = []
+                , fieldHidden = False
+                }
+    methods = [funD 'introspect [clause args (normalB body) []]]
+      where
+        args = [varP (mkName "_")]
+        body = varE 'pure
 deriveObjectRep _ = pure []
+--
+--
+--instance InputObjectConstraint a => Introspect a INPUT_OBJECT InputType where
+--  __field _ = buildField (Proxy @a) ()
+--  introspect _ = updateLib (InputObject . buildType fields') stack' (Proxy @a)
+--    where
+--      (fields', stack') = unzip $ objectFieldTypes (Proxy @(Rep a))
+--
+--
+--class Introspect a kind args where
+--  __field :: Context a kind args -> Text -> DataField args
+--    --   generates data field representation of object field
+--    --   according to parameter 'args' it could be
+--    --   * input object field: if args is '()'
+--    --   * object: if args is 'DataArguments'
+--
+--
+--  introspect :: Context a kind args -> TypeUpdater -- Generates internal GraphQL Schema
+-- type TypeUpdater = DataTypeLib -> SchemaValidation DataTypeLib
