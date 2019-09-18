5137c5d94e5cbb7d8307a041c03d518f9d22ae58
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-18 17:55:07 +0200

    organize Internal Declare

--- src/Data/Morpheus/Execution/Internal/Declare.hs
+++ src/Data/Morpheus/Execution/Internal/Declare.hs
@@ -25,15 +25,15 @@ import           GHC.Generics                       (Generic)
 type FUNC = (->)
 
 declareType :: [Name] -> TypeD -> Dec
-declareType = __declareType Nothing
+declareType = __declareType False Nothing
 
 declareResolverType :: KindD -> [Name] -> TypeD -> Dec
-declareResolverType x = __declareType (Just x)
+declareResolverType x = __declareType False (Just x)
 
 --
 --
-__declareType :: Maybe KindD -> [Name] -> TypeD -> Dec
-__declareType kindD derivingList TypeD {tName, tCons} =
+__declareType :: Bool -> Maybe KindD -> [Name] -> TypeD -> Dec
+__declareType namespace kindD derivingList TypeD {tName, tCons} =
   DataD [] (mkName tName) tVars Nothing (map cons tCons) $ map derive (''Generic : derivingList)
   where
     gqlKind = unKindD <$> kindD
@@ -46,24 +46,27 @@ __declareType kindD derivingList TypeD {tName, tCons} =
     declareTyVar = map (PlainTV . mkName)
     defBang = Bang NoSourceUnpackedness NoSourceStrictness
     derive className = DerivClause Nothing [ConT className]
-    cons ConsD {cName, cFields} = RecC (mkName cName) (map genField cFields)
+    cons ConsD {cName, cFields} = RecC (mkName cName) (map declareField cFields)
       where
-        genField FieldD {fieldNameD, fieldTypeD} = (mkName fieldNameD, defBang, genFieldT False fieldTypeD)
+        declareField FieldD {fieldNameD, fieldTypeD} = (fieldName, defBang, fieldType)
           where
-            monadVar = VarT $ mkName "m"
-            subscriptionVar = VarT $ mkName "subscriptionM"
-            ---------------------------
-            genFieldT resM (ListD td) = AppT (ConT ''[]) (genFieldT resM td)
-            genFieldT resM (MaybeD td) = AppT (ConT ''Maybe) (genFieldT resM td)
-            genFieldT True (BaseD name) = AppT (ConT (mkName name)) monadVar
-            genFieldT False (BaseD name)
-              | gqlKind == Just KindUnion = AppT (ConT (mkName name)) monadVar
-            genFieldT False (BaseD name) = ConT (mkName name)
-            genFieldT _ (ResD arg resKind td) = AppT (AppT arrowType argType) (resultType resKind)
+            fieldName = mkName fieldNameD
+            fieldType = genFieldT False fieldTypeD
               where
-                argType = ConT $ mkName arg
-                arrowType = ConT ''FUNC
-                resultType _
-                  | isSubscription = AppT subscriptionVar (genFieldT True td)
-                resultType TypeVarResolver = AppT monadVar (genFieldT True td)
-                resultType _ = AppT monadVar (genFieldT False td)
+                monadVar = VarT $ mkName "m"
+                subscriptionVar = VarT $ mkName "subscriptionM"
+                ---------------------------
+                genFieldT resM (ListD td) = AppT (ConT ''[]) (genFieldT resM td)
+                genFieldT resM (MaybeD td) = AppT (ConT ''Maybe) (genFieldT resM td)
+                genFieldT True (BaseD name) = AppT (ConT (mkName name)) monadVar
+                genFieldT False (BaseD name)
+                  | gqlKind == Just KindUnion = AppT (ConT (mkName name)) monadVar
+                genFieldT False (BaseD name) = ConT (mkName name)
+                genFieldT _ (ResD arg resKind td) = AppT (AppT arrowType argType) (resultType resKind)
+                  where
+                    argType = ConT $ mkName arg
+                    arrowType = ConT ''FUNC
+                    resultType _
+                      | isSubscription = AppT subscriptionVar (genFieldT True td)
+                    resultType TypeVarResolver = AppT monadVar (genFieldT True td)
+                    resultType _ = AppT monadVar (genFieldT False td)
