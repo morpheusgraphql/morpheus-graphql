fc287f053ad7333d544e9f4d56bed89ca6ec4dc8
nalchevanidze <d.nalchevanidze@gmail.com>
2019-09-23 14:17:50 +0200

    Flexible Context

--- examples/TH/Simple.hs
+++ examples/TH/Simple.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns        #-}
--- src/Data/Morpheus/Execution/Document/Encode.hs
+++ src/Data/Morpheus/Execution/Document/Encode.hs
@@ -1,29 +1,42 @@
-{-# LANGUAGE NamedFieldPuns    #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE NamedFieldPuns        #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 module Data.Morpheus.Execution.Document.Encode
   ( deriveEncode
   ) where
 
-import           Data.Text                             (unpack)
+import           Data.Text                               (unpack)
+import           Data.Typeable                           (Typeable)
 import           Language.Haskell.TH
 
 --
 -- MORPHEUS
-import           Data.Morpheus.Execution.Server.Encode (Encode (..), ObjectResolvers (..))
-import           Data.Morpheus.Types.Internal.Data     (DataField (..))
-import           Data.Morpheus.Types.Internal.DataD    (ConsD (..), TypeD (..))
-import           Data.Morpheus.Types.Internal.TH       (instanceHeadMultiT, typeT)
+import           Data.Morpheus.Execution.Server.Encode   (Encode (..), ObjectResolvers (..))
+import           Data.Morpheus.Types.Internal.Data       (DataField (..))
+import           Data.Morpheus.Types.Internal.DataD      (ConsD (..), TypeD (..))
+import           Data.Morpheus.Types.Internal.TH         (applyT, instanceHeadMultiT, typeT)
+import           Data.Morpheus.Types.Internal.Validation (ResolveT)
+import           Data.Morpheus.Types.Internal.Value      (GQLValue (..))
+import           Data.Morpheus.Types.Resolver
 
 deriveEncode :: TypeD -> Q [Dec]
-deriveEncode TypeD {tName, tCons = [ConsD {cFields}]} = pure <$> instanceD (cxt []) appHead methods
+deriveEncode TypeD {tName, tCons = [ConsD {cFields}]} = pure <$> instanceD (cxt constrains) appHead methods
   where
-    appHead = instanceHeadMultiT ''ObjectResolvers (typeT (mkName tName) ["m"]) ["value"]
+    resolveT = typeT ''ResolveT ["m", "value"]
+    -- defines Type : ResolveT m value
+    -------------------------------------------------
+    -- defines Constraint: (Typeable m, Monad m, GQLValue (ResolveT m value), GQLValue value)
+    constrains =
+      [typeT ''Typeable ["m"], typeT ''Monad ["m"], typeT ''GQLValue ["value"], appT (conT ''GQLValue) resolveT]
+    -------------------------------------------------------------------
+    -- defines:  instance <constraint> =>  ObjectResolvers (<Type> (ResolveT m)) (ResolveT m value) where
+    appHead = instanceHeadMultiT ''ObjectResolvers (applyT (mkName tName) [typeT ''Resolver ["m"]]) [resolveT]
+    ------------------------------------------------------------------
     methods = [funD 'objectResolvers [clause argsE (normalB body) []]]
       where
         argsE = [conP (mkName tName) $ map (varP . mkName) varNames]
-        body = listE $ map decodeVar [] --varNames
+        body = listE $ map decodeVar varNames
         decodeVar name = [|(name, encode $(varName))|]
           where
             varName = varE $ mkName name
