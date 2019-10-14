ebbf801e74dce1185d0ed9635d98711feceaa337
nalchevanidze <d.nalchevanidze@gmail.com>
2019-10-14 19:36:31 +0200

    fix tests

--- test/Feature/Schema/API.hs
+++ test/Feature/Schema/API.hs
@@ -10,7 +10,7 @@ module Feature.Schema.API
 
 import           Data.Morpheus       (interpreter)
 import           Data.Morpheus.Kind  (OBJECT)
-import           Data.Morpheus.Types (GQLRequest, GQLResponse, GQLRootResolver (..), GQLType (..))
+import           Data.Morpheus.Types (GQLRequest, GQLResponse, GQLRootResolver (..), GQLType (..), Undefined (..))
 import           Data.Text           (Text)
 import qualified Feature.Schema.A2   as A2 (A (..))
 import           GHC.Generics        (Generic)
@@ -23,17 +23,17 @@ data A = A
 instance GQLType A where
   type KIND A = OBJECT
 
-data Query = Query
+data Query (m :: * -> * ) = Query
   { a1 :: A
   , a2 :: A2.A
   } deriving (Generic, GQLType)
 
-rootResolver :: GQLRootResolver IO () Query () ()
+rootResolver :: GQLRootResolver IO () Query Undefined Undefined
 rootResolver =
   GQLRootResolver
     { queryResolver = return Query {a1 = A "" 0, a2 = A2.A 0}
-    , mutationResolver = return ()
-    , subscriptionResolver = return ()
+    , mutationResolver = return Undefined
+    , subscriptionResolver = return Undefined
     }
 
 api :: GQLRequest -> IO GQLResponse
