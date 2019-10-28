0be6acb8255c9e827da1959f5611cd593f228cca
nalchevanidze <d.nalchevanidze@gmail.com>
2019-10-29 00:14:47 +0100

    patterns inputValueDefinition

--- src/Data/Morpheus/Parsing/Internal/Pattern.hs
+++ src/Data/Morpheus/Parsing/Internal/Pattern.hs
@@ -1,15 +1,41 @@
-module Data.Morpheus.Parsing.Internal.Pattern (
-  directive ) where
+module Data.Morpheus.Parsing.Internal.Pattern 
+  ( directive 
+  , inputValueDefinition 
+  ) where
 
 
-
+import           Text.Megaparsec                         (label, optional)
 import           Text.Megaparsec.Char                    (char)
--- MORPHEUS
-import           Data.Morpheus.Parsing.Internal.Internal (Parser)
-import           Data.Morpheus.Parsing.Internal.Terms    (parseAssignment, parseTuple, qualifier)
-import           Data.Morpheus.Parsing.Internal.Value    (parseValue)
-import           Debug.Trace                             (traceShowId)
 
+-- MORPHEUS
+import           Data.Morpheus.Parsing.Internal.Create   (createField)
+import           Data.Morpheus.Parsing.Internal.Internal (Parser)
+import           Data.Morpheus.Parsing.Internal.Terms    (parseAssignment, parseNonNull, parseTuple, parseWrappedType,
+                                                          qualifier)
+import           Data.Morpheus.Parsing.Internal.Value    (parseDefaultValue, parseValue)
+import           Data.Morpheus.Types.Internal.Data       (DataField, Key, WrapperD, toHSWrappers)
+
+
+typeDefinition :: Parser ([WrapperD],Key)
+typeDefinition = do
+    (wrappers, fieldType) <- parseWrappedType
+    nonNull <- parseNonNull
+    return (toHSWrappers $ nonNull ++ wrappers, fieldType)
+
+-- InputValueDefinition:
+--
+--   Description(opt) Name : Type DefaultValue(opt) Directives (Const)(opt)
+--   {
+--      url: https://graphql.github.io/graphql-spec/June2018/#InputValueDefinition
+--      usedIn: ArgumentsDefinition , InputObjectTypeDefinition
+--   }
+inputValueDefinition ::  Parser (Key, DataField)
+inputValueDefinition = label "entry" $ do
+    ((fieldName, _), (wrappers, fieldType)) <- parseAssignment qualifier typeDefinition
+    -- TODO: handle default value
+    defaultValue <- parseDefaultValue
+    _ <- optional directive
+    return (fieldName, createField [] fieldName (wrappers, fieldType))
 
 
 -- @directive ( arg1: "value" , .... )
@@ -19,5 +45,4 @@ directive = do
     _ <- char '@'
     _name <- qualifier
     _boo <- parseTuple (parseAssignment qualifier parseValue) -- TODO: string
-    let _ = traceShowId ("Trace Show",_boo,_name)
     pure ()
