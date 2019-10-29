5843af98af7ae9446327477c9d4fd83244f4d089
nalchevanidze <d.nalchevanidze@gmail.com>
2019-10-29 22:30:52 +0100

    add directive to selectionField

--- src/Data/Morpheus/Parsing/Request/Body.hs
+++ src/Data/Morpheus/Parsing/Request/Body.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Data.Morpheus.Parsing.Request.Body
-  ( entries
+  ( parseSelectionSet
   ) where
 
 import           Data.Text                                     (Text)
@@ -11,11 +11,61 @@ import           Text.Megaparsec                               (label, try, (<|>
 --
 -- MORPHEUS
 import           Data.Morpheus.Parsing.Internal.Internal       (Parser, getLocation)
-import           Data.Morpheus.Parsing.Internal.Terms          (onType, parseAlias, setOf, spreadLiteral, token)
+import           Data.Morpheus.Parsing.Internal.Pattern        (optionalDirectives)
+import           Data.Morpheus.Parsing.Internal.Terms          (onType, parseAlias, parseName, setOf, spreadLiteral,
+                                                                token)
 import           Data.Morpheus.Parsing.Request.Arguments       (maybeArguments)
 import           Data.Morpheus.Types.Internal.AST.RawSelection (Fragment (..), RawArguments, RawSelection (..),
                                                                 RawSelectionSet, Reference (..))
 import           Data.Morpheus.Types.Internal.AST.Selection    (Selection (..))
+
+
+-- Selection Sets : https://graphql.github.io/graphql-spec/June2018/#sec-Selection-Sets
+--
+-- SelectionSet:
+--  { Selection(list) }
+--
+-- Selection:
+--   Field
+--   FragmentSpread
+--   InlineFragment
+--
+parseSelectionSet :: Parser RawSelectionSet
+parseSelectionSet = label "SelectionSet" $ setOf parseSelection
+  where
+    parseSelection = label "Selection" $
+            try inlineFragment
+        <|> try spread
+        <|> parseSelectionField
+
+-- Fields: https://graphql.github.io/graphql-spec/June2018/#sec-Language.Fields
+--
+-- Field
+-- Alias(opt) Name Arguments(opt) Directives(opt) SelectionSet(opt)
+--
+parseSelectionField :: Parser (Text, RawSelection)
+parseSelectionField =
+  label "SelectionField" $ do
+    position <- getLocation
+    aliasName  <- parseAlias
+    name <- parseName
+    arguments <- maybeArguments
+    -- TODO: handle Directives
+    _directives <- optionalDirectives
+    value <- selSet aliasName arguments <|> buildField aliasName arguments position
+    return (name, value)
+  where
+    ----------------------------------------
+    buildField selectionAlias selectionArguments selectionPosition =
+       pure (RawSelectionField $ Selection { selectionAlias , selectionArguments,  selectionRec = (), selectionPosition})
+    -----------------------------------------
+    selSet :: Maybe Text -> RawArguments -> Parser RawSelection
+    selSet selectionAlias selectionArguments =
+      label "body" $ do
+        selectionPosition <- getLocation
+        selectionRec <- parseSelectionSet
+        return (RawSelectionSet $ Selection {selectionAlias , selectionArguments, selectionRec, selectionPosition})
+
 
 spread :: Parser (Text, RawSelection)
 spread =
@@ -29,41 +79,5 @@ inlineFragment =
   label "InlineFragment" $ do
     fragmentPosition <- spreadLiteral
     fragmentType <- onType
-    fragmentSelection <- entries
+    fragmentSelection <- parseSelectionSet
     pure ("INLINE_FRAGMENT", InlineFragment $ Fragment {fragmentType, fragmentSelection, fragmentPosition})
-
-{-
-  accept:
-  - field
-  - field {...}
-  - field (...)
-  - field () {...}
--}
-
-parseSelectionField :: Parser (Text, RawSelection)
-parseSelectionField =
-  label "SelectionField" $ do
-    position <- getLocation
-    aliasName  <- parseAlias
-    name <- token
-    arguments <- maybeArguments
-    value <- selSet aliasName arguments <|> buildField aliasName arguments position
-    return (name, value)
-  where
-    ----------------------------------------
-    buildField selectionAlias selectionArguments selectionPosition =
-       pure (RawSelectionField $ Selection { selectionAlias , selectionArguments,  selectionRec = (), selectionPosition})
-    -----------------------------------------
-    selSet :: Maybe Text -> RawArguments -> Parser RawSelection
-    selSet selectionAlias selectionArguments =
-      label "body" $ do
-        selectionPosition <- getLocation
-        selectionRec <- entries
-        return (RawSelectionSet $ Selection {selectionAlias , selectionArguments, selectionRec, selectionPosition})
-
-
-entries :: Parser RawSelectionSet
-entries = label "entries" $ setOf entry
-  where
-    entry = label "entry" $ try inlineFragment <|> try spread <|> parseSelectionField
-
