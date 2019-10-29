31290172fe7a1362d9e86e4decfecbe7f37b35fc
nalchevanidze <d.nalchevanidze@gmail.com>
2019-10-29 19:09:34 +0100

    Formalization

--- src/Data/Morpheus/Parsing/Internal/Pattern.hs
+++ src/Data/Morpheus/Parsing/Internal/Pattern.hs
@@ -9,7 +9,7 @@ import           Text.Megaparsec                         (label, optional)
 import           Text.Megaparsec.Char                    (char)
 
 -- MORPHEUS
-import           Data.Morpheus.Parsing.Internal.Create   (createArgument, createField)
+import           Data.Morpheus.Parsing.Internal.Create   (createField)
 import           Data.Morpheus.Parsing.Internal.Internal (Parser)
 import           Data.Morpheus.Parsing.Internal.Terms    (parseAssignment, parseMaybeTuple, parseNonNull, parseTuple,
                                                           parseWrappedType, qualifier, setOf)
@@ -29,12 +29,13 @@ typeDefinition = do
 --   Description(opt) Name : Type DefaultValue(opt) Directives (Const)(opt)
 --
 inputValueDefinition ::  Parser (Key, DataField)
-inputValueDefinition = label "entry" $ do
+inputValueDefinition = label "InputValueDefinition" $ do
     ((fieldName, _), (wrappers, fieldType)) <- parseAssignment qualifier typeDefinition
     -- TODO: handle default value
     defaultValue <- parseDefaultValue
     _ <- optional directive
     return (fieldName, createField [] fieldName (wrappers, fieldType))
+
 
 -- Field Arguments: https://graphql.github.io/graphql-spec/June2018/#sec-Field-Arguments
 --
@@ -44,19 +45,21 @@ inputValueDefinition = label "entry" $ do
 argumentsDefinition :: Parser [(Key, DataField)]
 argumentsDefinition = label "ArgumentsDefinition" $  parseMaybeTuple inputValueDefinition
 
+
 --  FieldsDefinition : https://graphql.github.io/graphql-spec/June2018/#FieldsDefinition
 --
 --  FieldsDefinition :
 --    { FieldDefinition(list) }
 --
---  FieldDefinition
---    Description(opt) Name ArgumentsDefinition(opt) : Type Directives(Const)(opt)
---
 fieldsDefinition :: Parser [(Key, DataField)]
-fieldsDefinition = label "entries" $ setOf (entryWith argumentsDefinition)
-    where
-        entryWith :: Parser [(Key, DataField)] -> Parser (Key, DataField)
-        entryWith argsParser = label "entry" $ do
+fieldsDefinition = label "entries" $ setOf fieldDefinition 
+ 
+       
+ --  FieldDefinition
+ --    Description(opt) Name ArgumentsDefinition(opt) : Type Directives(Const)(opt)
+ --       
+fieldDefinition ::  Parser (Key, DataField)
+fieldDefinition  = label "entry" $ do
             ((fieldName, fieldArgs), (wrappers, fieldType)) <- parseAssignment fieldWithArgs parseWrappedType
             nonNull <- parseNonNull
             _ <- optional directive
@@ -65,8 +68,9 @@ fieldsDefinition = label "entries" $ setOf (entryWith argumentsDefinition)
                 fieldWithArgs =
                   label "fieldWithArgs" $ do
                     (name, _) <- qualifier
-                    args <- argsParser
+                    args <- argumentsDefinition
                     return (name, args)
+
 
 -- @directive ( arg1: "value" , .... )
 -- TODO:  returns real DataType
