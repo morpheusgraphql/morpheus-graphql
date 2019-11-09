d5e7af3a31e3ed6527c0051f3c22b4977b1779d8
nalchevanidze <d.nalchevanidze@gmail.com>
2019-11-09 23:25:35 +0100

    Fix/Morpheus Client supports Subscription and Mutation (#302)
    * moveGetOperation
    
    * refactor Validation
    
    * fix operation selection
    
    * subEmail
    
    * update changelog

--- src/Data/Morpheus/Validation/Query/Validation.hs
+++ src/Data/Morpheus/Validation/Query/Validation.hs
@@ -5,44 +5,51 @@
 
 module Data.Morpheus.Validation.Query.Validation
   ( validateRequest
-  ) where
+  )
+where
 
-import           Data.Map                                   (fromList)
-import           Data.Morpheus.Error.Mutation               (mutationIsNotDefined)
-import           Data.Morpheus.Error.Subscription           (subscriptionIsNotDefined)
-import           Data.Morpheus.Types.Internal.AST.Operation (Operation (..), RawOperation, ValidOperation,
-                                                             getOperationName)
-import           Data.Morpheus.Types.Internal.Data          (DataObject, DataTypeLib (..), OperationType (..))
-import           Data.Morpheus.Types.Internal.Validation    (Validation)
-import           Data.Morpheus.Types.Types                  (GQLQueryRoot (..))
-import           Data.Morpheus.Validation.Internal.Utils    (VALIDATION_MODE)
-import           Data.Morpheus.Validation.Query.Fragment    (validateFragments)
-import           Data.Morpheus.Validation.Query.Selection   (validateSelectionSet)
-import           Data.Morpheus.Validation.Query.Variable    (resolveOperationVariables)
+import           Data.Map                       ( fromList )
+import           Data.Morpheus.Types.Internal.AST.Operation
+                                                ( Operation(..)
+                                                , ValidOperation
+                                                , getOperationName
+                                                , getOperationDataType
+                                                )
+import           Data.Morpheus.Types.Internal.Data
+                                                ( DataTypeLib(..) )
+import           Data.Morpheus.Types.Internal.Validation
+                                                ( Validation )
+import           Data.Morpheus.Types.Types      ( GQLQueryRoot(..) )
+import           Data.Morpheus.Validation.Internal.Utils
+                                                ( VALIDATION_MODE )
+import           Data.Morpheus.Validation.Query.Fragment
+                                                ( validateFragments )
+import           Data.Morpheus.Validation.Query.Selection
+                                                ( validateSelectionSet )
+import           Data.Morpheus.Validation.Query.Variable
+                                                ( resolveOperationVariables )
 
-getOperationDataType :: RawOperation -> DataTypeLib -> Validation DataObject
-getOperationDataType Operation {operationType = Query} lib = pure $ snd $ query lib
-getOperationDataType Operation {operationType = Mutation, operationPosition} lib =
-  case mutation lib of
-    Just (_, mutation') -> pure mutation'
-    Nothing             -> Left $ mutationIsNotDefined operationPosition
-getOperationDataType Operation {operationType = Subscription, operationPosition} lib =
-  case subscription lib of
-    Just (_, subscription') -> pure subscription'
-    Nothing                 -> Left $ subscriptionIsNotDefined operationPosition
 
-validateRequest :: DataTypeLib -> VALIDATION_MODE -> GQLQueryRoot -> Validation ValidOperation
-validateRequest lib validationMode GQLQueryRoot { fragments
-                                                , inputVariables
-                                                , operation = rawOperation@Operation { operationName
-                                                                                     , operationType
-                                                                                     , operationSelection
-                                                                                     , operationPosition
-                                                                                     }
-                                                } = do
-  operationDataType <- getOperationDataType rawOperation lib
-  variables <- resolveOperationVariables lib fragments (fromList inputVariables) validationMode rawOperation
-  validateFragments lib fragments operationSelection
-  selection <-
-    validateSelectionSet lib fragments (getOperationName operationName) variables operationDataType operationSelection
-  pure $ Operation {operationName, operationType, operationArgs = [], operationSelection = selection, operationPosition}
+validateRequest
+  :: DataTypeLib -> VALIDATION_MODE -> GQLQueryRoot -> Validation ValidOperation
+validateRequest lib validationMode GQLQueryRoot { fragments, inputVariables, operation = rawOperation@Operation { operationName, operationType, operationSelection, operationPosition } }
+  = do
+    operationDataType <- getOperationDataType rawOperation lib
+    variables         <- resolveOperationVariables lib
+                                                   fragments
+                                                   (fromList inputVariables)
+                                                   validationMode
+                                                   rawOperation
+    validateFragments lib fragments operationSelection
+    selection <- validateSelectionSet lib
+                                      fragments
+                                      (getOperationName operationName)
+                                      variables
+                                      operationDataType
+                                      operationSelection
+    pure $ Operation { operationName
+                     , operationType
+                     , operationArgs      = []
+                     , operationSelection = selection
+                     , operationPosition
+                     }
