cb24789e42c955226133537c107aa6d266b423f7
nalchevanidze <d.nalchevanidze@gmail.com>
2019-11-12 23:18:39 +0100

    TH

--- src/Data/Morpheus/Types/Internal/TH.hs
+++ src/Data/Morpheus/Types/Internal/TH.hs
@@ -1,25 +1,9 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE CPP #-}
 
 
 module Data.Morpheus.Types.Internal.TH where
 
-import           Data.Text                  (Text, pack, unpack)
 import           Language.Haskell.TH
-import           Language.Haskell.TH.Syntax
-
-liftMaybeText :: Maybe Text -> ExpQ
-liftMaybeText (Just x) = appE  (conE 'Just) (liftText x)
-liftMaybeText Nothing  = conE 'Nothing
-
-liftText :: Text -> ExpQ
-liftText x = appE (varE 'pack) (lift (unpack x))
-
-liftTextTuple :: Lift a => (Text, a) -> ExpQ
-liftTextTuple (name, x) = tupE [liftText name, lift x]
-
-liftTextMap :: Lift a => [(Text, a)] -> ExpQ
-liftTextMap = listE . map liftTextTuple
 
 apply :: Name -> [Q Exp] -> Q Exp
 apply n = foldl appE (conE n)
