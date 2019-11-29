4ee9aca9cafa86b2cdad79369f5c683fc06b9be7
nalchevanidze <d.nalchevanidze@gmail.com>
2019-11-29 11:28:49 +0100

    Some int

---
+++ test/Feature/TypeInference/API.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+
+module Feature.TypeInference.API
+  ( api
+  )
+where
+
+import           Data.Morpheus                  ( interpreter )
+import           Data.Morpheus.Types            ( GQLRequest
+                                                , GQLResponse
+                                                , GQLRootResolver(..)
+                                                , GQLType(..)
+                                                , Undefined(..)
+                                                )
+import           Data.Text                      ( Text )
+import           GHC.Generics                   ( Generic )
+
+data Power = Thunderbolts | Shapeshift | Hurricanes
+  deriving (Generic,GQLType)
+
+data Deity = Deity {
+  name :: Text ,
+  power :: Power
+} deriving(Generic,GQLType)
+
+deityRes :: Deity
+deityRes = Deity { name = "Morpheus", power = Shapeshift }
+
+
+data Character  =
+  CharacterDeity Deity -- Only <tycon name><type ref name> should generate direct link
+  -- RECORDS
+  | Creature { creatureName :: Text, creatureAge :: Int }
+  | BoxedDeity { boxedDeity :: Deity}
+  | SomeScalarRecord { scalarText :: Text }
+  --- Types 
+  | SomeDeity Deity
+  | CharacterInt Int -- should bex boxed, is not Object type
+  | SomeMutli Int Text
+  --- ENUMS
+  | Zeus
+  | Cronus deriving (Generic, GQLType)
+
+data Query (m :: * -> *) = Query
+  { deity :: Deity,
+    character :: [Character]
+  } deriving (Generic, GQLType)
+
+rootResolver :: GQLRootResolver IO () Query Undefined Undefined
+rootResolver = GQLRootResolver
+  { queryResolver        = Query { deity = deityRes, character }
+  , mutationResolver     = Undefined
+  , subscriptionResolver = Undefined
+  }
+ where
+  character :: [Character]
+  character =
+    [ CharacterDeity deityRes
+    , Creature { creatureName = "Lamia", creatureAge = 205 }
+    , BoxedDeity { boxedDeity = deityRes }
+    , SomeScalarRecord { scalarText = "Some Text" }
+      ---
+    , SomeDeity deityRes
+    , CharacterInt 12
+    , SomeMutli 21 "some text"
+    , Zeus
+    , Cronus
+    ]
+
+
+api :: GQLRequest -> IO GQLResponse
+api = interpreter rootResolver
