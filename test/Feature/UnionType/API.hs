d4ddfe100b7a564598fdd7bb06979a551d84c5a1
nalchevanidze <d.nalchevanidze@gmail.com>
2019-12-02 19:52:19 +0100

    FixUnion Type

--- test/Feature/UnionType/API.hs
+++ test/Feature/UnionType/API.hs
@@ -6,14 +6,22 @@
 
 module Feature.UnionType.API
   ( api
-  ) where
+  )
+where
 
-import           Data.Morpheus       (interpreter)
-import           Data.Morpheus.Kind  (OBJECT, UNION)
-import           Data.Morpheus.Types (GQLRequest, GQLResponse, GQLRootResolver (..), GQLType (..), IORes,
-                                      Undefined (..))
-import           Data.Text           (Text)
-import           GHC.Generics        (Generic)
+import           Data.Morpheus                  ( interpreter )
+import           Data.Morpheus.Kind             ( OBJECT
+                                                , UNION
+                                                )
+import           Data.Morpheus.Types            ( GQLRequest
+                                                , GQLResponse
+                                                , GQLRootResolver(..)
+                                                , GQLType(..)
+                                                , IORes
+                                                , Undefined(..)
+                                                )
+import           Data.Text                      ( Text )
+import           GHC.Generics                   ( Generic )
 
 instance GQLType A where
   type KIND A = OBJECT
@@ -24,8 +32,8 @@ instance GQLType B where
 instance GQLType C where
   type KIND C = OBJECT
 
-instance GQLType AOrB where
-  type KIND AOrB = UNION
+instance GQLType Sum where
+  type KIND Sum = UNION
 
 data A = A
   { aText :: Text
@@ -42,26 +50,28 @@ data C = C
   , cInt  :: Int
   } deriving (Generic)
 
-data AOrB
-  = A' A
-  | B' B
+data Sum
+  = SumA A
+  | SumB B
   deriving (Generic)
 
 data Query m = Query
-  { union :: () -> m [AOrB]
+  { union :: () -> m [Sum]
   , fc    :: C
   } deriving (Generic, GQLType)
 
-resolveUnion :: () -> IORes () [AOrB]
-resolveUnion _ = return [A' A {aText = "at", aInt = 1}, B' B {bText = "bt", bInt = 2}]
+resolveUnion :: () -> IORes () [Sum]
+resolveUnion _ =
+  return [SumA A { aText = "at", aInt = 1 }, SumB B { bText = "bt", bInt = 2 }]
 
 rootResolver :: GQLRootResolver IO () Query Undefined Undefined
-rootResolver =
-  GQLRootResolver
-    { queryResolver = Query {union = resolveUnion, fc = C {cText = "", cInt = 3}}
-    , mutationResolver = Undefined
-    , subscriptionResolver = Undefined
-    }
+rootResolver = GQLRootResolver
+  { queryResolver        = Query { union = resolveUnion
+                                 , fc    = C { cText = "", cInt = 3 }
+                                 }
+  , mutationResolver     = Undefined
+  , subscriptionResolver = Undefined
+  }
 
 api :: GQLRequest -> IO GQLResponse
 api = interpreter rootResolver
