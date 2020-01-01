6a77bf6fd99a2712b815a2115e9d209bb704bdd4
davit nalchevanidze <d.nalchevanidze@gmail.com>
2020-01-01 23:55:07 +0100

    update

--- release.sh
+++ release.sh
@@ -2,12 +2,15 @@
 export MORPHEUS_VESRION="0.9.0"
 export DIST_PATH="./dist"
 export MORPHEUS_RELEASE_NAME="morpheus-graphql-$MORPHEUS_VESRION"
+export MORPHEUS_RELEASE_TAR="$MORPHEUS_RELEASE_NAME.tar.gz" 
 
 # generate sdsit
 stack sdist . --tar-dir $DIST_PATH
 
 # unppack prod
 cd $DIST_PATH
-tar -zxvf $MORPHEUS_RELEASE_NAME.tar.gz
-cd $MORPHEUS_RELEASE_NAME
+tar -zxvf $MORPHEUS_RELEASE_TAR
+mv $MORPHEUS_RELEASE_NAME release
+rm $MORPHEUS_RELEASE_TAR
+cd release
 cd ../..
