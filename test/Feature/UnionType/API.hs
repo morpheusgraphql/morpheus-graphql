5f5a6cadbc4e440140e69803a25289619faa29fd
davit nalchevanidze <d.nalchevanidze@gmail.com>
2020-01-01 19:28:05 +0100

    emove depricated type instances for Object

--- test/Feature/UnionType/API.hs
+++ test/Feature/UnionType/API.hs
@@ -10,9 +10,6 @@ module Feature.UnionType.API
 where
 
 import           Data.Morpheus                  ( interpreter )
-import           Data.Morpheus.Kind             ( OBJECT
-                                                , UNION
-                                                )
 import           Data.Morpheus.Types            ( GQLRequest
                                                 , GQLResponse
                                                 , GQLRootResolver(..)
@@ -23,37 +20,25 @@ import           Data.Morpheus.Types            ( GQLRequest
 import           Data.Text                      ( Text )
 import           GHC.Generics                   ( Generic )
 
-instance GQLType A where
-  type KIND A = OBJECT
-
-instance GQLType B where
-  type KIND B = OBJECT
-
-instance GQLType C where
-  type KIND C = OBJECT
-
-instance GQLType Sum where
-  type KIND Sum = UNION
-
 data A = A
   { aText :: Text
   , aInt  :: Int
-  } deriving (Generic)
+  } deriving (Generic, GQLType)
 
 data B = B
   { bText :: Text
   , bInt  :: Int
-  } deriving (Generic)
+  } deriving (Generic, GQLType)
 
 data C = C
   { cText :: Text
   , cInt  :: Int
-  } deriving (Generic)
+  } deriving (Generic, GQLType)
 
 data Sum
   = SumA A
   | SumB B
-  deriving (Generic)
+  deriving (Generic, GQLType)
 
 data Query m = Query
   { union :: () -> m [Sum]
