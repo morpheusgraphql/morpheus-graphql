570e06017cd27d82bb26cc240a4c4deb7141ad95
nalchevanidze <d.nalchevanidze@gmail.com>
2020-01-02 00:33:51 +0100

    check-release (#359)
    * check-release
    
    * update config
    
    * cabal test
    
    * update
    
    * update release.sh
    
    * update
    
    * update
    
    * release.bash
    
    * update
    
    * update
    
    * update
    
    * git update
    
    * comment

---
+++ release.sh
@@ -0,0 +1,18 @@
+# vesrion
+export MORPHEUS_VESRION="0.9.0"
+export DIST_PATH="./dist"
+export MORPHEUS_RELEASE_NAME="morpheus-graphql-$MORPHEUS_VESRION"
+export MORPHEUS_RELEASE_TAR="$MORPHEUS_RELEASE_NAME.tar.gz" 
+
+# rm -r $DIST_PATH
+mkdir $DIST_PATH
+# generate sdsit
+cabal sdist -o $DIST_PATH
+rm -r dist-newstyle
+# unppack prod
+cd $DIST_PATH
+tar -zxvf $MORPHEUS_RELEASE_TAR
+mv $MORPHEUS_RELEASE_NAME release
+rm $MORPHEUS_RELEASE_TAR
+cd release
+# cd ../..
