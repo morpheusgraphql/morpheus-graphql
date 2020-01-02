e94495741dcaecae79c5eb43a308782325650811
nalchevanidze <d.nalchevanidze@gmail.com>
2020-01-02 03:05:29 +0100

    Create haskell.yml (#361)
    * Create haskell.yml
    
    * update
    
    * update
    
    * deps
    
    * sa
    
    * update
    
    * add cabal
    
    * build
    
    * update
    
    * release sh
    
    * echo PWD
    
    * update
    
    * externaf folder
    
    * dist path
    
    * quick check
    
    * s
    
    * s
    
    * env
    
    * release
    
    * home
    
    * update
    
    * update
    
    * gp
    
    * update
    
    * update
    
    * gp
    
    * test  dist
    
    * update
    
    * update
    
    * update

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 # vesrion
 export MORPHEUS_VESRION="0.9.0"
-export DIST_PATH="./dist"
 export MORPHEUS_RELEASE_NAME="morpheus-graphql-$MORPHEUS_VESRION"
 export MORPHEUS_RELEASE_TAR="$MORPHEUS_RELEASE_NAME.tar.gz" 
 
@@ -15,4 +14,9 @@ tar -zxvf $MORPHEUS_RELEASE_TAR
 mv $MORPHEUS_RELEASE_NAME release
 rm $MORPHEUS_RELEASE_TAR
 cd release
+echo $PWD
+ls
 # cd ../..
+
+cabal update
+cabal build --only-dependencies
