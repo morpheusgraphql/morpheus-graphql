bc112e5e344d178e7e6296325fd9ebfe5f6559fb
davit nalchevanidze <d.nalchevanidze@gmail.com>
2020-03-24 13:59:38 +0100

    change Internal

--- src/Data/Morpheus/Parsing/Internal/Internal.hs
+++ src/Data/Morpheus/Parsing/Internal/Internal.hs
@@ -5,9 +5,11 @@ module Data.Morpheus.Parsing.Internal.Internal
   , Position
   , processErrorBundle
   , getLocation
+  , processParser
   )
 where
 
+-- import            Data.Set  (toList)
 import qualified Data.List.NonEmpty            as NonEmpty
 import           Data.Morpheus.Error.Utils      ( toLocation )
 import           Data.Morpheus.Types.Internal.AST
@@ -15,11 +17,12 @@ import           Data.Morpheus.Types.Internal.AST
 import           Data.Morpheus.Types.Internal.Resolving
                                                 ( GQLError(..)
                                                 , GQLErrors
+                                                , Validation
+                                                , failure
                                                 )
 import           Data.Text                      ( Text
                                                 , pack
                                                 )
-import           Data.Void                      ( Void )
 import           Text.Megaparsec                ( ParseError
                                                 , ParseErrorBundle
                                                   ( ParseErrorBundle
@@ -32,25 +35,55 @@ import           Text.Megaparsec                ( ParseError
                                                 , errorOffset
                                                 , getSourcePos
                                                 , parseErrorPretty
+                                                , runParser
                                                 )
-
+-- import          Text.Megaparsec.Error (ParseError(..) , ErrorFancy(..))
+import           Data.Void                      ( Void )
 
 getLocation :: Parser Position
 getLocation = fmap toLocation getSourcePos
 
-type Parser = Parsec Void Text
+type MyError = Void
+type Parser = Parsec MyError Text
+type ErrorBundle = ParseErrorBundle Text MyError
 
-processErrorBundle :: ParseErrorBundle Text Void -> GQLErrors
+processParser :: Parser a -> Text -> Validation a
+processParser parser txt = case runParser parser [] txt of
+    Right root       -> pure root
+    Left  parseError -> failure (processErrorBundle parseError)
+
+
+processErrorBundle :: ErrorBundle -> GQLErrors
 processErrorBundle = fmap parseErrorToGQLError . bundleToErrors
  where
-  parseErrorToGQLError :: (ParseError Text Void, SourcePos) -> GQLError
+  parseErrorToGQLError :: (ParseError Text MyError, SourcePos) -> GQLError
   parseErrorToGQLError (err, position) = GQLError
     { message   = pack (parseErrorPretty err)
     , locations = [toLocation position]
     }
   bundleToErrors
-    :: ParseErrorBundle Text Void -> [(ParseError Text Void, SourcePos)]
+    :: ErrorBundle -> [(ParseError Text MyError, SourcePos)]
   bundleToErrors ParseErrorBundle { bundleErrors, bundlePosState } =
     NonEmpty.toList $ fst $ attachSourcePos errorOffset
                                             bundleErrors
                                             bundlePosState
+
+-- processErrorBundle :: ErrorBundle -> GQLErrors
+-- processErrorBundle = concatMap parseErrorToGQLError . bundleToErrors
+--  where
+--   parseErrorToGQLError :: (ParseError Text GQLErrors, SourcePos) -> GQLErrors
+--   parseErrorToGQLError (FancyError _ ls,_pos) = concatMap bla (toList ls)
+--     where 
+--         bla (ErrorCustom e) = e
+--   parseErrorToGQLError (err, position) =
+--     -- [GQLError
+--     --   { message   = pack (parseErrorPretty err)
+--     --     , locations = [toLocation position]
+--     --   }
+--     -- ]
+--   bundleToErrors
+--     :: ErrorBundle -> [(ParseError Text GQLErrors, SourcePos)]
+--   bundleToErrors ParseErrorBundle { bundleErrors, bundlePosState } =
+--     NonEmpty.toList $ fst $ attachSourcePos errorOffset
+--                                             bundleErrors
+--                                             bundlePosState
