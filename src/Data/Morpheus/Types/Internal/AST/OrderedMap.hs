18fb998b4b01b99a6eb0fa25bcafebf04c327ee5
davit nalchevanidze <d.nalchevanidze@gmail.com>
2020-03-31 10:24:16 +0200

    merge

--- src/Data/Morpheus/Types/Internal/AST/OrderedMap.hs
+++ src/Data/Morpheus/Types/Internal/AST/OrderedMap.hs
@@ -71,7 +71,7 @@ instance Traversable OrderedMap where
   traverse f (OrderedMap names values) = OrderedMap names <$> traverse f values
 
 instance NameCollision a => Join (OrderedMap a) where 
-  (OrderedMap k1 x) <:> (OrderedMap k2 y) = OrderedMap (k1 <> k2) <$> safeJoin x y
+  merge _ (OrderedMap k1 x)  (OrderedMap k2 y) = OrderedMap (k1 <> k2) <$> safeJoin x y
 
 instance Empty (OrderedMap a) where 
   empty = OrderedMap [] HM.empty
