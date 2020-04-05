866a52255263df939f20449c27e66bc7f668b371
davit nalchevanidze <d.nalchevanidze@gmail.com>
2020-04-05 16:34:18 +0200

    remove unused internalError

--- src/Data/Morpheus/Error/Internal.hs
+++ src/Data/Morpheus/Error/Internal.hs
@@ -2,7 +2,6 @@
 
 module Data.Morpheus.Error.Internal
   ( internalTypeMismatch
-  , internalArgumentError
   , internalUnknownTypeMessage
   , internalError
   , internalResolvingError
@@ -36,15 +35,10 @@ internalError x = failure $ globalErrorMessage $ "INTERNAL ERROR: " <> x
 internalResolvingError :: Text -> GQLErrors
 internalResolvingError = globalErrorMessage . ("INTERNAL RESOLVING ERROR:" <>)
 
-
 -- if type did not not found, but was defined by Schema
 internalUnknownTypeMessage :: Text -> GQLErrors
 internalUnknownTypeMessage x = globalErrorMessage
   $ T.concat ["type did not found, but was defined by Schema", x]
-
--- if arguments is already validated but has not found required argument
-internalArgumentError :: Text -> Stateless a
-internalArgumentError x = internalError $ "Argument " <> x
 
 -- if value is already validated but value has different type
 internalTypeMismatch :: Text -> ValidValue -> Stateless a
