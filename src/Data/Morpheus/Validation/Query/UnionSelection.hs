0d7d1b993fcad19bd9f3c34207b5bcc72a5964e2
davit nalchevanidze <d.nalchevanidze@gmail.com>
2020-04-10 19:44:02 +0200

    nn

--- src/Data/Morpheus/Validation/Query/UnionSelection.hs
+++ src/Data/Morpheus/Validation/Query/UnionSelection.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE NamedFieldPuns      #-}
-{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators       #-}
