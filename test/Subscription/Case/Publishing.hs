9173f1a344d09e5e65a3c488f45bb12f7b801b62
davit nalchevanidze <d.nalchevanidze@gmail.com>
2020-05-02 18:46:50 +0200

    simulate

--- test/Subscription/Case/Publishing.hs
+++ test/Subscription/Case/Publishing.hs
@@ -32,7 +32,7 @@ import Subscription.Utils
     apolloStart,
     apolloStop,
     inputsAreConsumed,
-    simulateAll,
+    simulate,
     simulatePublish,
     storeSubscriptions,
     testResponse,
