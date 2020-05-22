1d5b15468f9ecc552fa4f3226c207b554b0c3c03
david <d.nalchevanidze@gmail.com>
2020-05-22 23:28:39 +0200

    feature

--- morpheus-graphql-core/src/Data/Morpheus/Types/ID.hs
+++ morpheus-graphql-core/src/Data/Morpheus/Types/ID.hs
@@ -2,18 +2,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Data.Morpheus.Server.Types.ID
+module Data.Morpheus.Types.ID
   ( ID (..),
   )
 where
 
-import qualified Data.Aeson as A
-import Data.Morpheus.Kind (SCALAR)
-import Data.Morpheus.Server.Types.GQLType (GQLType (..))
-import Data.Morpheus.Types.GQLScalar (GQLScalar (..))
+import Data.Morpheus.Types.GQLScalar
+  ( GQLScalar (..),
+  )
 import Data.Morpheus.Types.Internal.AST
   ( ScalarValue (..),
-    internalFingerprint,
   )
 import Data.Text
   ( Text,
@@ -28,16 +26,6 @@ newtype ID = ID
   { unpackID :: Text
   }
   deriving (Show, Generic)
-
-instance GQLType ID where
-  type KIND ID = SCALAR
-  __typeFingerprint _ = internalFingerprint "ID" []
-
-instance A.ToJSON ID where
-  toJSON = A.toJSON . unpackID
-
-instance A.FromJSON ID where
-  parseJSON = fmap ID . A.parseJSON
 
 instance GQLScalar ID where
   parseValue (Int x) = return (ID $ pack $ show x)
