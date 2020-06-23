470dee67b36b4c29195291f37b368127335e0b71
david <d.nalchevanidze@gmail.com>
2020-06-23 15:18:02 +0200

    servant

--- morpheus-graphql-examples-servant/src/Server/ServantGQL.hs
+++ morpheus-graphql-examples-servant/src/Server/ServantGQL.hs
@@ -13,14 +13,12 @@ module Server.ServantGQL
 where
 
 import Control.Monad.Trans (MonadIO, liftIO)
-import qualified Data.ByteString.Lazy as L
-  ( readFile,
-  )
 import Data.ByteString.Lazy.Char8
   ( ByteString,
   )
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Morpheus.Types (GQLRequest, GQLResponse)
+import Data.String (IsString)
 import Data.Typeable (Typeable)
 import GHC.TypeLits
 import Network.HTTP.Media ((//), (/:))
@@ -49,37 +47,47 @@ type GQLAPI = ReqBody '[JSON] GQLRequest :> Post '[JSON] GQLResponse
 type GQLEndpoint (name :: Symbol) = name :> (GQLAPI :<|> Get '[HTML] ByteString)
 
 serveGQLEndpoint :: (GQLRequest -> IO GQLResponse) -> Server (GQLEndpoint name)
-serveGQLEndpoint app = (liftIO . app) :<|> gqlPlayground
+serveGQLEndpoint app = (liftIO . app) :<|> playground
 
-gqlPlayground :: (Monad m, MonadIO m) => m ByteString
-gqlPlayground = pure playground
+link :: (IsString a, Semigroup a) => a -> a -> a
+link rel href = "<link rel=\"" <> rel <> "\"  href=\"" <> href <> "\" />"
 
-playground :: ByteString
+meta :: (IsString a, Monoid a) => [(a, a)] -> a
+meta attr = "<meta " <> mconcat (map renderAttr attr) <> " />"
+  where
+    renderAttr (name, value) = name <> "=\"" <> value <> "\" "
+
+tag :: (Monoid a, IsString a) => a -> [a] -> a
+tag name children = "<" <> name <> ">" <> mconcat children <> "</" <> name <> ">"
+
+playground :: (Monad m, MonadIO m) => m ByteString
 playground =
-  "<!DOCTYPE html> \
-  \  <html> \
-  \    <head> \
-  \      <meta charset=\"utf-8\" /> \
-  \      <meta name=\"viewport\" content=\"user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui\" /> \
-  \      <title>GraphQL Playground</title> \
-  \      <link \
-  \        rel=\"stylesheet\" \
-  \        href=\"//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css\" \
-  \      /> \
-  \      <link \
-  \        rel=\"shortcut icon\" \
-  \        href=\"//cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png\" \
-  \      /> \
-  \      <script  \
-  \         src=\"//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js\"> \
-  \      </script> \
-  \    </head> \
-  \    <body> \
-  \      <div id='root'></div> \
-  \      <script> \
-  \        window.addEventListener('load', (_) => \
-  \          GraphQLPlayground.init(document.getElementById('root'), {}) \
-  \        ); \
-  \      </script> \
-  \    </body> \
-  \  </html>"
+  pure $
+    "<!DOCTYPE html>"
+      <> tag
+        "html"
+        [ tag
+            "head"
+            [ meta [("charset", "utf-8")],
+              meta
+                [ ("name", "viewport"),
+                  ("content", "user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui")
+                ],
+              tag "title" ["GraphQL Playground"],
+              link "stylesheet" "//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css",
+              link "shortcut icon" "//cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png"
+            ],
+          tag
+            "body"
+            [ "<script  \
+              \         src=\"//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js\"> \
+              \      </script> \
+              \    </head>"
+                <> "      <div id='root'></div> \
+                   \      <script> \
+                   \        window.addEventListener('load', (_) => \
+                   \          GraphQLPlayground.init(document.getElementById('root'), {}) \
+                   \        ); \
+                   \      </script>"
+            ]
+        ]
