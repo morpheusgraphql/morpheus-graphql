9495a55ce433635771a8e1720971a9c9e4469142
david <d.nalchevanidze@gmail.com>
2020-06-23 16:36:18 +0200

    scotty

--- morpheus-graphql-examples-scotty/src/Server/Utils.hs
+++ morpheus-graphql-examples-scotty/src/Server/Utils.hs
@@ -3,14 +3,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Server.Scotty
-  ( scottyServer,
+module Server.Utils
+  ( httpEndpoint,
   )
 where
 
 -- examples
 import Client.Client
   ( fetchUser,
+  )
+import Control.Monad.IO.Class (liftIO)
+import Data.ByteString.Lazy.Char8
+  ( ByteString,
   )
 import Data.Functor.Identity (Identity (..))
 import Data.Morpheus.Document (toGraphQLDocument)
@@ -23,18 +27,33 @@ import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.Wai.Handler.WebSockets as WaiWs
 import Network.WebSockets (defaultConnectionOptions)
 import Server.Mythology.API (mythologyApi)
+import Server.Playground (playground)
 import Server.Sophisticated.API
   ( EVENT,
     api,
     gqlRoot,
   )
 import Server.TH.Simple (thSimpleApi)
-import Server.Utils (httpEndpoint)
 import Web.Scotty
-  ( get,
+  ( RoutePattern,
+    ScottyM,
+    body,
+    get,
+    post,
     raw,
     scottyApp,
   )
+
+addPlayground :: RoutePattern -> ScottyM ()
+addPlayground route = get route (raw playground)
+
+httpEndpoint ::
+  RoutePattern ->
+  (ByteString -> IO ByteString) ->
+  ScottyM ()
+httpEndpoint route gqlApi = do
+  addPlayground route
+  post route $ raw =<< (liftIO . gqlApi =<< body)
 
 scottyServer :: IO ()
 scottyServer = do
