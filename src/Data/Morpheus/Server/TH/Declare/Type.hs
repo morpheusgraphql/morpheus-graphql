df9390440ea6ec2991153558032d5677347cd125
david <d.nalchevanidze@gmail.com>
2020-06-30 00:33:04 +0200

    rendewring

--- src/Data/Morpheus/Server/TH/Declare/Type.hs
+++ src/Data/Morpheus/Server/TH/Declare/Type.hs
@@ -24,6 +24,7 @@ import Data.Morpheus.Types.Internal.AST
     FieldContent (..),
     FieldDefinition (..),
     FieldName,
+    OUT,
     TRUE,
     TypeKind (..),
     TypeName,
@@ -97,32 +98,44 @@ renderFieldType ::
   FieldDefinition cat ->
   Type
 renderFieldType tKind FieldDefinition {fieldContent, fieldType} =
-  genFieldT
-    (declareTypeRef fieldType)
-    tKind
-    fieldContent
+  withFieldWrappers tKind fieldContent (declareTypeRef fieldType)
 
 fieldTypeName :: Bool -> TypeName -> FieldName -> Name
 fieldTypeName namespace tName fieldName
   | namespace = mkFieldName (nameSpaceField tName fieldName)
   | otherwise = mkFieldName fieldName
 
+-- withSubscriptionField: t => SubscriptionField t
 withSubscriptionField :: TypeKind -> Type -> Type
 withSubscriptionField tKind x
   | isSubscription tKind = AppT (ConT ''SubscriptionField) x
   | otherwise = x
 
-------------------------------------------------
-genFieldT :: Type -> TypeKind -> Maybe (FieldContent TRUE cat) -> Type
-genFieldT result kind (Just (FieldArgs ArgumentsDefinition {argumentsTypename = Just argsTypename})) =
-  AppT
-    (AppT arrowType argType)
-    (withSubscriptionField kind (AppT m' result))
+-- withArgs: t => a -> t
+withArgs :: TypeName -> Type -> Type
+withArgs argsTypename = AppT (AppT arrowType argType)
   where
     argType = ConT $ mkTypeName argsTypename
     arrowType = ConT ''Arrow
-genFieldT result kind _
-  | isOutputObject kind = withSubscriptionField kind (AppT m' result)
-  | otherwise = result
+
+-- withMonad: t => m t
+withMonad :: Type -> Type
+withMonad = AppT m'
+
+------------------------------------------------
+withFieldWrappers ::
+  TypeKind ->
+  Maybe (FieldContent TRUE cat) ->
+  Type ->
+  Type
+withFieldWrappers kind (Just (FieldArgs ArgumentsDefinition {argumentsTypename = Just argsTypename})) =
+  withArgs argsTypename
+    . withSubscriptionField kind
+    . withMonad
+withFieldWrappers kind _
+  | isOutputObject kind =
+    withSubscriptionField kind
+      . withMonad
+  | otherwise = id
 
 type Arrow = (->)
