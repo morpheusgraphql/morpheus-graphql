279d95ad10f0c6407dcfc5e00f6a759a5bf01483
david <d.nalchevanidze@gmail.com>
2020-08-01 15:36:05 +0200

    schema resolvers

--- morpheus-graphql-core/test/Spec.hs
+++ morpheus-graphql-core/test/Spec.hs
@@ -61,90 +61,22 @@ import Test.Tasty.HUnit
   )
 import Utils.Api
   ( apiTest,
-    assertion,
   )
 import Utils.Schema
   ( testSchema,
   )
 import Prelude
   ( ($),
-    (.),
-    (==),
-    FilePath,
     IO,
-    otherwise,
-    show,
-    uncurry,
   )
-
-apiSchema :: Schema VALID
-apiSchema =
-  [dsl|
-
-  type Query {
-    deity(name: String): Deity!
-  }
-
-  interface Character {
-    name : String
-  }
-
-  interface Supernatural {
-    power: [String!]!
-  }
-
-  type Hero implements Character {
-    name: String
-  }
-
-  type Deity implements Character Supernatural {
-    name: String!
-    power: [String!]!
-  }
-|]
-
-resolver :: Monad m => RootResModel e m
-resolver =
-  RootResModel
-    { query =
-        pure $
-          mkObject
-            "Query"
-            [("deity", resolveDeity)],
-      mutation = pure mkNull,
-      subscription = pure mkNull,
-      channelMap = Nothing
-    }
-
-resolveDeity :: (WithOperation o, Monad m) => Resolver o e m (ResModel o e m)
-resolveDeity =
-  pure $
-    mkObject
-      "Deity"
-      [ ("name", pure $ mkString "Morpheus"),
-        ("power", pure $ mkList [mkString "Shapeshifting"])
-      ]
 
 main :: IO ()
 main = do
   schema <- testSchema
-  defaultMain
-    $ testGroup
+  defaultMain $
+    testGroup
       "core tests"
-    $ fmap
-      (uncurry basicTest)
-      [ ("basic Test", "api/simple"),
-        ("test interface", "api/interface")
+      [ schema,
+        apiTest "api/simple",
+        apiTest "api/interface"
       ]
-      <> [ schema,
-           apiTest "/api/validation/simple"
-         ]
-
-basicTest :: FilePath -> FieldName -> TestTree
-basicTest description path = testCase description $ do
-  actual <- simpleTest <$> getRequest path
-  expected <- expectedResponse path
-  assertion expected actual
-
-simpleTest :: GQLRequest -> ResponseStream e Identity (Value VALID)
-simpleTest = runApi apiSchema resolver defaultConfig
