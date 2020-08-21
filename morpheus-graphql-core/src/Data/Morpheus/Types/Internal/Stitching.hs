1ceedb0c54b101c23e4bcdfdc313d62456ee1aa1
david <d.nalchevanidze@gmail.com>
2020-08-21 20:34:46 +0200

    schema stritching

--- morpheus-graphql-core/src/Data/Morpheus/Types/Internal/Stitching.hs
+++ morpheus-graphql-core/src/Data/Morpheus/Types/Internal/Stitching.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Data.Morpheus.Types.Internal.Stitching
@@ -20,12 +20,11 @@ import Data.Morpheus.Internal.Utils
     mergeWithResolution,
   )
 import Data.Morpheus.Types.Internal.AST
-  ( ANY,
+  ( Directive,
     DirectiveDefinition,
     FieldDefinition,
     Fields (..),
     FieldsDefinition,
-    OBJECT,
     Schema (..),
     TRUE,
     TypeContent (..),
@@ -35,10 +34,9 @@ import Data.Morpheus.Types.Internal.AST
   )
 import qualified Data.Morpheus.Types.Internal.AST.OrdMap as OM (upsert)
 import qualified Data.Morpheus.Types.Internal.AST.SafeHashMap as SHM (upsert)
-import Data.Semigroup ((<>))
+import Data.Semigroup (Semigroup (..))
 import Prelude
-  ( ($),
-    (.),
+  ( (.),
     Eq (..),
     otherwise,
   )
@@ -58,8 +56,19 @@ optional (Just x) (Just y) = Just <$> stitch x y
 prop :: (b -> b -> m b) -> (a -> b) -> a -> a -> m b
 prop f fSel a1 a2 = f (fSel a1) (fSel a2)
 
+equal :: (Eq a, Applicative m, Failure ValidationErrors m) => ValidationErrors -> a -> a -> m a
+equal err p1 p2
+  | p1 == p2 = pure p2
+  | otherwise = failure err
+
+stitchSemiGroup :: (Applicative m, Semigroup a) => a -> a -> m a
+stitchSemiGroup x = pure . (x <>)
+
 class Stitching a where
   stitch :: (Monad m, Failure ValidationErrors m) => a -> a -> m a
+
+instance Stitching a => Stitching (Maybe a) where
+  stitch = optional
 
 instance Stitching (Schema s) where
   stitch s1 s2 =
@@ -74,19 +83,24 @@ instance Stitching (TypeLib s) where
   stitch = mergeWithResolution SHM.upsert stitch
 
 instance Stitching [DirectiveDefinition s] where
-  stitch x = pure . (x <>)
+  stitch = stitchSemiGroup
 
-instance Stitching (TypeDefinition ANY s) where
-  stitch x y = failure [nameCollision y]
+instance Stitching [Directive s] where
+  stitch = stitchSemiGroup
 
-instance Stitching (TypeDefinition OBJECT s) where
-  stitch ty1 TypeDefinition {typeContent = cont2, ..} = do
-    cont <- stitch (typeContent ty1) cont2
-    pure $ TypeDefinition {typeContent = cont, ..}
+instance Stitching (TypeDefinition cat s) where
+  stitch x y =
+    TypeDefinition
+      <$> prop (equal [nameCollision y]) typeName x y
+      <*> prop (equal [nameCollision y]) typeFingerprint x y
+      <*> prop stitchSemiGroup typeDescription x y
+      <*> prop stitch typeDirectives x y
+      <*> prop stitch typeContent x y
 
-instance Stitching (TypeContent TRUE OBJECT s) where
+instance Stitching (TypeContent TRUE cat s) where
   stitch (DataObject i1 fields1) (DataObject i2 fields2) =
     DataObject (i1 <> i2) <$> stitch fields1 fields2
+  stitch _ _ = failure (["schema Stitching works only for objects"] :: ValidationErrors)
 
 instance Stitching (FieldsDefinition cat s) where
   stitch (Fields x) (Fields y) = Fields <$> mergeWithResolution OM.upsert stitch x y
