65520acef648967eb095d59dbaa1bf96007a5c0a
david <d.nalchevanidze@gmail.com>
2020-08-23 04:17:58 +0200

    ok

--- morpheus-graphql-core/test/Utils/MergeSchema.hs
+++ morpheus-graphql-core/test/Utils/MergeSchema.hs
@@ -1,8 +1,5 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Utils.MergeSchema
@@ -11,17 +8,9 @@ module Utils.MergeSchema
 where
 
 import Control.Applicative (pure)
-import Control.Monad ((>=>), (>>=))
+import Control.Monad ((>=>))
 import Control.Monad.Fail (fail)
-import Data.Aeson
-  ( FromJSON (..),
-    ToJSON (..),
-    eitherDecode,
-  )
 import qualified Data.ByteString.Lazy as L (readFile)
-import Data.Either (either)
-import Data.Foldable (foldlM)
-import Data.Functor (fmap)
 import Data.Morpheus.Core
   ( parseGQLDocument,
     render,
@@ -36,8 +25,6 @@ import Data.Morpheus.Types.Internal.Resolving
 import Data.Morpheus.Types.Internal.Stitching (Stitching (stitch))
 import Data.Semigroup ((<>))
 import Data.Text (unpack)
-import Data.Traversable (traverse)
-import GHC.Generics (Generic)
 import Test.Tasty
   ( TestTree,
     testGroup,
@@ -49,48 +36,27 @@ import Test.Tasty.HUnit
 import Prelude
   ( ($),
     (.),
-    Bool (..),
     Eq (..),
     FilePath,
     IO,
-    Maybe (..),
     otherwise,
     show,
   )
-
-data Case = Case
-  { schemas :: [FilePath],
-    success :: Maybe Bool
-  }
-  deriving (Generic, FromJSON, ToJSON)
 
 readSchema :: FilePath -> IO (Schema VALID)
 readSchema =
   L.readFile . ("test/" <>) . (<> ".gql")
     >=> (resultOr (fail . show) pure . parseGQLDocument)
 
-readSchemas :: Case -> IO [Schema VALID]
-readSchemas = traverse readSchema . schemas
-
-getCase :: FilePath -> IO Case
-getCase url =
-  L.readFile ("test/" <> url <> "/case.json")
-    >>= either fail prefix . eitherDecode
-  where
-    prefix Case {..} = pure $ Case {schemas = fmap ((url <> "/") <>) schemas, ..}
-
 readSchemaResult :: FilePath -> IO (Schema VALID)
-readSchemaResult = readSchema . (<> "/success")
-
-joinSchemas :: [Schema VALID] -> IO (Schema VALID)
-joinSchemas [] = fail "no empty case"
-joinSchemas (x : xs) = resultOr (fail . show) pure (foldlM stitch x xs)
+readSchemaResult = readSchema . (<> "/expected/ok")
 
 schemaCase :: FilePath -> TestTree
 schemaCase url = testCase url $ do
-  schemas <- getCase url >>= readSchemas
+  schema <- readSchema (url <> "/test/schema")
+  extension <- readSchema (url <> "/test/ext")
+  result <- resultOr (fail . show) pure (stitch schema extension)
   expected <- readSchemaResult url
-  result <- joinSchemas schemas
   assertion expected result
 
 assertion :: Schema VALID -> Schema VALID -> IO ()
