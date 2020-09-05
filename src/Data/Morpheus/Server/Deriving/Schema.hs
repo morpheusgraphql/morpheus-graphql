8eafcbf5923784deac372ffbb2eb65aff121be2f
david <d.nalchevanidze@gmail.com>
2020-09-05 21:10:53 +0200

    remove type operators

--- src/Data/Morpheus/Server/Deriving/Schema.hs
+++ src/Data/Morpheus/Server/Deriving/Schema.hs
@@ -9,7 +9,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
