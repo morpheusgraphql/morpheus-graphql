cfde6bc6ec4354a5ac264b81505a53ca913ad7b1
nalchevanidze <d.nalchevanidze@gmail.com>
2020-12-31 15:05:56 +0100

    Feature/Haxl Example (#568)

---
+++ morpheus-graphql-examples-scotty/src/Server/Haxl/API.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Server.Haxl.API
+  ( app,
+    httpEndpoint,
+  )
+where
+
+import Control.Applicative ((<|>))
+import Control.Monad.IO.Class (liftIO)
+import Data.Morpheus
+  ( App,
+    deriveApp,
+    runApp,
+  )
+import Data.Morpheus.Server
+  ( httpPlayground,
+  )
+import Data.Morpheus.Types
+  ( ComposedResolver,
+    ID,
+    QUERY,
+    Resolver,
+    ResolverQ,
+    RootResolver (..),
+    Undefined (..),
+    lift,
+    render,
+  )
+import Data.Morpheus.Types.IO (MapAPI)
+import Data.Text (Text)
+import Haxl.Core (dataFetch, initEnv, runHaxl, stateEmpty, stateSet)
+import Server.Haxl.DataSource
+  ( DeityReq (..),
+    Haxl,
+    State (DeityState),
+  )
+import Server.Haxl.Schema
+  ( Deity (..),
+    DeityArgs (..),
+    Query (..),
+  )
+import Server.Utils (isSchema)
+import Web.Scotty
+  ( RoutePattern,
+    ScottyM,
+    body,
+    get,
+    post,
+    raw,
+  )
+
+-- FETCH
+getDeityIds :: Haxl [ID]
+getDeityIds = dataFetch GetDeityIds
+
+getDeityNameById :: ID -> Haxl Text
+getDeityNameById = dataFetch . GetDeityNameById
+
+getDeityPowerById :: ID -> Haxl (Maybe Text)
+getDeityPowerById = dataFetch . GetDeityPowerById
+
+-- RESOLVERS
+getDeityById :: ID -> ResolverQ e Haxl Deity
+getDeityById deityId =
+  pure
+    Deity
+      { name = lift (getDeityNameById deityId),
+        power = lift (getDeityPowerById deityId)
+      }
+
+resolveDeity :: DeityArgs -> ResolverQ e Haxl Deity
+resolveDeity DeityArgs {deityId} = getDeityById deityId
+
+resolveDeities :: ComposedResolver QUERY e Haxl [] Deity
+resolveDeities = do
+  ids <- lift getDeityIds
+  traverse getDeityById ids
+
+resolveQuery :: Query (Resolver QUERY e Haxl)
+resolveQuery =
+  Query
+    { deity = resolveDeity,
+      deities = resolveDeities
+    }
+
+rootResolver :: RootResolver Haxl () Query Undefined Undefined
+rootResolver =
+  RootResolver
+    { queryResolver = resolveQuery,
+      mutationResolver = Undefined,
+      subscriptionResolver = Undefined
+    }
+
+app :: App () Haxl
+app = deriveApp rootResolver
+
+httpEndpoint ::
+  RoutePattern ->
+  App () Haxl ->
+  ScottyM ()
+httpEndpoint route app' = do
+  get route $ (isSchema *> raw (render app)) <|> raw httpPlayground
+  post route $ raw =<< (liftIO . runHaxlApp app' =<< body)
+
+runHaxlApp :: MapAPI a b => App e Haxl -> a -> IO b
+runHaxlApp haxlApp input = do
+  let stateStore = stateSet DeityState stateEmpty
+  enviroment <- initEnv stateStore ()
+  runHaxl enviroment (runApp haxlApp input)
---
+++ morpheus-graphql-examples-scotty/src/Server/Haxl/DataSource.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Server.Haxl.DataSource
+  ( Deity (..),
+    State (DeityState),
+    Haxl,
+    DeityReq (..),
+  )
+where
+
+import Control.Monad
+import Data.Hashable
+import Data.Morpheus.Types
+  ( ID (..),
+  )
+import Data.Semigroup ((<>))
+import Data.Text (Text)
+import Data.Typeable
+import Haxl.Core
+  ( BlockedFetch (..),
+    DataSource (..),
+    DataSourceName (..),
+    GenHaxl,
+    PerformFetch (..),
+    ResultVar (..),
+    ShowP (..),
+    StateKey (..),
+    putSuccess,
+  )
+import Server.Haxl.Schema
+  ( Deity (..),
+  )
+
+type Haxl = GenHaxl () ()
+
+data DeityReq a where
+  GetDeityIds :: DeityReq [ID]
+  GetDeityNameById :: ID -> DeityReq Text
+  GetDeityPowerById :: ID -> DeityReq (Maybe Text)
+  deriving (Typeable)
+
+deriving instance Eq (DeityReq a)
+
+instance Hashable (DeityReq a) where
+  hashWithSalt s GetDeityIds = hashWithSalt s (0 :: Int)
+  hashWithSalt s (GetDeityNameById a) = hashWithSalt s (1 :: Int, a)
+  hashWithSalt s (GetDeityPowerById a) = hashWithSalt s (2 :: Int, a)
+
+deriving instance Show (DeityReq a)
+
+instance ShowP DeityReq where showp = show
+
+instance StateKey DeityReq where
+  data State DeityReq = DeityState
+
+instance DataSourceName DeityReq where
+  dataSourceName _ = "DeityDataSource"
+
+instance DataSource u DeityReq where
+  fetch _ _ _ = BackgroundFetch myfetch
+
+fetchAll :: Foldable t => t (ResultVar [ID]) -> IO ()
+fetchAll allIdVars = do
+  allIds <- fetchDeityIds
+  mapM_ (`putSuccess` allIds) allIdVars
+
+handleBatched :: ([i] -> IO [a]) -> [(i, ResultVar a)] -> IO ()
+handleBatched f ls = unless (null ids) $ do
+  names <- f ids
+  mapM_ (uncurry putSuccess) (zip vars names)
+  where
+    (ids, vars) = unzip ls
+
+myfetch ::
+  [BlockedFetch DeityReq] ->
+  IO ()
+myfetch blockedFetches = do
+  unless (null allIdVars) (fetchAll allIdVars)
+  handleBatched fetchDeityNames [(uid, r) | BlockedFetch (GetDeityNameById uid) r <- blockedFetches]
+  handleBatched fetchDeityPowers [(uid, r) | BlockedFetch (GetDeityPowerById uid) r <- blockedFetches]
+  where
+    allIdVars :: [ResultVar [ID]]
+    allIdVars = [r | BlockedFetch GetDeityIds r <- blockedFetches]
+
+-- Fetch
+
+fetchDeityIds :: IO [ID]
+fetchDeityIds = do
+  print ("Fetch Ids" :: String)
+  pure ["Morpheus", "Zeus", "Ares"]
+
+fetchDeityNames :: [ID] -> IO [Text]
+fetchDeityNames ids = do
+  print ("Fetch Name for: " <> show ids)
+  pure (map unpackID ids)
+
+fetchDeityPowers :: [ID] -> IO [Maybe Text]
+fetchDeityPowers ids = do
+  print ("Fetch Power for: " <> show ids)
+  pure $ map (const $ Just "Shapeshifting") ids
