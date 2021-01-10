b9efe56d2b65b9d50617a7d6d66e325bdedff873
Davit Nalchevanidze <d.nalchevanidze@gmail.com>
2021-01-10 12:40:20 +0100

    core-tests

--- morpheus-graphql-core/test/Spec.hs
+++ morpheus-graphql-core/test/Spec.hs
@@ -11,10 +11,6 @@ import Test.Tasty
   ( defaultMain,
     testGroup,
   )
-import Utils.Api
-  ( apiTest,
-  )
-import qualified Utils.MergeSchema as MergeSchema
 import qualified Utils.Rendering as Rendering
 import Utils.Schema
   ( testSchema,
@@ -23,24 +19,10 @@ import Utils.Schema
 main :: IO ()
 main = do
   schema <- testSchema
-  mergeSchema <- MergeSchema.test
   defaultMain $
     testGroup
       "core tests"
       [ schema,
-        mergeSchema,
-        apiTest "api/deity" ["simple", "interface"],
-        apiTest
-          "api/validation/fragment"
-          [ "on-type",
-            "on-interface",
-            "on-interface-inline",
-            "on-union-type",
-            "fail-unknown-field-on-interface",
-            "on-interface-type-casting",
-            "on-interface-type-casting-inline",
-            "on-interface-fail-without-casting"
-          ],
         Rendering.test
           "rendering/simple"
           [ "simple",
