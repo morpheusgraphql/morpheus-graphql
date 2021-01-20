58a174f6120db5d18aa8a7999898d41d7eb16ed5
Davit Nalchevanidze <d.nalchevanidze@gmail.com>
2021-01-20 12:21:32 +0100

    Text vs BS

--- morpheus-graphql-core/bench/Main.hs
+++ morpheus-graphql-core/bench/Main.hs
@@ -11,18 +11,21 @@ import qualified Data.Morpheus.Core as Morpheus
 import Data.Morpheus.Internal.Ext (resultOr)
 import Data.Morpheus.Internal.Utils (fromLBS)
 import qualified Data.Text as T
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO
 import qualified Language.GraphQL.AST as GQL
 import Text.Megaparsec (runParser)
 
 main :: IO ()
 main = do
-  file <- L.readFile "bench/schema.gql"
-  defaultMain [schemaBenchmark file]
+  fileBS <- L.readFile "bench/schema.gql"
+  fileTS <- TIO.readFile "bench/schema.gql"
+  defaultMain [schemaBenchmark fileBS fileTS]
 
 parseMorpheus :: ByteString -> ByteString
 parseMorpheus x = resultOr (error . show) (const "OK") (Morpheus.parseTypeDefinitions x)
 
-parseGraphQL :: ByteString -> ByteString
+parseGraphQL :: Text -> Text
 parseGraphQL x = either (error . show) (const "OK") (parseTypeSysDefinition x)
 
 parseDoc :: T.Text -> Either T.Text [GQL.Definition]
@@ -31,9 +34,9 @@ parseDoc s =
     Right d -> Right (toList d)
     Left e -> Left (T.pack $ show e)
 
-parseTypeSysDefinition :: ByteString -> Either T.Text [GQL.TypeSystemDefinition]
+parseTypeSysDefinition :: Text -> Either Text [GQL.TypeSystemDefinition]
 parseTypeSysDefinition s =
-  case runParser GQL.document "<doc>" (fromLBS s) of
+  case runParser GQL.document "<doc>" s of
     Right (toList -> d) ->
       let tds = [td | GQL.TypeSystemDefinition td _ <- d]
        in if length d == length tds
@@ -42,10 +45,10 @@ parseTypeSysDefinition s =
     Left e ->
       Left (T.pack $ show e)
 
-schemaBenchmark :: ByteString -> Benchmark
-schemaBenchmark text =
+schemaBenchmark :: ByteString -> Text -> Benchmark
+schemaBenchmark bs txt =
   bgroup
     "basic"
-    [ bench "morpheus" $ nf parseMorpheus text,
-      bench "graphql" $ nf parseGraphQL text
+    [ bench "morpheus" $ nf parseMorpheus bs,
+      bench "graphql" $ nf parseGraphQL txt
     ]
