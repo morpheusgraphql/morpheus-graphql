0752d6ae63a2cf07a5cf5dfb69a1b6c4251a56ee
Davit Nalchevanidze <d.nalchevanidze@gmail.com>
2021-01-20 11:26:08 +0100

    morpheus

--- morpheus-graphql-core/bench/Main.hs
+++ morpheus-graphql-core/bench/Main.hs
@@ -4,14 +4,14 @@ module Main where
 
 import Criterion.Main
 import Data.ByteString.Lazy.Char8 (ByteString, pack)
-import Data.Morpheus.Core (parseSchema)
+import qualified Data.Morpheus.Core as Morpheus (parseSchema)
 import Data.Morpheus.Internal.Ext (resultOr)
 
 main :: IO ()
 main = defaultMain [schemaBenchmark "type Query {}"]
 
 parse :: ByteString -> ByteString
-parse x = resultOr (error . show) (const "OK") (parseSchema x)
+parse x = resultOr (error . show) (const "OK") (Morpheus.parseSchema x)
 
 schemaBenchmark :: ByteString -> Benchmark
 schemaBenchmark text =
