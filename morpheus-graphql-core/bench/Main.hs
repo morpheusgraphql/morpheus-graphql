6796ae8cc2f3b4581ef8ecf78896f3d2b8b41d88
Davit Nalchevanidze <d.nalchevanidze@gmail.com>
2021-01-20 12:13:42 +0100

    update error

--- morpheus-graphql-core/bench/Main.hs
+++ morpheus-graphql-core/bench/Main.hs
@@ -4,19 +4,21 @@
 module Main where
 
 import Criterion.Main
-import Data.ByteString.Lazy.Char8 (ByteString, pack)
+import qualified Data.ByteString.Lazy as L (readFile)
+import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.Foldable (toList)
 import qualified Data.Morpheus.Core as Morpheus (parseTypeDefinitions)
 import Data.Morpheus.Internal.Ext (resultOr)
 import Data.Morpheus.Internal.Utils (fromLBS)
 import qualified Data.Text as T
-import Language.GraphQL.AST (document)
 import qualified Language.GraphQL.AST as GQL
-import qualified Language.GraphQL.AST.Parser as GraphQL (document)
+--import qualified Language.GraphQL.AST.Parser as GQL (document)
 import Text.Megaparsec (runParser)
 
 main :: IO ()
-main = defaultMain [schemaBenchmark "type Query {}"]
+main = do
+  file <- L.readFile "bench/schema.gql"
+  defaultMain [schemaBenchmark file]
 
 parseMorpheus :: ByteString -> ByteString
 parseMorpheus x = resultOr (error . show) (const "OK") (Morpheus.parseTypeDefinitions x)
@@ -26,13 +28,13 @@ parseGraphQL x = either (error . show) (const "OK") (parseTypeSysDefinition x)
 
 parseDoc :: T.Text -> Either T.Text [GQL.Definition]
 parseDoc s =
-  case runParser document "<doc>" s of
+  case runParser GQL.document "<doc>" s of
     Right d -> Right (toList d)
     Left e -> Left (T.pack $ show e)
 
 parseTypeSysDefinition :: ByteString -> Either T.Text [GQL.TypeSystemDefinition]
 parseTypeSysDefinition s =
-  case runParser document "<doc>" (fromLBS s) of
+  case runParser GQL.document "<doc>" (fromLBS s) of
     Right (toList -> d) ->
       let tds = [td | GQL.TypeSystemDefinition td _ <- d]
        in if length d == length tds
