9437116f5ee019b2ce97330eeb0895c37c594f5f
Davit Nalchevanidze <d.nalchevanidze@gmail.com>
2021-03-28 19:21:23 +0200

    ignored characters

--- morpheus-graphql-core/src/Data/Morpheus/Parsing/Internal/Literals.hs
+++ morpheus-graphql-core/src/Data/Morpheus/Parsing/Internal/Literals.hs
@@ -17,8 +17,11 @@ import Relude hiding (ByteString, empty, many)
 import Text.Megaparsec
   ( label,
     many,
+    satisfy,
     sepBy1,
     skipManyTill,
+    takeWhile1P,
+    takeWhileP,
   )
 import Text.Megaparsec.Byte
   ( char,
@@ -41,44 +44,59 @@ import Text.Megaparsec.Byte
 -- '#'
 #define HASH_TAG 35
 
+-- White Space
+--
+-- Horizontal: Tab (U+0009) https://codepoints.net/U+0009
+#define TABULATION 9
+-- Space (U+0020) https://codepoints.net/U+0020
+#define SPACE 32
+-- UnicodeBOM (U+FEFF): https://codepoints.net/U+FEFF
+#define UNICODE_BOM 65279
+-- New Line (U+000A)
+#define NEW_LINE 10
+-- Carriage Return (U+000D) https://codepoints.net/U+000D
+#define CARRIAGE_RETURN 13
+-- U+FFFF : https://codepoints.net/U+FFFF
+#define NON_CHARACTER 65535
+
 at :: Parser ()
 at = symbol AT
-{-# INLINEABLE at #-}
+{-# INLINE at #-}
 
 equal :: Parser ()
 equal = symbol EQUAL
-{-# INLINEABLE equal #-}
+{-# INLINE equal #-}
 
 colon :: Parser ()
 colon = symbol COLON
-{-# INLINEABLE colon #-}
+{-# INLINE colon #-}
 
 symbol :: Word8 -> Parser ()
 symbol x = char x *> ignoredTokens
-{-# INLINEABLE symbol #-}
+{-# INLINE symbol #-}
 
 -- Ignored Tokens : https://graphql.github.io/graphql-spec/June2018/#sec-Source-Text.Ignored-Tokens
---  Ignored:
---    UnicodeBOM
---    WhiteSpace
---    LineTerminator
---    Comment
---    Comma
 ignoredTokens :: Parser ()
-ignoredTokens =
-  label "IgnoredTokens" $
-    space
-      *> many (comment <|> (char COMMA *> space))
-      *> space
+ignoredTokens = label "IgnoredTokens" $ many ignored $> ()
+{-# INLINE ignoredTokens #-}
+
+-- isIgnored :: UnicodeBOM, WhiteSpace, LineTerminator, Comment , Comma
+ignored :: Parser ()
+ignored = (takeWhile1P Nothing isIgnored <|> comment) $> ()
   where
-    comment = char HASH_TAG *> skipManyTill printChar newline *> space
-    {-# INLINEABLE comment #-}
-{-# INLINEABLE ignoredTokens #-}
+    isIgnored x =
+      (x >= TABULATION && x <= CARRIAGE_RETURN)
+        || x == SPACE
+        || x == COMMA
+        || x == UNICODE_BOM
+    comment = char HASH_TAG *> takeWhileP Nothing (/= NEW_LINE)
+    {-# INLINE comment #-}
+{-# INLINE ignored #-}
 
 ignoredTokens1 :: Parser ()
 ignoredTokens1 = space1 *> ignoredTokens
-{-# INLINEABLE ignoredTokens1 #-}
+{-# INLINE ignoredTokens1 #-}
 
 pipe :: Parser a -> Parser [a]
 pipe x = optional (symbol PIPE) *> (x `sepBy1` symbol PIPE)
-{-# INLINEABLE pipe #-}
+{-# INLINE pipe #-}
