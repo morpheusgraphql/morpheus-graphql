ed5966ae99bbd1dfb794f174546a3b7cd2b3a5b6
nalchevanidze <d.nalchevanidze@gmail.com>
2022-03-24 16:57:47 +0100

    update workflow

---
+++ .github/actions/upload-pkg/src/index.ts
@@ -0,0 +1,26 @@
+import { getInput, setFailed } from "@actions/core";
+// const github = require("@actions/github");
+import { promisify } from "util";
+import { exec } from "child_process";
+import { readFile } from "fs";
+import path from "path";
+
+const checkPackage = async (name: string) => {
+  const file = await promisify(readFile)(path.join(name, "package.yaml"));
+
+  console.log(file);
+};
+
+const uploadPackage = async () => {
+  const name = getInput("name");
+  const packageName = `morpheus-graphql-${name}`;
+
+  console.info(`start uploading package ${packageName}!`);
+  // check package version number
+  await checkPackage(packageName);
+  // upload package
+  await promisify(exec)(`stack upload ${packageName}`);
+  console.info(`successfully uploaded package${packageName}!`);
+};
+
+uploadPackage().catch(({ message }) => setFailed(message));
