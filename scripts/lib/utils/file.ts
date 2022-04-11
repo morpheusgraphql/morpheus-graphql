6ef65409f6c7191f553b6e9e429400599f73abf5
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-04-11 23:07:20 +0200

    stack config yaml (#725)
    * update stack packages
    
    * update cabal files

--- scripts/lib/utils/file.ts
+++ scripts/lib/utils/file.ts
@@ -2,12 +2,13 @@ import { promisify } from "util";
 import { readFile, writeFile } from "fs";
 import { dirname, join } from "path";
 import { dump, load } from "js-yaml";
-import { Config } from "../check-packages/types";
+import { Config, DepsMap } from "../check-packages/types";
+import { map } from "ramda";
 
 const ROOT_DIR = join(dirname(require.main?.filename ?? ""), "../");
 
 const absolutePath = (p: string) => join(ROOT_DIR, p);
-const STACK_CONFIG_URL = "./config/stack.json";
+const STACK_CONFIG_URL = "./config/stack.yaml";
 
 export const read = (url: string) =>
   promisify(readFile)(absolutePath(url), "utf8");
@@ -23,7 +24,17 @@ export const readJSON = <T>(name: string) =>
 
 export const writeYAML = <T>(url: string, obj: T) => write(url, dump(obj));
 
-export const getConfig = () => readJSON<Config>(STACK_CONFIG_URL);
+export const getConfig = async (): Promise<Config> => {
+  const { rules, ...rest } = await readYAML<Config>(STACK_CONFIG_URL);
+
+  return {
+    ...rest,
+    rules: map<DepsMap, DepsMap>(
+      ([min, max]) => [min.toString(), max.toString()],
+      rules
+    ),
+  };
+};
 
 export const writeConfig = (config: Config) =>
-  write(STACK_CONFIG_URL, JSON.stringify(config, null, 2));
+  write(STACK_CONFIG_URL, dump(config, { sortKeys: true }));
