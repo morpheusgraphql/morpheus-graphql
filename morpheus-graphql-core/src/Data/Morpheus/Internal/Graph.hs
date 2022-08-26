f679ceb10d5997571aaaa6a569f8a27a2e34fe0d
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-08-26 13:11:24 +0200

    es lint (#740)

--- morpheus-graphql-core/src/Data/Morpheus/Internal/Graph.hs
+++ morpheus-graphql-core/src/Data/Morpheus/Internal/Graph.hs
@@ -22,7 +22,7 @@ type Edges name = (Ref name, [Ref name])
 type Graph name = [Edges name]
 
 cycleChecking ::
-  (Applicative m, Eq name) =>
+  (Eq name, Monad m) =>
   (NonEmpty (Ref name) -> m ()) ->
   Graph name ->
   m ()
@@ -31,16 +31,13 @@ cycleChecking fail' graph = traverse_ checkNode graph
     checkNode (node, _) = cycleCheckingWith graph node [node] fail'
 
 cycleCheckingWith ::
-  (Applicative m, Eq name) =>
+  (Eq name, Monad m) =>
   Graph name ->
   Ref name ->
   [Ref name] ->
   (NonEmpty (Ref name) -> m ()) ->
   m ()
-cycleCheckingWith graph parentNode history fail' =
-  case lookup parentNode graph of
-    Just node -> traverse_ checkNode node
-    Nothing -> pure ()
+cycleCheckingWith graph parentNode history fail' = forM_ (lookup parentNode graph) (traverse_ checkNode)
   where
     checkNode node
       | node `elem` history =
