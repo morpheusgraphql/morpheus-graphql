ebf64260143d66947197b1dd4e781bae40af88b4
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-09-13 11:06:05 +0200

    update

--- morpheus-graphql/src/Data/Morpheus/Server/Types/Directives.hs
+++ morpheus-graphql/src/Data/Morpheus/Server/Types/Directives.hs
@@ -1,29 +1,62 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveLift #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Data.Morpheus.Server.Types.Directives
-  ( DIRECTIVE_LOCATION,
-    GQLDirective (..),
+  ( GQLDirective (..),
   )
 where
 
 import Data.Morpheus.Internal.Ext (GQLResult)
-import Data.Morpheus.Types.Internal.AST (DirectiveLocation (..), FieldDefinition (FieldDefinition), TypeDefinition (TypeDefinition), VALID)
+import Data.Morpheus.Types.Internal.AST
+  ( FieldDefinition (..),
+    INPUT_OBJECT,
+    OBJECT,
+    OUT,
+    TypeDefinition (..),
+    VALID,
+  )
+import Language.Haskell.TH.Syntax (Lift)
 
-type family DIRECTIVE_LOCATION a (l :: DirectiveLocation) :: Bool
+data SchemaDirectiveLocation
+  = SCALAR
+  | OBJECT
+  | FIELD_DEFINITION
+  | INPUT_FIELD_DEFINITION
+  | ARGUMENT_DEFINITION
+  | INTERFACE
+  | UNION
+  | ENUM
+  | INPUT_OBJECT
+  deriving (Show, Eq, Lift)
 
-type family DirRes a (l :: Bool) where
+type family DirRes a l where
   DirRes a 'True = a -> GQLResult a
-  DirRes a 'False = ()
+  DirRes a l = ()
 
 class GQLDirective a where
-  mapObject :: a -> DirRes (TypeDefinition k b) (DIRECTIVE_LOCATION a 'OBJECT)
-  mapField :: a -> DirRes (FieldDefinition k b) (DIRECTIVE_LOCATION a 'FIELD)
+  type DIRECTIVE_LOCATION a (l :: SchemaDirectiveLocation) :: Bool
 
--- example
+  visitObject :: f a -> DirRes (TypeDefinition OBJECT VALID) (DIRECTIVE_LOCATION a 'OBJECT)
+  visitInputObject :: f a -> DirRes (TypeDefinition INPUT_OBJECT VALID) (DIRECTIVE_LOCATION a 'INPUT_OBJECT)
+  visitUnion :: f a -> DirRes (TypeDefinition OUT VALID) (DIRECTIVE_LOCATION a 'UNION)
+
+  visitInputFieldDefinition :: f a -> DirRes (FieldDefinition INPUT_OBJECT VALID) (DIRECTIVE_LOCATION a 'INPUT_FIELD_DEFINITION)
+  visitFieldDefinition :: f a -> DirRes (FieldDefinition OBJECT VALID) (DIRECTIVE_LOCATION a 'FIELD_DEFINITION)
+
+-- visitArgumentDefinition :: GraphQLArgument
+
+-- visitScalar :: (scalar : GraphQLScalarType)
+
+-- visitSchema(schema: GraphQLSchema)
+-- visitInterface(i: GraphQLInterfaceType)
+-- visitEnum :: GraphQLEnumType
+-- visitEnumValue :: (value : GraphQLEnumValue)
 
 data User = User {}
 
-type instance DIRECTIVE_LOCATION User 'FIELD = 'True
-
-type instance DIRECTIVE_LOCATION User 'OBJECT = 'False
+instance GQLDirective User where
+  type DIRECTIVE_LOCATION User 'FIELD_DEFINITION = 'True
+  type DIRECTIVE_LOCATION User 'OBJECT = 'True
+  visitFieldDefinition _ = pure
+  visitObject _ = pure
