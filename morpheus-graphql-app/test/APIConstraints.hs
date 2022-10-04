483e2348e286b6aa8b7c5d189c73e5536d18c6cd
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-10-04 12:09:10 +0200

    better SelectionTree API

--- morpheus-graphql-app/test/APIConstraints.hs
+++ morpheus-graphql-app/test/APIConstraints.hs
@@ -6,6 +6,7 @@ module APIConstraints
   )
 where
 
+import Data.Aeson (Value (..))
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import Data.Morpheus.App
   ( APIConstraint,
@@ -24,7 +25,7 @@ import Data.Morpheus.Core
   ( parseSchema,
   )
 import Data.Morpheus.Types.IO
-  ( GQLRequest (..),
+  ( GQLRequest,
     GQLResponse,
   )
 import Data.Morpheus.Types.Internal.AST
@@ -50,7 +51,8 @@ resolvers =
         const $
           object
             [ ("success", pure "success"),
-              ("forbidden", pure "forbidden!")
+              ("forbidden", pure "forbidden!"),
+              ("limited", pure "is less then 5")
             ]
       )
     ]
@@ -61,15 +63,22 @@ getSchema url = LBS.readFile url >>= resultOr (fail . show) pure . parseSchema
 getApp :: FileUrl -> IO (App e IO)
 getApp _ = (`mkApp` resolvers) <$> getSchema "test/api-constraints/schema.gql"
 
-constraint :: APIConstraint
-constraint _ operation
-  | memberNode "forbidden" operation = Left "no forbidden field!"
+forbidden :: APIConstraint
+forbidden _ query
+  | hasChild ("forbidden" :: String) query = Left "no forbidden field!"
   | otherwise = Right ()
+
+max5 :: APIConstraint
+max5 _ query = do
+  let value = getChild ("limited" :: String) query >>= getArgument ("num" :: String)
+  case value of
+    Just (Number num) | num >= 5 -> Left ("no num should be less then 5! but found " <> show num)
+    _ -> pure ()
 
 runAPIConstraints :: FileUrl -> FileUrl -> TestTree
 runAPIConstraints url = testApi api
   where
     api :: GQLRequest -> IO GQLResponse
     api req = do
-      app <- withConstraint constraint <$> getApp url
+      app <- withConstraint max5 . withConstraint forbidden <$> getApp url
       runApp app req
