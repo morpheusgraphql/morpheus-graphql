de3130746f7f9de3c9114a2a94ef0e0012a5776a
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-10-19 22:06:32 +0200

    consistent printing

--- morpheus-graphql-code-gen/src/Data/Morpheus/CodeGen/Server/Internal/AST.hs
+++ morpheus-graphql-code-gen/src/Data/Morpheus/CodeGen/Server/Internal/AST.hs
@@ -51,14 +51,12 @@ import Data.Morpheus.Types.Internal.AST
     Value,
     unpackName,
   )
-import Data.Text.Prettyprint.Doc (indent, line)
+import Data.Text.Prettyprint.Doc (concatWith, indent, line)
 import Language.Haskell.TH.Lib (appE, varE)
 import Prettyprinter
   ( Pretty (..),
     align,
     pretty,
-    punctuate,
-    vsep,
     (<+>),
   )
 import Relude hiding (Show, optional, print, show)
@@ -159,7 +157,7 @@ data ServerMethod
 
 instance Pretty ServerMethod where
   pretty (ServerMethodDefaultValues x) = pretty (show x)
-  pretty (ServerMethodDirectives dirs) = line <> indent 2 (align $ vsep $ punctuate " <>" (map pretty dirs))
+  pretty (ServerMethodDirectives dirs) = line <> indent 2 (align $ concatWith (\x y -> x <> "\n  <>" <+> y) (map pretty dirs))
 
 instance PrintExp ServerMethod where
   printExp (ServerMethodDefaultValues values) = [|values|]
