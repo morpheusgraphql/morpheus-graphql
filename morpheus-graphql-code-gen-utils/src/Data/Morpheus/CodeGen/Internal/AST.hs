d62c8cd312de7bf4b4a60d7cb2cf40cb6a853310
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-10-19 21:28:29 +0200

    print

--- morpheus-graphql-code-gen-utils/src/Data/Morpheus/CodeGen/Internal/AST.hs
+++ morpheus-graphql-code-gen-utils/src/Data/Morpheus/CodeGen/Internal/AST.hs
@@ -70,14 +70,14 @@ data TypeValue
   deriving (Show)
 
 renderField :: (FieldName, TypeValue) -> Doc n
-renderField (fName, fValue) = pretty (unpackName fName :: Text) <> "=" <+> pretty fValue
+renderField (fName, fValue) = pretty (unpackName fName :: Text) <+> "=" <+> pretty fValue
 
 instance Pretty TypeValue where
   pretty (TypeValueObject name xs) =
     pretty (unpackName name :: Text)
       <+> "{"
-      <+> vsep (punctuate "," (map renderField xs))
-      <+> "}"
+      <> vsep (punctuate "," (map renderField xs))
+      <> "}"
   pretty (TypeValueNumber x) = pretty x
   pretty (TypeValueString x) = pretty (show x :: String)
   pretty (TypeValueBool x) = pretty x
