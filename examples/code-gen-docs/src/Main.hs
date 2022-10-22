24097457aa9aa58aba2f1536b42eed9da147b949
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-10-22 17:10:16 +0200

    build apps

--- examples/code-gen-docs/src/Main.hs
+++ examples/code-gen-docs/src/Main.hs
@@ -1,4 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Morpheus (httpPlayground, runApp)
+import Server.API (app)
+import Web.Scotty (ActionM, body, get, post, raw, scotty)
+
+runScotty :: ActionM () -> IO ()
+runScotty = scotty 3000 . post "/"
+
 main :: IO ()
-main = pure ()
+main =
+  scotty 3000 $ do
+    get "/" (raw httpPlayground)
+    post "/" (raw =<< runApp app =<< body)
