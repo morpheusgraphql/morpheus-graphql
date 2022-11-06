1e1f7e4f9e4b44d8c2057eb31ae2eaff0f3b2db0
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-11-06 04:01:23 +0100

    update

--- morpheus-graphql-server/src/Data/Morpheus/Server/NamedResolvers.hs
+++ morpheus-graphql-server/src/Data/Morpheus/Server/NamedResolvers.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
