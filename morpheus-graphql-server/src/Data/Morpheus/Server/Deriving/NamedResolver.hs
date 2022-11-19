7f5ab132914d8885b8a3bbfadea0b69faa5ee8f5
David Nalchevanidze <d.nalchevanidze@gmail.com>
2022-11-19 21:44:01 +0100

    remove decode value

--- morpheus-graphql-server/src/Data/Morpheus/Server/Deriving/NamedResolver.hs
+++ morpheus-graphql-server/src/Data/Morpheus/Server/Deriving/NamedResolver.hs
@@ -38,11 +38,11 @@ import Data.Morpheus.Server.Deriving.Utils.GTraversable
   )
 import Data.Morpheus.Server.Deriving.Utils.Kinded (KindedProxy (KindedProxy), outputType)
 import Data.Morpheus.Server.Deriving.Utils.Proxy (ContextValue (..))
-import Data.Morpheus.Server.Deriving.Utils.Use (UseDeriving (..), UseGQLType (useTypename), UseNamedResolver (..))
+import Data.Morpheus.Server.Deriving.Utils.Use (UseDeriving (..), UseGQLType (useTypename), UseNamedResolver (..), UseValue (useDecodeValue))
 import Data.Morpheus.Server.NamedResolvers (Dependency, NamedResolverT (..), ResolveNamed (..))
 import Data.Morpheus.Server.Types.GQLType
   ( GQLType,
-    GQLValue (decodeValue),
+    GQLValue,
     KIND,
     withDir,
   )
@@ -73,70 +73,72 @@ withNamed =
 instance KindedNamedFunValue GQLNamedResolverFun GQLType GQLValue (KIND a) m a => GQLNamedResolverFun m a where
   namedResolverFun resolver = kindedNamedFunValue withNamed (ContextValue resolver :: ContextValue (KIND a) a)
 
-deriveNamedResolver :: Mappable (KindedNamedResolver GQLType m) [NamedResolver m] KindedProxy
+deriveNamedResolver :: Mappable (KindedNamedResolver GQLType GQLValue m) [NamedResolver m] KindedProxy
 deriveNamedResolver = Mappable (kindedNamedResolver withNamed)
 
-type EncodeTypeConstraint gql m a =
-  ( GFmap (ScanConstraint (KindedNamedResolver gql m)) (KIND (a (NamedResolverT m))) (a (NamedResolverT m)),
-    KindedNamedResolver gql m (KIND (a (NamedResolverT m))) (a (NamedResolverT m)),
-    gql (a (NamedResolverT m))
-  )
-
-class KindedNamedResolver gql (m :: Type -> Type) (k :: DerivingKind) a where
-  kindedNamedResolver :: UseNamedResolver GQLNamedResolverFun gql val -> f k a -> [NamedResolver m]
-
-instance
-  ( DecodeValuesConstraint o e m a,
-    EncodeScalar a,
-    gql a
-  ) =>
-  KindedNamedResolver gql (Resolver o e m) SCALAR a
+decodeValues :: forall gql val o e m a. DecodeValuesConstraint val o e m a => UseDeriving gql val -> Proxy a -> [ValidValue] -> Resolver o e m [Maybe a]
+decodeValues ctx _ xs = traverse decodeArg xs >>= resolveBatched
   where
-  kindedNamedResolver ctx _ =
-    [ NamedResolver
-        { resolverName = useTypename (dirGQL $ namedDrv ctx) (outputType proxy),
-          resolverFun = decodeValues proxy >=> pure . map (maybe NamedNullResolver (NamedScalarResolver . encodeScalar))
-        }
-    ]
-    where
-      proxy = Proxy @a
+    decodeArg :: ValidValue -> Resolver o e m (Dependency a)
+    decodeArg = liftResolverState . useDecodeValue (dirArgs ctx)
 
-type DecodeValuesConstraint o e m a =
+type DecodeValuesConstraint val o e m a =
   ( LiftOperation o,
     ResolveNamed (Resolver o e m) a,
     Monad m,
-    GQLValue (Dependency a)
+    val (Dependency a)
   )
 
-decodeValues :: forall o e m a. DecodeValuesConstraint o e m a => Proxy a -> [ValidValue] -> Resolver o e m [Maybe a]
-decodeValues _ xs = traverse decodeArg xs >>= resolveBatched
-  where
-    decodeArg :: ValidValue -> Resolver o e m (Dependency a)
-    decodeArg = liftResolverState . decodeValue
+type EncodeTypeConstraint gql val m a =
+  ( GFmap (ScanConstraint (KindedNamedResolver gql val m)) (KIND (a (NamedResolverT m))) (a (NamedResolverT m)),
+    KindedNamedResolver gql val m (KIND (a (NamedResolverT m))) (a (NamedResolverT m)),
+    gql (a (NamedResolverT m))
+  )
+
+class KindedNamedResolver gql val (m :: Type -> Type) (k :: DerivingKind) a where
+  kindedNamedResolver :: UseNamedResolver GQLNamedResolverFun gql val -> f k a -> [NamedResolver m]
 
 instance
-  ( DecodeValuesConstraint o e m a,
-    Generic a,
-    gql a,
-    gql [Maybe a],
-    DeriveWith gql (GQLNamedResolverFun (Resolver o e m)) (Resolver o e m (ResolverValue (Resolver o e m))) (Rep a)
+  ( DecodeValuesConstraint gql o e m a,
+    EncodeScalar a,
+    val (Dependency a),
+    gql a
   ) =>
-  KindedNamedResolver gql (Resolver o e m) TYPE (a :: Type)
+  KindedNamedResolver gql val (Resolver o e m) SCALAR a
   where
   kindedNamedResolver ctx _ =
     [ NamedResolver
         { resolverName = useTypename (dirGQL $ namedDrv ctx) (outputType proxy),
-          resolverFun = decodeValues proxy >=> deriveNamedResolverFun ctx
+          resolverFun = decodeValues (namedDrv ctx) proxy >=> pure . map (maybe NamedNullResolver (NamedScalarResolver . encodeScalar))
         }
     ]
     where
       proxy = Proxy @a
 
-instance KindedNamedResolver gql m (KIND a) a => KindedNamedResolver gql m CUSTOM (NamedResolverT m a) where
+instance
+  ( DecodeValuesConstraint gql o e m a,
+    Generic a,
+    gql a,
+    gql [Maybe a],
+    val (Dependency a),
+    DeriveWith gql (GQLNamedResolverFun (Resolver o e m)) (Resolver o e m (ResolverValue (Resolver o e m))) (Rep a)
+  ) =>
+  KindedNamedResolver gql val (Resolver o e m) TYPE (a :: Type)
+  where
+  kindedNamedResolver ctx _ =
+    [ NamedResolver
+        { resolverName = useTypename (dirGQL $ namedDrv ctx) (outputType proxy),
+          resolverFun = decodeValues (namedDrv ctx) proxy >=> deriveNamedResolverFun ctx
+        }
+    ]
+    where
+      proxy = Proxy @a
+
+instance KindedNamedResolver gql val m (KIND a) a => KindedNamedResolver gql val m CUSTOM (NamedResolverT m a) where
   kindedNamedResolver ctx _ = kindedNamedResolver ctx (KindedProxy :: KindedProxy (KIND a) a)
 
-instance KindedNamedResolver gql m (KIND a) a => KindedNamedResolver gql m CUSTOM (input -> a) where
+instance KindedNamedResolver gql val m (KIND a) a => KindedNamedResolver gql val m CUSTOM (input -> a) where
   kindedNamedResolver ctx _ = kindedNamedResolver ctx (KindedProxy :: KindedProxy (KIND a) a)
 
-instance KindedNamedResolver gql m (KIND a) a => KindedNamedResolver gql m WRAPPER (f a) where
+instance KindedNamedResolver gql val m (KIND a) a => KindedNamedResolver gql val m WRAPPER (f a) where
   kindedNamedResolver ctx _ = kindedNamedResolver ctx (KindedProxy :: KindedProxy (KIND a) a)
