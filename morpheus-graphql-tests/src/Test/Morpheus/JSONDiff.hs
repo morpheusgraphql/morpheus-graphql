c9868ad41ba6c941bb35aa572c0a7b5d2cf65fe5
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-04-21 13:28:42 +0200

    diff arrays

--- morpheus-graphql-tests/src/Test/Morpheus/JSONDiff.hs
+++ morpheus-graphql-tests/src/Test/Morpheus/JSONDiff.hs
@@ -25,7 +25,14 @@ instance Show JSONDiff where
   show (DiffNode xs) = intercalate "\n" (map showField xs)
     where
       showField (k, v) = unescape (show k) <> ":\n  " <> indent (show v)
-  show (DiffLeaf x y) = "expected: " <> unpack (encode x) <> "\ngot:" <> unpack (encode y) <> ")"
+  show (DiffLeaf x y) =
+    "should be:"
+      <> showLeaf x
+      <> "but it is:"
+      <> showLeaf y
+
+showLeaf :: ToJSON a => a -> [Char]
+showLeaf x = " " <> unpack (encode x) <> "\n"
 
 unescape :: String -> String
 unescape = concatMap f
@@ -47,6 +54,13 @@ diff (Object beforeFields) (Object afterFields)
     changes = concatMap (\key -> (key,) <$> getFieldPair key) ks
     getFieldPair key = maybeToList $ diff (getField key beforeFields) (getField key afterFields)
     ks = uniq (keys (beforeFields <> afterFields))
+diff (Array ls1) (Array ls2)
+  | null changes = Nothing
+  | otherwise = Just (DiffNode changes)
+  where
+    changes = concatMap (\(key, value) -> (fromString $ show key,) <$> getFieldPair value) ks
+    getFieldPair (b, a) = maybeToList $ diff b a
+    ks = zip ([1 ..] :: [Int]) (zipOptional (toList ls1) (toList ls2))
 diff v1 v2
   | v1 == v2 =
       Nothing
@@ -65,3 +79,9 @@ jsonEQ :: ToJSON a => a -> a -> IO ()
 jsonEQ expected actual = case diff (toJSON expected) (toJSON actual) of
   Just x -> assertFailure $ indent $ "\n" <> show x <> "\n"
   Nothing -> pure ()
+
+zipOptional :: [Value] -> [Value] -> [(Value, Value)]
+zipOptional [] [] = []
+zipOptional (x : xs) [] = (x, Null) : zipOptional xs []
+zipOptional [] (y : ys) = (Null, y) : zipOptional [] ys
+zipOptional (x : xs) (y : ys) = (x, y) : zipOptional xs ys
