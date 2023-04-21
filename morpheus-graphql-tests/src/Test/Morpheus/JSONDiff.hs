67ed8a7d62915c3dd864ca3b6c70dcf249de6904
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-04-21 14:34:36 +0200

    update test diff

--- morpheus-graphql-tests/src/Test/Morpheus/JSONDiff.hs
+++ morpheus-graphql-tests/src/Test/Morpheus/JSONDiff.hs
@@ -18,13 +18,13 @@ import Relude hiding (ByteString, Show, show)
 import Test.Tasty.HUnit (assertFailure)
 
 data JSONDiff
-  = DiffNode [(Key, JSONDiff)]
+  = DiffNode [(String, JSONDiff)]
   | DiffLeaf Value Value
 
 instance Show JSONDiff where
   show (DiffNode xs) = intercalate "\n" (map showField xs)
     where
-      showField (k, v) = unescape (show k) <> ":\n  " <> indent (show v)
+      showField (k, v) = k <> ":\n  " <> indent (show v)
   show (DiffLeaf x y) =
     "should be:"
       <> showLeaf x
@@ -46,25 +46,26 @@ indent = concatMap f
     f '\n' = "\n  "
     f x = x : ""
 
-diff :: Value -> Value -> Maybe JSONDiff
-diff (Object beforeFields) (Object afterFields)
-  | null changes = Nothing
-  | otherwise = Just (DiffNode changes)
+diff :: (Value, Value) -> Maybe JSONDiff
+diff (Object beforeFields, Object afterFields) = diffNode $ map toPair ks
   where
-    changes = concatMap (\key -> (key,) <$> getFieldPair key) ks
-    getFieldPair key = maybeToList $ diff (getField key beforeFields) (getField key afterFields)
     ks = uniq (keys (beforeFields <> afterFields))
-diff (Array ls1) (Array ls2)
-  | null changes = Nothing
-  | otherwise = Just (DiffNode changes)
+    toPair key = (unescape (show key), (getField key beforeFields, getField key afterFields))
+diff (Array beforeElems, Array afterElems) = diffNode (zip ks vs)
   where
-    changes = concatMap (\(key, value) -> (fromString $ show key,) <$> getFieldPair value) ks
-    getFieldPair (b, a) = maybeToList $ diff b a
-    ks = zip ([1 ..] :: [Int]) (zipOptional (toList ls1) (toList ls2))
-diff v1 v2
+    ks = map show ([1 ..] :: [Int])
+    vs = zipOptional (toList beforeElems) (toList afterElems)
+diff (v1, v2)
   | v1 == v2 =
       Nothing
   | otherwise = Just (DiffLeaf v1 v2)
+
+diffNode :: [(String, (Value, Value))] -> Maybe JSONDiff
+diffNode values
+  | null entries = Nothing
+  | otherwise = Just (DiffNode entries)
+  where
+    entries = mapMaybe (\(key, value) -> (key,) <$> diff value) values
 
 getField :: Key -> KeyMap Value -> Value
 getField key = fromMaybe Null . lookup key
@@ -76,7 +77,7 @@ uniq (x : xs)
   | otherwise = x : uniq xs
 
 jsonEQ :: ToJSON a => a -> a -> IO ()
-jsonEQ expected actual = case diff (toJSON expected) (toJSON actual) of
+jsonEQ expected actual = case diff (toJSON expected, toJSON actual) of
   Just x -> assertFailure $ indent $ "\n" <> show x <> "\n"
   Nothing -> pure ()
 
