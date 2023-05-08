b24d1ea6767252c79138e8c73b521fbd01a647f3
David Nalchevanidze <d.nalchevanidze@gmail.com>
2023-05-08 22:06:13 +0200

    simplify gmap

--- morpheus-graphql-server/src/Data/Morpheus/Generic/Gmap.hs
+++ morpheus-graphql-server/src/Data/Morpheus/Generic/Gmap.hs
@@ -6,7 +6,6 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -15,7 +14,6 @@
 
 module Data.Morpheus.Generic.Gmap
   ( Gmap,
-    GmapFun (..),
     gmap,
   )
 where
@@ -38,24 +36,15 @@ import GHC.Generics
   )
 import Relude
 
-newtype GmapFun (fun :: Type -> Constraint) (v :: Type) = GmapFun
-  { gmapFun :: forall f a. (fun a) => f a -> v
-  }
+-- newtype GmapFun (fun :: Type -> Constraint) (v :: Type) = GmapFun
+--   { gmapFun :: forall f a. (fun a) => f a -> v
+--   }
 
 gmap :: (Gmap c (Rep a)) => f a -> [CProxy c]
-gmap p = runGmap (rep p) gmapProxyFun
-
-runGmap :: (Gmap c a, Monoid b) => f a -> GmapFun c b -> b
-runGmap x = runReader (gfmap x)
-
-runFun :: (fun a) => f a -> GmapFun fun v -> v
-runFun x GmapFun {..} = gmapFun x
-
-gmapProxyFun :: GmapFun c [CProxy c]
-gmapProxyFun = GmapFun (pure . CProxy)
+gmap p = gfmap (rep p)
 
 class Gmap (c :: Type -> Constraint) a where
-  gfmap :: (Monoid v) => proxy a -> Reader (GmapFun c v) v
+  gfmap :: f a -> [CProxy c]
 
 instance (Datatype d, Gmap c a) => Gmap c (M1 D d a) where
   gfmap _ = gfmap (Proxy @a)
@@ -64,13 +53,13 @@ instance (Gmap con a) => Gmap con (M1 C c a) where
   gfmap _ = gfmap (Proxy @a)
 
 instance (Gmap c a, Gmap c b) => Gmap c (a :+: b) where
-  gfmap _ = liftA2 (<>) (gfmap (Proxy @a)) (gfmap (Proxy @b))
+  gfmap _ = gfmap (Proxy @a) <> gfmap (Proxy @b)
 
 instance (Gmap c a, Gmap c b) => Gmap c (a :*: b) where
-  gfmap _ = liftA2 (<>) (gfmap (Proxy @a)) (gfmap (Proxy @b))
+  gfmap _ = gfmap (Proxy @a) <> gfmap (Proxy @b)
 
 instance (c a) => Gmap c (M1 S s (K1 x a)) where
-  gfmap _ = runFun (Proxy @a) <$> ask
+  gfmap _ = [CProxy (Proxy @a)]
 
 instance Gmap c U1 where
-  gfmap _ = pure mempty
+  gfmap _ = []
