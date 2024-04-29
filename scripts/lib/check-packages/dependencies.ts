532da8bcb367b413fb88d32ed05d26ff0ad4a574
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-04-29 22:17:25 +0200

    sort deps

--- scripts/lib/check-packages/dependencies.ts
+++ scripts/lib/check-packages/dependencies.ts
@@ -2,27 +2,28 @@ import { formatTable } from "./formatting";
 import { Config } from "../utils/config";
 import { StackPackage } from "../utils/package";
 
-const formatDeps = (config: Config) => (dependencies: string[]) =>
+const formatDeps = <T extends string[]>(config: Config, dependencies: T) =>
   formatTable(
     dependencies
       .map((d) => d.split(/\s+/))
       .sort(([a], [b]) => a.charCodeAt(0) - b.charCodeAt(0))
       .map(([name, ...args]) => config.checkDependency(name, !args.length))
-  );
+  ) as T;
 
-const updateDeps = <T extends object>(config: Config, value: T): T => {
+const updateDeps = <T extends object>(
+  config: Config,
+  value: T,
+  isDeps?: boolean
+): T => {
   if (!value) return value;
   if (typeof value === "object") {
     if (Array.isArray(value)) {
-      return value.sort();
+      return isDeps ? formatDeps(config, value.sort()) : value.sort();
     }
 
     Object.fromEntries(
       Object.entries(value).map(([k, v]) => {
-        return [
-          k,
-          k === "dependencies" ? formatDeps(config)(v) : updateDeps(config, v),
-        ];
+        return [k, updateDeps(config, v, k === "dependencies")];
       })
     ) as T;
   }
