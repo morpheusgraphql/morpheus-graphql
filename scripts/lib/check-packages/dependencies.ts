a49fdd633424f2dc21a9c4253e182574b7dba31e
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-04-29 22:21:24 +0200

    update deps

--- scripts/lib/check-packages/dependencies.ts
+++ scripts/lib/check-packages/dependencies.ts
@@ -10,18 +10,17 @@ const formatDeps = <T extends string[]>(config: Config, deps: T) =>
       .map(([name, ...args]) => config.checkDependency(name, !args.length))
   ) as T;
 
-const update = <T>(config: Config, value: T, isDeps?: boolean): T => {
+const update = <T>(config: Config, value: T, k?: string): T => {
   if (!value) return value;
   if (typeof value === "object") {
     if (Array.isArray(value)) {
-      return isDeps ? formatDeps(config, value.sort()) : value.sort();
+      return k === "dependencies"
+        ? formatDeps(config, value.sort())
+        : value.sort();
     }
 
     Object.fromEntries(
-      Object.entries(value).map(([k, v]) => [
-        k,
-        update(config, v, k === "dependencies"),
-      ])
+      Object.entries(value).map(([k, v]) => [k, update(config, v, k)])
     ) as T;
   }
   return value;
