bb5265b12b9553831e3d1d02dd20a8fe1c728d44
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-05-12 14:33:43 +0200

    ConfigT

--- hconf/src/HConf/ConfigT.hs
+++ hconf/src/HConf/ConfigT.hs
@@ -2,22 +2,40 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
-module HConf.ConfigT (ConfigT (..)) where
+module HConf.ConfigT
+  ( ConfigT (..),
+    packages,
+    version,
+    unpackValue,
+  )
+where
 
-import Control.Monad.Reader (MonadReader, ReaderT)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader.Class (MonadReader (..), asks)
+import Control.Monad.Trans.Reader (ReaderT (..))
 import Data.Kind
-import HConf.Config (Config)
+import HConf.Config (Config, getPackages, getVersion)
+import HConf.Utils (Name)
+import HConf.Version (Version)
 
-newtype
-  ConfigT
-    (m :: Type -> Type)
-    (a :: Type)
-  = ConfigT {runConfigT :: ReaderT Config m a}
+newtype ConfigT (a :: Type)
+  = ConfigT {runConfigT :: ReaderT Config IO a}
   deriving
     ( Functor,
       Applicative,
       Monad,
-      MonadReader Config
+      MonadReader Config,
+      MonadIO
     )
+
+unpackValue :: Config -> ConfigT a -> IO a
+unpackValue config (ConfigT (ReaderT f)) = f config
+
+packages :: ConfigT [Name]
+packages = getPackages <$> asks id
+
+version :: ConfigT Version
+version = getVersion <$> asks id
