779d3bb6b2ecb7bf57ab79e17eb4202df0c76eb4
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-05-22 20:38:37 +0200

    log

--- scripts/lib/git.ts
+++ scripts/lib/git.ts
@@ -3,10 +3,10 @@ import { exec } from "./utils";
 
 const git = (...cmd: string[]) => exec(["git", ...cmd].join(" "));
 
-const getDate = () => git("log -1 --format=%cd --date=short");
-const lastTag = () => git("describe --abbrev=0 --tags");
+const getDate = () => git("log", "-1 --format=%cd --date=short");
+const lastTag = () => git("describe", "--abbrev=0 --tags");
 const commitsAfter = (tag: string) =>
-  git("rev-list", `--reverse ${tag}..`).split("\n");
+  git("rev-list", "--reverse", `${tag}..`).split("\n");
 
 export const push = (name: string) => {
   git("add", ".");
