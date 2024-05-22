9706fe098a4ccbeb9bfb5604cd04e1087742d5b4
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-05-22 17:03:29 +0200

    file

--- scripts/lib/utils/file.ts
+++ scripts/lib/utils/file.ts
@@ -1,24 +1,15 @@
 import { readFile, writeFile } from "fs/promises";
 import { dirname, join } from "path";
-import { DumpOptions, dump, load } from "js-yaml";
+import { load } from "js-yaml";
 
 const ROOT = join(dirname(require.main?.filename ?? ""), "../");
 
 const absolute = (p: string) => join(ROOT, p);
 
-const read = (url: string) => readFile(absolute(url), "utf8");
-
 export const write = (url: string, file: string) =>
   writeFile(absolute(url), file, "utf8");
 
-class Yaml<T, A extends string[] = [string]> {
-  constructor(private file: (...x: A) => string) {}
-  read = (...p: A) => read(this.file(...p)).then(load) as Promise<T>;
-  write = (o: T, ops?: DumpOptions, ...p: A) =>
-    write(this.file(...p), dump(o, ops));
-}
-
 export const getVersion = () =>
-  new Yaml<{ version: string }, []>(() => "./hconf.yaml")
-    .read()
-    .then((x) => x.version);
+  readFile(absolute("./hconf.yaml"), "utf8")
+    .then((s) => load(s) as { version: string })
+    .then(({ version }) => version);
