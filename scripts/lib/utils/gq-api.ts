8ab3d63913baecf4783853940141c08c18043e7d
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-05-22 18:10:04 +0200

    api

--- scripts/lib/utils/gq-api.ts
+++ scripts/lib/utils/gq-api.ts
@@ -4,20 +4,23 @@ const GH_ORG = "morpheusgraphql";
 
 const GH_REPO = "morpheus-graphql";
 
-const { GITHUB_TOKEN } = process.env;
+const token = () => {
+  const { GITHUB_TOKEN } = process.env;
 
-if (!GITHUB_TOKEN) {
-  throw new Error("missing variable: GITHUB_TOKEN");
-}
+  if (!GITHUB_TOKEN) {
+    throw new Error("missing variable: GITHUB_TOKEN");
+  }
+  return GITHUB_TOKEN;
+};
 
 const authorizedGithubUrl = () =>
-  `https://${GITHUB_TOKEN}@github.com/${GH_ORG}/${GH_REPO}.git`;
+  `https://${token()}@github.com/${GH_ORG}/${GH_REPO}.git`;
 
 const ghApiREST = (path: string, body: {}) =>
   axios
     .post(`https://api.github.com/${path}`, JSON.stringify(body), {
       headers: {
-        authorization: `Bearer ${GITHUB_TOKEN}`,
+        authorization: `Bearer ${token()}`,
         "content-type": "application/json",
         accept: "Accept: application/vnd.github.v3+json",
       },
