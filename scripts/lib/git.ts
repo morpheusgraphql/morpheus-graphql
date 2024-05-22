31d8d18094f8f046d623da3efd3f657cd01a598d
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-05-22 20:36:01 +0200

    update

--- scripts/lib/git.ts
+++ scripts/lib/git.ts
@@ -5,8 +5,8 @@ const git = (...cmd: string[]) => exec(["git", ...cmd].join(" "));
 
 const getDate = () => git("log -1 --format=%cd --date=short");
 const lastTag = () => git("describe --abbrev=0 --tags");
-const commitsAfter = (tag: string): string[] =>
-  git(`rev-list --reverse ${tag}..`).split("\n");
+const commitsAfter = (tag: string) =>
+  git("rev-list", `--reverse ${tag}..`).split("\n");
 
 export const push = (name: string) => {
   git("add .");
