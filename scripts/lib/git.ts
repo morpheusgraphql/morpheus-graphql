3a518bb678880a88aa4af883993fb8a8b8431849
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-05-22 20:35:30 +0200

    update

--- scripts/lib/git.ts
+++ scripts/lib/git.ts
@@ -1,16 +1,18 @@
 import { authorizedGithubUrl } from "./gq-api";
 import { exec } from "./utils";
 
-const getDate = () => exec("git log -1 --format=%cd --date=short");
-const lastTag = () => exec("git describe --abbrev=0 --tags");
+const git = (...cmd: string[]) => exec(["git", ...cmd].join(" "));
+
+const getDate = () => git("log -1 --format=%cd --date=short");
+const lastTag = () => git("describe --abbrev=0 --tags");
 const commitsAfter = (tag: string): string[] =>
-  exec(`git rev-list --reverse ${tag}..`).split("\n");
+  git(`rev-list --reverse ${tag}..`).split("\n");
 
 export const push = (name: string) => {
-  exec("git add .");
-  exec("git status");
-  exec(`git commit -m "${name}"`);
-  exec(`git push ${authorizedGithubUrl()} HEAD:${name}`);
+  git("add .");
+  git("status");
+  git(`commit -m "${name}"`);
+  git(`push ${authorizedGithubUrl()} HEAD:${name}`);
 };
 
 export { getDate, lastTag, commitsAfter, exec };
