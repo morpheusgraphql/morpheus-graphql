a810e3349ac37ef0bb2ac51fac0a984ed989030b
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-05-30 18:47:27 +0200

    update

--- hconf/src/HConf/Bounds.hs
+++ hconf/src/HConf/Bounds.hs
@@ -48,6 +48,13 @@ parseRestriction '>' = pure Min -- > 0.7.0
 parseRestriction '<' = pure Max -- <  1.0.0
 parseRestriction x = fail ("unsorted bound type" <> show x)
 
+instance ToString Restriction where
+  toString Min = ">" -- >  0.7.0
+  toString Max = "<" -- <  1.0.0
+
+instance ToText Restriction where
+  toText = pack . toString
+
 data Bound = Bound
   { restriction :: Restriction,
     orEquals :: Bool,
@@ -61,12 +68,21 @@ instance Ord Bound where
       <> compare (restriction a) (restriction b)
       <> compare (orEquals a) (orEquals b)
 
-instance ToString Restriction where
-  toString Min = ">" -- >  0.7.0
-  toString Max = "<" -- <  1.0.0
+parseOrEquals :: [Char] -> (Bool, [Char])
+parseOrEquals ('=' : ver) = (True, ver)
+parseOrEquals ver = (False, ver)
 
-instance ToText Restriction where
-  toText = pack . toString
+printBoundPart :: Bound -> [Text]
+printBoundPart Bound {..} = pack (toString restriction <> if orEquals then "=" else "") : [toText version]
+
+instance Parse Bound where
+  parse = parseBound . toString
+    where
+      parseBound (char : str) = do
+        res <- parseRestriction char
+        let (isStrict, value) = parseOrEquals str
+        Bound res isStrict <$> parse value
+      parseBound x = fail ("unsorted bound type" <> toString x)
 
 newtype Bounds = Bounds [Bound]
   deriving (Generic, Show, Eq)
@@ -86,27 +102,11 @@ upperBounds version = do
 diff :: Bounds -> Bounds -> String
 diff old deps = printBounds old <> chalk Yellow "  ->  " <> printBounds deps
 
-parseOrEquals :: [Char] -> (Bool, [Char])
-parseOrEquals ('=' : ver) = (True, ver)
-parseOrEquals ver = (False, ver)
-
-instance Parse Bound where
-  parse = parseBound . toString
-    where
-      parseBound (char : str) = do
-        res <- parseRestriction char
-        let (isStrict, value) = parseOrEquals str
-        Bound res isStrict <$> parse value
-      parseBound x = fail ("unsorted bound type" <> toString x)
-
 printBoundParts :: Bounds -> [Text]
 printBoundParts (Bounds xs) = intercalate ["&&"] $ map printBoundPart $ sort xs
 
 getBound :: Restriction -> Bounds -> Maybe Bound
 getBound v (Bounds xs) = find (\Bound {..} -> restriction == v) xs
-
-printBoundPart :: Bound -> [Text]
-printBoundPart Bound {..} = pack (toString restriction <> if orEquals then "=" else "") : [toText version]
 
 printBounds :: Bounds -> String
 printBounds = intercalate "  " . map toString . printBoundParts
