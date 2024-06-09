fbc53a1c888f265151147bfca06695b0f774a10d
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-10 01:02:13 +0200

    Scripts (#866)
    * scripts
    
    * config
    
    * config
    
    * releasy
    
    * package
    
    * update

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -1,13 +1,43 @@
-import { github } from "./lib/gh";
-import { exit, write } from "./lib/utils";
-import { changelog } from "./lib/changelog";
+import { exit, hconf, write } from "./utils";
 
 import { Command } from "commander";
-import { format } from "./lib/format";
+import { format } from "./format";
+import { GHRelEasy } from "gh-rel-easy";
 
 const cli = new Command();
 
 cli.name("cli").description("cli").version("0.0.0");
+
+const scope: Record<string, string> = {
+  server: "morpheus-graphql",
+  client: "morpheus-graphql-client",
+  core: "morpheus-graphql-core",
+  subscriptions: "morpheus-graphql-subscriptions",
+  tests: "morpheus-graphql-tests",
+  app: "morpheus-graphql-app",
+};
+
+const relEasy = new GHRelEasy({
+  pkg: (name) => `https://hackage.haskell.org/package/${scope[name]}`,
+  gh: {
+    org: "morpheusgraphql",
+    repo: "morpheus-graphql",
+  },
+  scope,
+  pr: {
+    major: "Major Change",
+    breaking: "Breaking Change",
+    feature: "New features",
+    fix: "Bug Fixes",
+    chore: "Minor Changes",
+  },
+  version: () => hconf("version"),
+  next: async (isBreaking) => {
+    await hconf("next", ...(isBreaking ? ["-b"] : []));
+
+    return hconf("version");
+  },
+});
 
 cli
   .command("format")
@@ -22,13 +52,20 @@ release
   .command("open")
   .option("-p, --preview", "preview", false)
   .action(({ preview }: { preview: string }) =>
-    changelog(true)
-      .then(preview ? () => Promise.resolve() : github.release)
+    relEasy
+      .changelog()
+      .then(async (body) => {
+        await hconf("setup");
+
+        if (preview) return;
+
+        await relEasy.open(body);
+      })
       .catch(exit)
   );
 
 release
   .command("changelog")
-  .action(() => changelog().then(write("changelog.md")).catch(exit));
+  .action(() => relEasy.changelog().then(write("changelog.md")).catch(exit));
 
 cli.parse();
