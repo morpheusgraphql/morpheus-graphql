1bd2aa497047e36cd5b538961e4178c345107aa0
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-12 12:32:50 +0200

    0.12.0

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,17 +1,11 @@
-import { writeFile } from "fs/promises";
 import { GHRelEasy, runCli } from "gh-rel-easy";
 import { Command } from "commander";
-
-export const exit = (error: Error) => {
-  console.log(error.message);
-  process.exit(1);
-};
 
 // HConf
 const hconf = runCli("hconf");
 
 // GHRelEasy
-const release = new GHRelEasy({
+const gh = new GHRelEasy({
   gh: "morpheusgraphql/morpheus-graphql",
   scope: {
     server: "morpheus-graphql",
@@ -23,7 +17,8 @@ const release = new GHRelEasy({
   },
   pkg: (p) => `https://hackage.haskell.org/package/${p}`,
   version: () => hconf("version"),
-  next: (b) => hconf("next", ...(b ? ["-b"] : [])).then(console.log),
+  next: (b) => hconf("next", b && "-b").then(console.log),
+  setup: () => hconf("setup").then(console.log),
 });
 
 // CLI
@@ -33,23 +28,11 @@ cli.name("release-cli").description("Automated Releases").version("1.0");
 
 cli
   .command("open")
-  .option("-p, --preview", "preview", false)
-  .action(({ preview }: { preview: boolean }) =>
-    release
-      .changelog()
-      .then((body) =>
-        hconf("setup")
-          .then(console.log)
-          .then(() => (preview ? undefined : release.open(body)))
-      )
-      .catch(exit)
-  );
+  .option("-d, --dry", "only changelog and setup", false)
+  .action(({ dry }: { dry: boolean }) => gh.release(dry));
 
-cli.command("changelog").action(() =>
-  release
-    .changelog()
-    .then((body: string) => writeFile("./changelog.md", body, "utf8"))
-    .catch(exit)
-);
+cli.command("changelog").action(async () => {
+  await gh.changelog("changelog");
+});
 
 cli.parse();
